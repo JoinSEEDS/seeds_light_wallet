86e7667303f845f1b854cb38424b387c528fa8d7
Igor Berlenko <igor@berlenko.com>
2020-02-04 18:04:08 +0800

    add reverseHash implementation

--- lib/utils/invites.dart
+++ lib/utils/invites.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:crypto/crypto.dart';
 import 'package:random_words/random_words.dart';
+import 'package:basic_utils/basic_utils.dart';
 
 String generateMnemonic({ int words = 5, separator = '-' }) {
   var random = Random.secure();
@@ -21,4 +22,19 @@ String generateMnemonic({ int words = 5, separator = '-' }) {
 
 String convertHash(String data) {
   return sha256.convert(utf8.encode(data)).toString();
+}
+
+String reverseHash(String hash) {
+    String firstPart = hash.substring(0, 32);
+    String secondPart = hash.substring(32, 64);
+
+    List<String> firstPartChunks = StringUtils.chunk(firstPart, 2);
+    List<String> secondPartChunks = StringUtils.chunk(secondPart, 2);
+
+    String firstPartReversed = firstPartChunks.reversed.toList().join('');
+    String secondPartReversed = secondPartChunks.reversed.toList().join('');
+
+    String concatenatedReversedHash = "$firstPartReversed$secondPartReversed";
+
+    return concatenatedReversedHash; 
 }
