c29721411e311cd48797027e75b1e609107f0b5e
nik <nheger@gmail.com>
2020-04-17 21:32:55 +0200

    Using our node #128

--- lib/constants/config.dart
+++ lib/constants/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static final defaultEndpoint = "https://api.telosfoundation.io";
+  static final defaultEndpoint = "https://node.hypha.earth";
   static final onboardingAccountName = "join.seeds";
   static final onboardingPrivateKey = "5JhM4vypLzLdDtHo67TR5RtmsYm2mr8F2ugqcrCzfrMPLvo8cQW";
   static final testingPrivateKey = "5K8TTSrUkeJRaznjN75zjNKcget1Uo3M5VNXj4xt7mRYU3HdmhC";
