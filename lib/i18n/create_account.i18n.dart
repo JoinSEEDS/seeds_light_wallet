b340a136de6214746ac38461e2decff58ca6e251
nik <nheger@gmail.com>
2020-06-19 22:09:00 +0200

    fix

--- lib/i18n/create_account.i18n.dart
+++ lib/i18n/create_account.i18n.dart
@@ -21,7 +21,8 @@ static var _t = Translations.byLocale("en_us") +
         "Name can't have special characters":"Nombre no puede tener caracteres especiale",
         "Name should have 12 symbols": "Nombre debe tener 12 caracteres",
         "Only letters a..z and numbers 1..5": "Solo letras de a..z y números 1..5",
-        "%s is not available": "%s no está disponible"
+        "%s is not available": "%s no está disponible",
+        
         // NOTE: The sentence "your account should have exactly 12 symbols ..."
         // the words "exactly 12" are bold on the screen
         "Your account name should have ": "Tu nombre de cuenta debe tener ",
