fee830a04176c6e21fbc98d56967b0090fbb2819
nik <nheger@gmail.com>
2020-10-02 16:10:25 +0200

    lint - remove unused import

--- lib/screens/app/ecosystem/proposals/proposal_header_details.dart
+++ lib/screens/app/ecosystem/proposals/proposal_header_details.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_toolbox/flutter_toolbox.dart';
 import 'package:intl/intl.dart';
 import 'package:percent_indicator/percent_indicator.dart';
-import 'package:seeds/constants/app_colors.dart';
 import 'package:seeds/models/models.dart';
 import 'package:seeds/providers/notifiers/voted_notifier.dart';
 import 'package:timeago/timeago.dart' as timeago;
