c4c72b10c92699b7d55b41879455f1fabf3c8835
Gerardo Guijarro <gguijarro_c@chewy.com>
2020-10-23 15:32:45 -0400

    Guardians views

---
+++ lib/screens/app/guardians/guardian_users_list.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:seeds/models/firebase/guardian_status.dart';
+import 'package:seeds/models/models.dart';
+import 'package:seeds/providers/services/firebase/firebase_database_map_keys.dart';
+
+import 'guardian_user_tile.dart';
+
+ListView buildGuardiansListView(
+    AsyncSnapshot<List<MemberModel>> memberModels, String currentUserId, List<QueryDocumentSnapshot> guardians) {
+  return ListView(
+    children: memberModels.data
+        .map((e) => guardianUserTile(
+            user: e,
+            currentUserId: currentUserId,
+            status: fromName(guardians.firstWhere((element) => element.id == e.account)[GUARDIANS_STATUS_KEY]),
+            onTap: () {}))
+        .toList(),
+  );
+}
--- lib/screens/app/guardians/my_guardians_tab.dart
+++ lib/screens/app/guardians/my_guardians_tab.dart
@@ -1,11 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:seeds/models/firebase/guardian_status.dart';
 import 'package:seeds/models/models.dart';
 import 'package:seeds/providers/notifiers/settings_notifier.dart';
-import 'package:seeds/providers/services/firebase/firebase_database_map_keys.dart';
 import 'package:seeds/providers/services/http_service.dart';
-import 'package:seeds/screens/shared/guardian_user_tile.dart';
+import 'package:seeds/screens/app/guardians/guardian_users_list.dart';
 
 class MyGuardiansTab extends StatefulWidget {
   final List<QueryDocumentSnapshot> guardians;
@@ -25,16 +23,7 @@ class _MyGuardiansState extends State<MyGuardiansTab> {
         future: HttpService().getMembersByIds(widget.guardians.map((e) => e.id).toList()),
         builder: (context, memberModels) {
           if (memberModels.hasData) {
-            return ListView(
-              children: memberModels.data
-                  .map((e) => guardianUserTile(
-                      user: e,
-                      currentUserId: SettingsNotifier.of(context).accountName,
-                      status: fromName(
-                          widget.guardians.firstWhere((element) => element.id == e.account)[GUARDIANS_STATUS_KEY]),
-                      onTap: () {}))
-                  .toList(),
-            );
+            return buildGuardiansListView(memberModels, SettingsNotifier.of(context).accountName, widget.guardians);
           } else {
             return SizedBox.shrink();
           }
