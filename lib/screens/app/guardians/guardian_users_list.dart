7169334151b0d9a944bb3b5d42da07196ef6e41b
Gerardo Guijarro <gguijarro_c@chewy.com>
2020-10-25 14:33:22 -0400

    Cleanup

--- lib/screens/app/guardians/guardian_users_list.dart
+++ lib/screens/app/guardians/guardian_users_list.dart
@@ -4,15 +4,15 @@ import 'package:seeds/models/models.dart';
 
 import 'guardian_user_tile.dart';
 
-Widget buildGuardiansListView(List<MemberModel> memberModels, String currentUserId,
-    List<Guardian> guardians, GestureTapCallback tileOnTap) {
-  return memberModels.isNotEmpty ? ListView(
+ListView buildGuardiansListView(
+    List<MemberModel> memberModels, String currentUserId, List<Guardian> guardians, GestureTapCallback tileOnTap) {
+  return ListView(
     children: memberModels
         .map((e) => guardianUserTile(
             user: e,
             currentUserId: currentUserId,
-            status: guardians.firstWhere((element) => element.uid == e.account, orElse: () => null)?.status,
+            status: guardians.firstWhere((element) => element.uid == e.account).status,
             tileOnTap: tileOnTap))
         .toList(),
-  ) : SizedBox.shrink();
+  );
 }
