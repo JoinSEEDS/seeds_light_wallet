73ae82083675ec2057540057a841e6bb6711a08f
Gerardo Guijarro <gguijarro_c@chewy.com>
2020-10-25 14:31:04 -0400

    Better logic handling and less network calls.

--- lib/screens/app/guardians/guardian_users_list.dart
+++ lib/screens/app/guardians/guardian_users_list.dart
@@ -4,15 +4,15 @@ import 'package:seeds/models/models.dart';
 
 import 'guardian_user_tile.dart';
 
-ListView buildGuardiansListView(AsyncSnapshot<List<MemberModel>> memberModels, String currentUserId,
+Widget buildGuardiansListView(List<MemberModel> memberModels, String currentUserId,
     List<Guardian> guardians, GestureTapCallback tileOnTap) {
-  return ListView(
-    children: memberModels.data
+  return memberModels.isNotEmpty ? ListView(
+    children: memberModels
         .map((e) => guardianUserTile(
             user: e,
             currentUserId: currentUserId,
-            status: guardians.firstWhere((element) => element.uid == e.account).status,
+            status: guardians.firstWhere((element) => element.uid == e.account, orElse: () => null)?.status,
             tileOnTap: tileOnTap))
         .toList(),
-  );
+  ) : SizedBox.shrink();
 }
--- lib/screens/app/guardians/im_guardian_for_tab.dart
+++ lib/screens/app/guardians/im_guardian_for_tab.dart
@@ -1,33 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:seeds/models/firebase/guardian.dart';
+import 'package:seeds/models/firebase/guardian_type.dart';
 import 'package:seeds/models/models.dart';
 import 'package:seeds/providers/notifiers/settings_notifier.dart';
-import 'package:seeds/providers/services/http_service.dart';
 import 'package:seeds/screens/app/guardians/guardian_users_list.dart';
 
-class ImGuardianForTab extends StatefulWidget {
+class ImGuardianForTab extends StatelessWidget {
   final List<Guardian> guardians;
+  final List<MemberModel> allMembers;
 
-  ImGuardianForTab(this.guardians);
+  ImGuardianForTab(this.guardians, this.allMembers);
 
-  @override
-  State<StatefulWidget> createState() {
-    return _ImGuardianForState();
-  }
-}
-
-class _ImGuardianForState extends State<ImGuardianForTab> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<MemberModel>>(
-        future: HttpService().getMembersByIds(widget.guardians.map((e) => e.uid).toList()),
-        builder: (context, memberModels) {
-          if (memberModels.hasData) {
-            return buildGuardiansListView(
-                memberModels, SettingsNotifier.of(context).accountName, widget.guardians, () {});
-          } else {
-            return SizedBox.shrink();
-          }
-        });
+    var myGuardians = guardians.where((Guardian e) => e.type == GuardianType.imGuardian).toList();
+    var myMembers = allMembers.where((item) => myGuardians.map((e) => e.uid).contains(item.account)).toList();
+
+    return buildGuardiansListView(myMembers, SettingsNotifier.of(context).accountName, myGuardians, () {
+      // TODO: Not sure what we do here
+    });
   }
 }
