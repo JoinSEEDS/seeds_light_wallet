309f6536de2ed097b16825176ce5dff79cd1b1f4
NIK <nheger@gmail.com>
2020-11-06 18:38:38 +0800

    use improved dart-esr and eosdart library (#337)
    added unit test

--- test/utils/dartesr_test.dart
+++ test/utils/dartesr_test.dart
@@ -10,8 +10,6 @@ void main() {
 
       await request.resolve(account: "bukabukabuka");
 
-      print("1 action: "+request.actions.first.toJson().toString());
-
       var action = request.actions.first;
       var data = Map<String, dynamic>.from(action.data);
 
@@ -41,7 +39,9 @@ void main() {
       expect(data['quantity'], '7.0000 SEEDS');
       expect(data['memo'], '');
 
-    });    test('decode esr 3', () async {
+    });    
+    
+    test('decode esr 3', () async {
       var esr = 'esr:gmN0S9_Eeqy57zv_9xn9eU3hL_bxCbUs-jptJqsXY3-JtawgU09qb_yjZdUau3YzAAEjQ0EQV_NKhbNAdriuzdmzjIwMEMAEpTVgAgeWdbM9zBDoDTgM4bMEu7q6BDMwsJekFpdk5qUDzWIAAA';
 
       var request = SeedsESR(uri: esr);
@@ -60,7 +60,27 @@ void main() {
       expect(data['quantity'], '5.0000 SEEDS');
       expect(data['memo'], 'testing');
     });
-    
+
+    test('decode esr with 112 Seeds', () async {
+      var esr = 'esr://gmN0S9_Eeqy57zv_9xn9eU3hL_bxCbUs-jptJqsXY3-JtawgA0NBEFfzSoWzDAwM4bo2Z88yMjJAABOUVoQJGKw8nWa1MrnkhZQgmM8S7OrqEgxkABUAAA';
+
+      var request = SeedsESR(uri: esr);
+
+      await request.resolve(account: "bukabukabuka");
+
+      print("1 action: "+request.actions.first.toJson().toString());
+
+      var action = request.actions.first;
+      var data = Map<String, dynamic>.from(action.data);
+
+      expect(request.actions.first.account, 'token.seeds');
+      expect(request.actions.first.name, 'transfer');
+      expect(data['from'], 'bukabukabuka');
+      expect(data['to'], 'illumination');
+      expect(data['quantity'], '112.1000 SEEDS');
+      expect(data['memo'], '');
+    });
+
     test('login signing', () async {
       
       var esr = 'esr:AgACAwACO2h0dHBzOi8vY2IuYW5jaG9yLmxpbmsvODE3NzNjYWUtYjUzZS00YTdmLTg2ZjctNzJmOTQzZjhiYTk3AQRsaW5rKgAIAAAA06oHAAKLu05xjZ9d38TXa0W9f_WH76gGXk4wxIzEw31kEpVMQg';
