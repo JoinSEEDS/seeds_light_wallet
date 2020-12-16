321b7ee243c86ece1f0dbace655de37815659898
NIK <nheger@gmail.com>
2020-12-17 01:50:10 +0800

    added documentation, group methods more logically together (#389)

--- lib/providers/services/eos_service.dart
+++ lib/providers/services/eos_service.dart
@@ -331,6 +331,83 @@ Map<String, dynamic> requiredAuthToJson(RequiredAuth instance) =>
     return client.pushTransaction(transaction, broadcast: true);
   }
 
+  Future<List<Permission>> getAccountPermissions() async {
+    print("[http] get account permissions");
+
+    if (mockEnabled) {
+      return Future.value(HttpMockResponse.accountPermissions);
+    }
+
+    final String url = "$baseURL/v1/chain/get_account";
+    final String body = '{ "account_name": "$accountName" }';
+    Map<String, String> headers = { "Content-type": "application/json" };
+
+    Response res = await post(url, headers: headers, body: body);
+
+    if (res.statusCode == 200) {
+      Map<String, dynamic> body = res.parseJson();
+
+      List<Permission> permissions =
+          List<Permission>.from(body["permissions"].map((item) => Permission.fromJson(item)).toList());
+
+      return permissions;
+    } else {
+      print('Cannot fetch account permissions...');
+      return List<Permission>();
+    }
+  }
+
+  /// Step 1 in the guardian set up - call this to allow the guard.seeds contract to 
+  /// change the key.
+  /// 
+  /// Before the guardian contract can act on a recovery request, the users account needs to 
+  /// allow the guardian contract to change the keys.
+  /// 
+  Future<void> setGuardianPermission() async {
+
+    final currentPermissions = await getAccountPermissions();
+
+    final ownerPermission =
+        currentPermissions.firstWhere((item) => item.permName == "owner");
+
+    ownerPermission.requiredAuth.accounts.add({
+      "weight": ownerPermission.requiredAuth.threshold,
+      "permission": {"actor": "guard.seeds", "permission": "eosio.code"}
+    });
+
+    await updatePermission(ownerPermission);
+  }
+
+  /// Remove guardian contract permission
+  /// 
+  /// Call this to remove the permission to change keys from guard.seeds contract.
+  /// 
+  /// Call when user turns off recovery to leave the account clean.
+  /// 
+  Future<void> removeGuardianPermission() async {
+    final currentPermissions = await getAccountPermissions();
+
+    final ownerPermission =
+        currentPermissions.firstWhere((item) => item.permName == "owner");
+
+    List<dynamic> newAccounts = [];
+    for (Map<String, dynamic> acct in ownerPermission.requiredAuth.accounts) {
+      if (acct["permission"]["actor"] == "guard.seeds") {
+        //print("found guardian permission");
+      } else {
+        newAccounts.add(acct);
+      }
+    }
+    ownerPermission.requiredAuth.accounts = newAccounts;
+    await updatePermission(ownerPermission);
+  }
+
+  /// Step 2 setting up guardians - set the guardians for an account
+  /// 
+  /// guardians - list of Seeds account names that are the guardians - 3, 4, or 5 elements. 
+  /// 
+  /// Will fail when it's already set up - in that case, call cancelGuardians first.
+  /// 
   Future<dynamic> initGuardians(List<String> guardians) async {
     print("[eos] init guardians");
 
@@ -355,6 +432,13 @@ Map<String, dynamic> requiredAuthToJson(RequiredAuth instance) =>
     return client.pushTransaction(transaction, broadcast: true);
   }
 
+  /// Recover an account via the key guardian system
+  /// 
+  /// userAcount - the account to recovery, iE current user is a guardian for userAccount
+  /// publicKey - the new public key on the account once the recovery is complete
+  /// 
+  /// When 2 or 3 of the guardians call this function, the account can be recovered with claim
+  /// 
   Future<dynamic> recoverAccount(String userAccount, String publicKey) async {
     print("[eos] recover account $userAccount");
 
@@ -379,7 +463,11 @@ Map<String, dynamic> requiredAuthToJson(RequiredAuth instance) =>
     return client.pushTransaction(transaction, broadcast: true);
   }
 
-  Future<dynamic> cancelRecovery() async {
+  /// Cancel guardians. 
+  /// 
+  /// This cancels any recovery currently in process, and removes all guardians
+  ///  
+  Future<dynamic> cancelGuardians() async {
     print("[eos] cancel recovery $accountName");
 
     if (mockEnabled) return HttpMockResponse.transactionResult;
@@ -399,6 +487,13 @@ Map<String, dynamic> requiredAuthToJson(RequiredAuth instance) =>
     return client.pushTransaction(transaction, broadcast: true);
   }
 
+  /// Claim recovered account for user - this switches the new public key live at the end of the
+  /// recovery process.
+  /// 
+  /// This can be called without logging in - the assumption is that the user lost their key, 
+  /// asked for recovery, was recovered, waited 24 hours for the time lock - and then calls this 
+  /// method to regain access.
+  /// 
   Future<dynamic> claimRecoveredAccount(String userAccount) async {
     print("[eos] claim recovered account $userAccount");
     String applicationAccount = Config.onboardingAccountName;
@@ -438,65 +533,6 @@ Map<String, dynamic> requiredAuthToJson(RequiredAuth instance) =>
     Transaction transaction = buildFreeTransaction(actions);
 
     return client.pushTransaction(transaction, broadcast: true);
-  }
-
-  Future<List<Permission>> getAccountPermissions() async {
-    print("[http] get account permissions");
-
-    if (mockEnabled) {
-      return Future.value(HttpMockResponse.accountPermissions);
-    }
-
-    final String url = "$baseURL/v1/chain/get_account";
-    final String body = '{ "account_name": "$accountName" }';
-    Map<String, String> headers = { "Content-type": "application/json" };
-
-    Response res = await post(url, headers: headers, body: body);
-
-    if (res.statusCode == 200) {
-      Map<String, dynamic> body = res.parseJson();
-
-      List<Permission> permissions =
-          List<Permission>.from(body["permissions"].map((item) => Permission.fromJson(item)).toList());
-
-      return permissions;
-    } else {
-      print('Cannot fetch account permissions...');
-      return List<Permission>();
-    }
-  }
-
-  Future<void> setGuardianPermission() async {
-
-    final currentPermissions = await getAccountPermissions();
-
-    final ownerPermission =
-        currentPermissions.firstWhere((item) => item.permName == "owner");
-
-    ownerPermission.requiredAuth.accounts.add({
-      "weight": ownerPermission.requiredAuth.threshold,
-      "permission": {"actor": "guard.seeds", "permission": "eosio.code"}
-    });
-
-    await updatePermission(ownerPermission);
-  }
-
-  Future<void> removeGuardianPermission() async {
-    final currentPermissions = await getAccountPermissions();
-
-    final ownerPermission =
-        currentPermissions.firstWhere((item) => item.permName == "owner");
-
-    List<dynamic> newAccounts = [];
-    for (Map<String, dynamic> acct in ownerPermission.requiredAuth.accounts) {
-      if (acct["permission"]["actor"] == "guard.seeds") {
-        //print("found guardian permission");
-      } else {
-        newAccounts.add(acct);
-      }
-    }
-    ownerPermission.requiredAuth.accounts = newAccounts;
-    await updatePermission(ownerPermission);
   }
 
   Future<String> generateInvoice(double amount) async {
