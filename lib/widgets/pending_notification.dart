11074cbf41867ba6e9dd4822ecd11f309be10c46
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-01-09 21:23:55 -0500

    remove icon shadow

--- lib/widgets/pending_notification.dart
+++ lib/widgets/pending_notification.dart
@@ -9,14 +9,6 @@ guardianNotification(bool showGuardianNotification) {
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           color: Colors.red,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 3,
-              color: Colors.black,
-              spreadRadius: -2,
-              offset: Offset(0, 2),
-            )
-          ],
         ));
   } else {
     return Container();
