52e65bf5e0172d5262e79d7cb07c98bfe2f096dd
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-01-18 13:57:10 -0500

    add a flag for key guardian notification (#435)

--- lib/widgets/pending_notification.dart
+++ lib/widgets/pending_notification.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:seeds/providers/services/firebase/firebase_remote_config.dart';
 
 guardianNotification(bool showGuardianNotification) {
-  if (showGuardianNotification) {
+  if (showGuardianNotification && FirebaseRemoteConfigService().featureFlagGuardiansEnabled) {
     return Icon(Icons.brightness_1, size: 18.0, color: Colors.redAccent);
   } else {
-    return Container();
+    return SizedBox.shrink();
   }
 }
+
