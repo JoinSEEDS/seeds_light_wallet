715735c8f73f40d886988a4aa09cfc955f16664b
Raul Urtecho <raul.urtecho512@gmail.com>
2021-02-18 20:26:06 -0600

    first advace

--- lib/v2/screens/profile/components/profile_bottom.dart
+++ lib/v2/screens/profile/components/profile_bottom.dart
@@ -165,6 +165,103 @@ class ProfileBottom extends StatelessWidget {
               ],
             ),
           ),
+          Container(
+            decoration: const BoxDecoration(
+              color: AppColors.jungle,
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+            ),
+            child: Stack(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        bottomRight: Radius.circular(12.0),
+                      ),
+                      child: SvgPicture.asset(
+                        "assets/images/lotus.svg",
+                        alignment: Alignment.bottomRight,
+                        color: AppColors.canopy,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'You are on the way from'.i18n,
+                                  style: Theme.of(context).textTheme.subtitle2HighEmphasis,
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    'Resident'.i18n,
+                                    style: Theme.of(context).textTheme.headline6.copyWith(color: AppColors.canopy),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                                    child: Container(
+                                      width: 32,
+                                      height: 32,
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.white,
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          'to'.i18n,
+                                          style:
+                                              Theme.of(context).textTheme.subtitle1.copyWith(color: AppColors.primary),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Flexible(
+                                    child: Text(
+                                      'Citizen'.i18n,
+                                      style: Theme.of(context).textTheme.headline6.copyWith(color: AppColors.canopy),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                FlatButton(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                  color: AppColors.springGreen,
+                                  padding: const EdgeInsets.all(8.0),
+                                  onPressed: () {},
+                                  child: Text(
+                                    'View your progress'.i18n,
+                                    style: Theme.of(context).textTheme.subtitle2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
           const SizedBox(height: 16.0),
           CardListTile(leadingIcon: Icons.settings_outlined, title: 'Settings'.i18n, route: Routes.settings),
           const SizedBox(height: 8.0),
