c7587f8f5a1e753acc9a9159828f9071dc0dc753
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-02-21 21:42:16 -0500

    Fixed broken build

--- lib/i18n/set_currency.i18n.dart
+++ lib/i18n/set_currency.i18n.dart
@@ -1,7 +1,7 @@
 import 'package:i18n_extension/i18n_extension.dart';
 
 extension Localization on String {
-  static var _t = Translations.byLocale("en_us") +
+  static final _t = Translations.byLocale("en_us") +
       {
         "es_es": {
           "Select Currency": "Selecionar moneda",
