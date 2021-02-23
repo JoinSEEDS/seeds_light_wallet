5a709be7dbcd6a317e6ff3a4e7edd0b46cf983ed
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-02-23 18:41:15 -0500

    Full page all the things

--- lib/v2/screens/explore/explore_screen.dart
+++ lib/v2/screens/explore/explore_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:seeds/constants/app_colors.dart';
 import 'package:seeds/providers/notifiers/settings_notifier.dart';
 import 'package:seeds/v2/components/full_page_error_indicator.dart';
-import 'package:seeds/v2/components/loading_indicator.dart';
+import 'package:seeds/v2/components/full_page_loading_indicator.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
 import 'package:seeds/v2/screens/explore/components/explore_info_card.dart';
 import 'package:seeds/v2/screens/explore/interactor/explore_bloc.dart';
@@ -27,7 +27,7 @@ class ExploreScreen extends StatelessWidget {
                 case PageState.initial:
                   return const SizedBox.shrink();
                 case PageState.loading:
-                  return Container(child: const Center(child: SeedsLoadingIndicator()));
+                  return const FullPageLoadingIndicator();
                 case PageState.failure:
                   return const FullPageErrorIndicator();
                 case PageState.success:
