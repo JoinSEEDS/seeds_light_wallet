cbf15e448234913d932459994cf67978ee916902
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-02-23 18:38:54 -0500

    Error state+

--- lib/v2/screens/explore/explore_screen.dart
+++ lib/v2/screens/explore/explore_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:seeds/constants/app_colors.dart';
-import 'package:seeds/design/app_theme.dart';
 import 'package:seeds/providers/notifiers/settings_notifier.dart';
 import 'package:seeds/v2/components/full_page_error_indicator.dart';
 import 'package:seeds/v2/components/loading_indicator.dart';
