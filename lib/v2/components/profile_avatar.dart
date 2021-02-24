edeb290bc401f51dec7bad2c8363caa5e512576c
Raul Urtecho <raul.urtecho512@gmail.com>
2021-02-23 21:19:52 -0600

    created

---
+++ lib/v2/components/profile_avatar.dart
@@ -0,0 +1,55 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:seeds/constants/app_colors.dart';
+import 'package:seeds/design/app_theme.dart';
+
+/// PROFILE AVATAR
+///
+/// This class works with http, svg images or just returns a short name
+/// with a colored background
+class ProfileAvatar extends StatelessWidget {
+  final double size;
+  final String image;
+  final String nickname;
+  final String account;
+
+  const ProfileAvatar({
+    this.size = 100,
+    @required this.image,
+    @required this.nickname,
+    @required this.account,
+  })  : assert(image != null),
+        assert(nickname != null),
+        assert(account != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(50),
+      child: Container(
+        width: size,
+        height: size,
+        child: _buildContent(context),
+      ),
+    );
+  }
+
+  Widget _buildContent(BuildContext context) {
+    if (image.startsWith('http')) {
+      return CachedNetworkImage(imageUrl: image, fit: BoxFit.cover);
+    } else if (image.endsWith('.svg')) {
+      return SvgPicture.asset(image, fit: BoxFit.scaleDown);
+    } else {
+      var shortName = nickname.isNotEmpty && nickname != 'Seeds Account' && nickname != 'Telos Account'
+          ? nickname.substring(0, 2).toUpperCase()
+          : account.substring(0, 2).toUpperCase();
+
+      return Container(
+        color: AppColors.getColorByString(shortName),
+        alignment: Alignment.center,
+        child: Text(shortName, style: Theme.of(context).textTheme.subtitle1HighEmphasis),
+      );
+    }
+  }
+}
--- lib/v2/screens/profile/profile_screen.dart
+++ lib/v2/screens/profile/profile_screen.dart
@@ -4,8 +4,8 @@ import 'package:seeds/v2/components/divider_jungle.dart';
 import 'package:seeds/v2/screens/profile/components/profile_bottom.dart';
 import 'package:seeds/v2/screens/profile/components/profile_header.dart';
 import 'package:seeds/v2/screens/profile/components/profile_middle.dart';
-import 'package:seeds/v2/screens/profile/interactor/profile_bloc.dart';
-import 'package:seeds/v2/screens/profile/interactor/viewmodels/events.dart';
+import 'package:seeds/v2/screens/profile/interactor/viewmodels/bloc.dart';
+import 'package:seeds/v2/domain-shared/page_state.dart';
 
 /// PROFILE SCREEN
 class ProfileScreen extends StatelessWidget {
@@ -14,15 +14,28 @@ class ProfileScreen extends StatelessWidget {
     return BlocProvider(
       create: (context) => ProfileBloc()..add(LoadProfile(userName: 'raul11111111')),
       child: Scaffold(
-        body: ListView(
-          children: <Widget>[
-            const ProfileHeader(),
-            const DividerJungle(thickness: 2),
-            const ProfileMiddle(),
-            const DividerJungle(thickness: 2),
-            const ProfileBottom(),
-          ],
-        ),
+        body: BlocBuilder<ProfileBloc, ProfileState>(builder: (context, state) {
+          switch (state.pageState) {
+            case PageState.initial:
+              return const SizedBox.shrink();
+            case PageState.loading:
+              return Container(child: const Center(child: CircularProgressIndicator()));
+            case PageState.failure:
+              return Container(child: const Center(child: Text('Error:')));
+            case PageState.success:
+              return ListView(
+                children: <Widget>[
+                  const ProfileHeader(),
+                  const DividerJungle(thickness: 2),
+                  const ProfileMiddle(),
+                  const DividerJungle(thickness: 2),
+                  const ProfileBottom(),
+                ],
+              );
+            default:
+              return const SizedBox.shrink();
+          }
+        }),
       ),
     );
   }
