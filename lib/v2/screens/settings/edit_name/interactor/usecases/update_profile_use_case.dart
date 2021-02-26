a5d9dbdd63255f72eb9df8124a7f658c95fcff6a
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-02-25 22:30:53 -0500

    d

--- lib/v2/screens/settings/edit_name/interactor/usecases/update_profile_use_case.dart
+++ lib/v2/screens/settings/edit_name/interactor/usecases/update_profile_use_case.dart
@@ -5,10 +5,10 @@ import 'package:seeds/v2/datasource/remote/api/profile_repository.dart';
 export 'package:async/src/result/result.dart';
 
 class UpdateProfileUseCase {
-  final ProfileRepository _eosRepository = ProfileRepository();
+  final ProfileRepository _profileRepository = ProfileRepository();
 
   Future<Result> run({@required String name, String accountName, String privateKey, String nodeEndpoint}) {
-    return _eosRepository.updateProfile(
+    return _profileRepository.updateProfile(
       nickname: name,
       image: '',
       story: '',
