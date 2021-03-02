09bd7adc803abcb658156ea118b8ab23529f7431
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-03-01 21:52:12 -0500

    Check key use case

--- lib/v2/screens/import_key/interactor/import_key_bloc.dart
+++ lib/v2/screens/import_key/interactor/import_key_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
-import 'package:eosdart_ecc/eosdart_ecc.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
 import 'package:seeds/v2/screens/import_key/interactor/mappers/import_key_state_mapper.dart';
+import 'package:seeds/v2/screens/import_key/interactor/usecases/check_private_key_use_case.dart';
 import 'package:seeds/v2/screens/import_key/interactor/usecases/import_key_use_case.dart';
 import 'package:seeds/v2/screens/import_key/interactor/viewmodels/import_key_events.dart';
 import 'package:seeds/v2/screens/import_key/interactor/viewmodels/import_key_state.dart';
@@ -15,14 +15,7 @@ class ImportKeyBloc extends Bloc<ImportKeyEvent, ImportKeyState> {
     if (event is FindAccountByKey) {
       yield state.copyWith(pageState: PageState.loading);
 
-      var publicKey;
-      try {
-        EOSPrivateKey eosPrivateKey = EOSPrivateKey.fromString(event.userKey);
-        EOSPublicKey eosPublicKey = eosPrivateKey.toEOSPublicKey();
-        publicKey = eosPublicKey.toString();
-      } catch (e) {
-        print("Error EOSPrivateKey.fromString");
-      }
+      var publicKey = CheckPrivateKeyUseCase().isKeyValid(event.userKey);
 
       if (publicKey == null || publicKey.isEmpty) {
         yield state.copyWith(pageState: PageState.failure, errorMessage: "Private key is not valid");
