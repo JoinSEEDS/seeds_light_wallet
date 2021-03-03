6e129f04a58c73e776d94b85a569b45dd1149d71
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-03-02 22:45:07 -0500

    V2 Login: 559: Import key (#557)
    * Login Basic work
    
    * UI work and bloc work
    
    * Try to fetch accounts
    
    * Search on change
    
    * Search on change+
    
    * Get account works
    
    * Response works
    
    * Merge develop and handle errors
    
    * Check key use case
    
    * Added cells and pop
    
    * Added cells and pop+
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup
    
    * move bloc provider
    
    * RTemove app bar
    
    * Fix missing account

---
+++ lib/v2/datasource/remote/api/key_accounts_repository.dart
@@ -0,0 +1,37 @@
+import 'package:async/async.dart';
+import 'package:http/http.dart' as http;
+import 'package:seeds/v2/datasource/remote/api/network_repository.dart';
+
+export 'package:async/src/result/error.dart';
+export 'package:async/src/result/result.dart';
+
+class KeyAccountsRepository extends NetworkRepository {
+  Future<Result<dynamic>> getKeyAccountsMongo(String publicKey) {
+    print('[http] get seeds getKeyAccountsMongo ');
+
+    var body = '''
+        {
+          "collection": "pub_keys",
+          "query": {
+            "public_key": "$publicKey"\n    
+          },
+          "limit": 100
+        }
+        ''';
+
+    return http
+        .post(Uri.parse('https://mongo-api.hypha.earth/find'), headers: headers, body: body)
+        .then((http.Response response) => mapHttpResponse(response, (dynamic body) {
+              print('result: $body');
+
+              var items = List<Map<String, dynamic>>.from(body['items'])
+                  .where((item) => item['permission'] == 'active' || item['permission'] == 'owner');
+              var result = items.map<String>((item) => item['account']).toSet().toList();
+
+              result.sort();
+
+              return result;
+            }))
+        .catchError((dynamic error) => mapHttpError(error));
+  }
+}
---
+++ lib/v2/screens/import_key/interactor/import_key_bloc.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:seeds/v2/domain-shared/page_state.dart';
+import 'package:seeds/v2/screens/import_key/interactor/mappers/import_key_state_mapper.dart';
+import 'package:seeds/v2/screens/import_key/interactor/usecases/check_private_key_use_case.dart';
+import 'package:seeds/v2/screens/import_key/interactor/usecases/import_key_use_case.dart';
+import 'package:seeds/v2/screens/import_key/interactor/viewmodels/import_key_events.dart';
+import 'package:seeds/v2/screens/import_key/interactor/viewmodels/import_key_state.dart';
+
+/// --- BLOC
+class ImportKeyBloc extends Bloc<ImportKeyEvent, ImportKeyState> {
+  ImportKeyBloc() : super(ImportKeyState.initial());
+
+  @override
+  Stream<ImportKeyState> mapEventToState(ImportKeyEvent event) async* {
+    if (event is FindAccountByKey) {
+      yield state.copyWith(pageState: PageState.loading);
+
+      var publicKey = CheckPrivateKeyUseCase().isKeyValid(event.userKey);
+
+      if (publicKey == null || publicKey.isEmpty) {
+        yield state.copyWith(pageState: PageState.failure, errorMessage: "Private key is not valid");
+      } else {
+        var results = await ImportKeyUseCase().run(publicKey);
+        yield ImportKeyStateMapper().mapResultsToState(state, results);
+      }
+    }
+  }
+}
