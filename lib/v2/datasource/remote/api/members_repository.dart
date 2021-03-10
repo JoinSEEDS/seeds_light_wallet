6233e95cc9f4db3773109e8e4a984e5f0171e80d
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-03-10 16:36:49 -0500

    get member by accountName

--- lib/v2/datasource/remote/api/members_repository.dart
+++ lib/v2/datasource/remote/api/members_repository.dart
@@ -44,4 +44,28 @@ class MembersRepository extends NetworkRepository {
             }))
         .catchError((error) => mapHttpError(error));
   }
+
+  /// accountName must be greater than 2 or we return empty list of users.
+  /// This will return one account if found or null if not found.
+  Future<Result> getMemberByAccountName(String accountName) {
+    print('[http] getMemberByAccountName $accountName ');
+    assert(accountName.length > 2);
+
+    final membersURL = '$baseURL/v1/chain/get_table_rows';
+
+    var request =
+        '{"json":true,"code":"accts.seeds","scope":"accts.seeds","table":"users","table_key":"","lower_bound":" $accountName","upper_bound":" $accountName","index_position":1,"key_type":"i64","limit":"1","reverse":false,"show_payer":false}';
+
+    return http
+        .post(membersURL, headers: headers, body: request)
+        .then((http.Response response) => mapHttpResponse(response, (dynamic body) {
+      List<dynamic> allAccounts = body['rows'].toList();
+      if (allAccounts.isNotEmpty) {
+        return MemberModel.fromJson(allAccounts[0]);
+      } else {
+        return null;
+      }
+    }))
+        .catchError((error) => mapHttpError(error));
+  }
 }
