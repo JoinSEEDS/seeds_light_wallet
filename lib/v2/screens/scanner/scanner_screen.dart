e9fa71706cbea4fc8c3fb6829e3177e639470228
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-03-11 22:06:42 -0600

    Show loading

--- lib/v2/screens/scanner/scanner_screen.dart
+++ lib/v2/screens/scanner/scanner_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-import 'package:seeds/constants/app_colors.dart';
+import 'package:seeds/v2/components/seeds_qr_code_scanner_widget.dart';
 import 'package:seeds/v2/screens/scanner/interactor/scanner_bloc.dart';
 import 'package:seeds/v2/screens/scanner/interactor/viewmodels/scanner_events.dart';
 import 'package:seeds/v2/screens/scanner/interactor/viewmodels/scanner_state.dart';
@@ -46,46 +46,18 @@ class _ScannerScreenState extends State<ScannerScreen> {
 
   @override
   Widget build(BuildContext context) {
-
-    double width = MediaQuery. of(context). size. width;
-
     return BlocProvider(
       create: (BuildContext context) => widget._scannerBloc,
       child: BlocBuilder<ScannerBloc, ScannerState>(builder: (context, ScannerState state) {
-        return Center(
-          child: Container(
-            width: width * 0.9,
-            height: width * 0.9,
-            child: Stack(
-              children: <Widget>[
-                Center(
-                  child: Container(
-                    width: width * 0.9,
-                    height: width * 0.9,
-                    child: QRView(
-                      key: qrKey,
-                      onQRViewCreated: _onQRViewCreated,
-                      showNativeAlertDialog: true,
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Container(
-                    width: width * 0.9,
-                    height: width * 0.9,
-                    decoration: ShapeDecoration(
-                        shape: QrScannerOverlayShape(
-                            borderLength: width * 0.2,
-                            borderRadius: 30,
-                            borderWidth: 8,
-                            borderColor: AppColors.green1,
-                            cutOutSize: width * 0.9,
-                            overlayColor: AppColors.primary)),
-                  ),
-                ),
-              ],
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            SeedsQRCodeScannerWidget(
+              onQRViewCreated: _onQRViewCreated,
+              qrKey: qrKey,
             ),
-          ),
+            Center(child: buildStateView(state))
+          ],
         );
       }),
     );
@@ -128,5 +100,3 @@ class _ScannerScreenState extends State<ScannerScreen> {
     }
   }
 }
-
-
