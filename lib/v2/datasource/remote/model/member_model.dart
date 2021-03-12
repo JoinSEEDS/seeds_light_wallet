97329a1b41f2a5e799f0195e74432c3ea34614f1
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-03-11 20:43:03 -0600

    Fix add equatable

--- lib/v2/datasource/remote/model/member_model.dart
+++ lib/v2/datasource/remote/model/member_model.dart
@@ -1,9 +1,11 @@
-class MemberModel {
+import 'package:equatable/equatable.dart';
+
+class MemberModel extends Equatable {
   final String account;
   final String nickname;
   final String image;
 
-  MemberModel({this.account, this.nickname, this.image});
+  const MemberModel({this.account, this.nickname, this.image});
 
   factory MemberModel.fromJson(Map<String, dynamic> json) {
     return MemberModel(
@@ -14,13 +16,5 @@ class MemberModel {
   }
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-          other is MemberModel &&
-              account == other.account &&
-              nickname == other.nickname &&
-              image == other.image;
-
-  @override
-  int get hashCode => super.hashCode;
+  List<Object> get props => [account, nickname, image];
 }
