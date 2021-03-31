f3c099d3aa464e886553a6601dc13f54708c5e59
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-03-31 10:46:01 -0600

    Edit name call fixed (#626)

--- lib/v2/screens/profile_screens/edit_name/interactor/usecases/update_profile_use_case.dart
+++ lib/v2/screens/profile_screens/edit_name/interactor/usecases/update_profile_use_case.dart
@@ -2,22 +2,22 @@ import 'package:async/async.dart';
 import 'package:meta/meta.dart';
 import 'package:seeds/v2/datasource/local/settings_storage.dart';
 import 'package:seeds/v2/datasource/remote/api/profile_repository.dart';
+import 'package:seeds/v2/datasource/remote/model/profile_model.dart';
 
 export 'package:async/src/result/result.dart';
 
 class UpdateProfileUseCase {
   final ProfileRepository _profileRepository = ProfileRepository();
-  final accountName = settingsStorage.accountName;
 
-  Future<Result> run({@required String name}) {
+  Future<Result> run({@required String newName, @required ProfileModel profile}) {
     return _profileRepository.updateProfile(
-      accountName: accountName,
-      nickname: name,
-      image: '',
-      story: '',
-      roles: '',
-      skills: '',
-      interests: '',
+      accountName: settingsStorage.accountName,
+      nickname: newName,
+      image: profile.image,
+      story: profile.story,
+      roles: profile.roles,
+      skills: profile.skills,
+      interests: profile.interests,
     );
   }
 }
