e3ba9384f1823fa311a836de0f9daf890ee0c806
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-03-31 10:46:41 -0600

    Update profile image logic added (#614)
    * Update profile image logic added
    
    * fix
    
    * Fix image settings

---
+++ lib/v2/screens/profile_screens/profile/interactor/mappers/update_profile_image_state_mapper.dart
@@ -0,0 +1,20 @@
+import 'package:seeds/v2/domain-shared/page_state.dart';
+import 'package:seeds/v2/domain-shared/result_to_state_mapper.dart';
+
+import 'package:seeds/v2/screens/profile_screens/profile/interactor/usecases/get_profile_use_case.dart';
+import 'package:seeds/v2/datasource/remote/model/transaction_response.dart';
+import 'package:seeds/v2/screens/profile_screens/profile/interactor/viewmodels/profile_state.dart';
+
+class UpdateProfileImageStateMapper extends StateMapper {
+  ProfileState mapResultToState(ProfileState currentState, Result result) {
+    if (result.isError) {
+      return currentState.copyWith(pageState: PageState.failure, errorMessage: result.asError.error.toString());
+    } else {
+      final TransactionResponse res = result.asValue.value;
+      return currentState.copyWith(
+        pageState: PageState.success,
+        profile: currentState.profile.copyWith(image: res.data.image),
+      );
+    }
+  }
+}
