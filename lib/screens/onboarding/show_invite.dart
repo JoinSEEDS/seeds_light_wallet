95d66ee1c06cfb53bac8eb235371b20475f96bfc
nik <nheger@gmail.com>
2021-04-01 23:29:19 +0800

    remove intro_views_flutter package dependency

--- lib/screens/onboarding/show_invite.dart
+++ lib/screens/onboarding/show_invite.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intro_views_flutter/intro_views_flutter.dart';
-import 'package:seeds/providers/services/navigation_service.dart';
-import 'package:seeds/screens/onboarding/onboarding_view_model.dart';
-import 'package:seeds/i18n/show_invite.i18n.dart';
+// import 'package:seeds/providers/services/navigation_service.dart';
+// import 'package:seeds/i18n/show_invite.i18n.dart';
 
 class ShowInviteArguments {
   final String inviterAccountName;
@@ -21,39 +19,41 @@ class ShowInvite extends StatelessWidget {
     final inviterAccountName = arguments.inviterAccountName;
     final inviteSecret = arguments.inviteSecret;
 
-    return Builder(
-      builder: (context) => IntroViewsFlutter(
-        [
-          OnboardingViewModel(
-            bubble: Icons.done,
-            mainImage: 'assets/images/onboarding/onboarding5.png',
-            body: 'Accept your invite to create a new account and join SEEDS'.i18n,
-            title: 'You are invited by %s'.i18n.fill(["$inviterAccountName"]),
-          ),
-        ],
-        key: new UniqueKey(),
-        onTapDoneButton: () async {
-          NavigationService.of(context)
-              .navigateTo(Routes.createAccount, inviteSecret, true);
-        },
-        doneButtonPersist: true,
-        doneText: Text(
-          "ACCEPT".i18n,
-          style: TextStyle(
-            color: Colors.white,
-            fontFamily: "worksans",
-            fontSize: 24,
-            fontWeight: FontWeight.w800,
-          ),
-        ),
-        showNextButton: true,
-        showBackButton: true,
-        pageButtonTextStyles: TextStyle(
-          fontFamily: "worksans",
-          fontSize: 18.0,
-          fontWeight: FontWeight.w700,
-        ),
-      ),
-    );
+    // TODO migrate to V2 and fix
+    return Text("Accept your invite to create a new account and join SEEDS'.i18n");
+    // return Builder(
+    //   builder: (context) => IntroViewsFlutter(
+    //     [
+    //       OnboardingViewModel(
+    //         bubble: Icons.done,
+    //         mainImage: 'assets/images/onboarding/onboarding5.png',
+    //         body: 'Accept your invite to create a new account and join SEEDS'.i18n,
+    //         title: 'You are invited by %s'.i18n.fill(["$inviterAccountName"]),
+    //       ),
+    //     ],
+    //     key: new UniqueKey(),
+    //     onTapDoneButton: () async {
+    //       NavigationService.of(context)
+    //           .navigateTo(Routes.createAccount, inviteSecret, true);
+    //     },
+    //     doneButtonPersist: true,
+    //     doneText: Text(
+    //       "ACCEPT".i18n,
+    //       style: TextStyle(
+    //         color: Colors.white,
+    //         fontFamily: "worksans",
+    //         fontSize: 24,
+    //         fontWeight: FontWeight.w800,
+    //       ),
+    //     ),
+    //     showNextButton: true,
+    //     showBackButton: true,
+    //     pageButtonTextStyles: TextStyle(
+    //       fontFamily: "worksans",
+    //       fontSize: 18.0,
+    //       fontWeight: FontWeight.w700,
+    //     ),
+    //   ),
+    // );
   }
 }
