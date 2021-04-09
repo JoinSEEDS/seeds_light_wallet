76ccdd2c59c746cd8aa10b7b830ad4b313dd6ca9
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-04-09 13:18:46 -0400

    add files

--- lib/providers/services/navigation_service.dart
+++ lib/providers/services/navigation_service.dart
@@ -24,6 +24,7 @@ import 'package:seeds/screens/app/wallet/receive_qr.dart';
 import 'package:seeds/screens/app/wallet/transfer/transfer.dart';
 import 'package:seeds/screens/app/wallet/transfer/transfer_form.dart';
 import 'package:seeds/screens/onboarding/join_process.dart';
+import 'package:seeds/v2/onBoarding_flow/display_name/display_name.dart';
 import 'package:seeds/v2/screens/explore/explore_screen.dart';
 import 'package:seeds/v2/screens/import_key/import_key_screen.dart';
 import 'package:seeds/v2/screens/login/login_screen.dart';
@@ -99,6 +100,7 @@ class NavigationService {
     Routes.joinProcess: (_) => JoinProcess(),
     Routes.login: (_) => LoginScreen(),
     Routes.importKey: (_) => const ImportKeyScreen(),
+    //Routes.createAccount: () => DisplayName(),
     // Routes.importAccount: (_) => ImportAccount(),
     // Routes.createAccount: (args) => CreateAccount(args),
     // Routes.showInvite: (args) => ShowInvite(args),
---
+++ lib/v2/onBoarding_flow/display_name/display_name.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DisplayName extends StatefulWidget {
+  @override
+  _DisplayNameState createState() => _DisplayNameState();
+}
+
+class _DisplayNameState  extends State<DisplayName> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("");
+  }
+}
