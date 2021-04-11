14a06c9d7ab766d0ab556b9da8ae151784e84cb2
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-04-11 13:28:15 -0400

    remove route

--- lib/v2/screens/login/login_screen.dart
+++ lib/v2/screens/login/login_screen.dart
@@ -39,9 +39,7 @@ class LoginScreen extends StatelessWidget {
                   height: 10,
                 ),
                 FlatButtonLong(
-                  onPressed: () {
-                    NavigationService.of(context).navigateTo(Routes.createDisplayName);
-                  },
+                  onPressed: () {},
                   title: "Claim invite code".i18n,
                 ),
                 const SizedBox(
