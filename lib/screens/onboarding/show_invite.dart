3436fa7d04314735db2898d408e90bdbebe7d4de
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-04-12 16:01:08 -0600

    Fix master merge issues

--- lib/screens/onboarding/show_invite.dart
+++ lib/screens/onboarding/show_invite.dart
@@ -21,38 +21,38 @@ class ShowInvite extends StatelessWidget {
     final inviteSecret = arguments.inviteSecret;
 
     return Text("Accept your invite to create a new account and join SEEDS'.i18n");
-    return Builder(
-      builder: (context) => IntroViewsFlutter(
-        [
-          OnboardingViewModel(
-            bubble: Icons.done,
-            mainImage: 'assets/images/onboarding/onboarding5.png',
-            body: 'Accept your invite to create a new account and join SEEDS'.i18n,
-            title: 'You are invited by %s'.i18n.fill(["$inviterAccountName"]),
-          ),
-        ],
-        key: new UniqueKey(),
-        onTapDoneButton: () async {
-          NavigationService.of(context).navigateTo(Routes.createAccount, inviteSecret, true);
-        },
-        doneButtonPersist: true,
-        doneText: Text(
-          "ACCEPT".i18n,
-          style: TextStyle(
-            color: Colors.white,
-            fontFamily: "worksans",
-            fontSize: 24,
-            fontWeight: FontWeight.w800,
-          ),
-        ),
-        showNextButton: true,
-        showBackButton: true,
-        pageButtonTextStyles: TextStyle(
-          fontFamily: "worksans",
-          fontSize: 18.0,
-          fontWeight: FontWeight.w700,
-        ),
-      ),
-    );
+    // return Builder(
+    //   builder: (context) => IntroViewsFlutter(
+    //     [
+    //       OnboardingViewModel(
+    //         bubble: Icons.done,
+    //         mainImage: 'assets/images/onboarding/onboarding5.png',
+    //         body: 'Accept your invite to create a new account and join SEEDS'.i18n,
+    //         title: 'You are invited by %s'.i18n.fill(["$inviterAccountName"]),
+    //       ),
+    //     ],
+    //     key: new UniqueKey(),
+    //     onTapDoneButton: () async {
+    //       NavigationService.of(context).navigateTo(Routes.createAccount, inviteSecret, true);
+    //     },
+    //     doneButtonPersist: true,
+    //     doneText: Text(
+    //       "ACCEPT".i18n,
+    //       style: TextStyle(
+    //         color: Colors.white,
+    //         fontFamily: "worksans",
+    //         fontSize: 24,
+    //         fontWeight: FontWeight.w800,
+    //       ),
+    //     ),
+    //     showNextButton: true,
+    //     showBackButton: true,
+    //     pageButtonTextStyles: TextStyle(
+    //       fontFamily: "worksans",
+    //       fontSize: 18.0,
+    //       fontWeight: FontWeight.w700,
+    //     ),
+    //   ),
+    // );
   }
 }
