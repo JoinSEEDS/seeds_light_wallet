f15997b58181180c99b4e3557283c90cbca1e574
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-04-13 15:03:46 -0400

    UI for addin phone number

--- lib/v2/screens/add_phone_number/add_phone_number.dart
+++ lib/v2/screens/add_phone_number/add_phone_number.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:seeds/v2/components/flat_button_long.dart';
 import 'package:seeds/v2/components/text_form_field_custom.dart';
+import 'package:seeds/design/app_theme.dart';
 
 /// Login SCREEN
 class AddPhoneNumberScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-        body: Column(
-        children: [
-          TextFormFieldCustom()
-        ]));}}
+        appBar: AppBar(),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(children: [
+            const TextFormFieldCustom(labelText: "Add Phone Number"),
+            Expanded(
+                child: Text("Note: Your phone number is encrypted and never shared with anyone.",
+                    style: Theme.of(context).textTheme.subtitle2OpacityEmphasis)),
+            FlatButtonLong(
+              title: "Next",
+              onPressed: () {},
+            ),
+            TextButton(onPressed: () {}, child: Text("Skip for now", style: Theme.of(context).textTheme.button))
+          ]),
+        ));
+  }
+}
