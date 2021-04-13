5b92b324a3aab7d4119b0866c7a2c5223c19bc65
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-04-13 15:32:21 -0500

    Issue 536 enter display name UI (#664)
    * add files
    
    * add route
    
    * Display name UI
    
    * remove route
    
    * fix import
    
    * merge master
    
    * fix master

---
+++ lib/v2/screens/display_name/interactor/viewmodels/display_name_state.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:seeds/v2/datasource/remote/model/profile_model.dart';
+import 'package:seeds/v2/domain-shared/page_state.dart';
+
+class DisplayNameState extends Equatable {
+  final PageState pageState;
+  final String errorMessage;
+  final String privateKey;
+
+  const DisplayNameState({@required this.pageState, this.errorMessage, this.privateKey});
+
+  @override
+  List<Object> get props => [pageState];
+
+  DisplayNameState copyWith({
+    PageState pageState,
+    String errorMessage,
+    List<ProfileModel> accounts,
+    String privateKey,
+  }) {
+    return DisplayNameState(
+      pageState: pageState ?? this.pageState,
+      errorMessage: errorMessage ?? this.errorMessage,
+      privateKey: privateKey ?? this.privateKey,
+    );
+  }
+
+  factory DisplayNameState.initial() {
+    return const DisplayNameState(
+      pageState: PageState.initial,
+      errorMessage: null,
+      privateKey: null,
+    );
+  }
+}
