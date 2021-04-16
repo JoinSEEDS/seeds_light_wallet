c054fc4a9211eedebe9e0fa7363109818def4834
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-04-15 22:25:22 -0600

    Fix gabriels sneaky commit

--- lib/v2/screens/display_name/interactor/viewmodels/display_name_state.dart
+++ lib/v2/screens/display_name/interactor/viewmodels/display_name_state.dart
@@ -1,23 +1,21 @@
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
-import 'package:seeds/v2/datasource/remote/model/profile_model.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
 
 class DisplayNameState extends Equatable {
-  final PageState pageState;
-  final String errorMessage;
-  final String privateKey;
+  final PageState? pageState;
+  final String? errorMessage;
+  final String? privateKey;
 
   const DisplayNameState({@required this.pageState, this.errorMessage, this.privateKey});
 
   @override
-  List<Object> get props => [pageState];
+  List<Object?> get props => [pageState, errorMessage, privateKey];
 
   DisplayNameState copyWith({
-    PageState pageState,
-    String errorMessage,
-    List<ProfileModel> accounts,
-    String privateKey,
+    PageState? pageState,
+    String? errorMessage,
+    String? privateKey,
   }) {
     return DisplayNameState(
       pageState: pageState ?? this.pageState,
