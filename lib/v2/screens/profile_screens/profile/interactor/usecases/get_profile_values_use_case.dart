3b618027a50e2b1f0c9a57dd16ab83c714e25693
Raul Urtecho <raul.urtecho512@gmail.com>
2021-04-18 17:22:19 -0500

    fix

--- lib/v2/screens/profile_screens/profile/interactor/usecases/get_profile_values_use_case.dart
+++ lib/v2/screens/profile_screens/profile/interactor/usecases/get_profile_values_use_case.dart
@@ -8,7 +8,7 @@ class GetProfileValuesUseCase {
   Future<List<Result>> run() {
     var futures = [
       _profileRepository.getProfile(settingsStorage.accountName),
-      _profileRepository.getScore('illumination'),
+      _profileRepository.getScore(settingsStorage.accountName),
     ];
     return Future.wait(futures);
   }
