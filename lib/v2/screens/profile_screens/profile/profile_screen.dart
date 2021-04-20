6b34bd6bdcabfd9665ad186c466983b6caf36cd5
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-04-20 13:50:16 -0500

    Profile appBar added (#676)
    * Profile appBar added
    
    * Profile model and Scanner icon fixed
    
    * Fix in v1 getProfile
    
    * handle functions added to profile model

--- lib/v2/screens/profile_screens/profile/profile_screen.dart
+++ lib/v2/screens/profile_screens/profile/profile_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:seeds/providers/services/navigation_service.dart';
 import 'package:seeds/v2/components/divider_jungle.dart';
 import 'package:seeds/v2/components/full_page_error_indicator.dart';
 import 'package:seeds/v2/components/full_page_loading_indicator.dart';
-
 import 'package:seeds/v2/domain-shared/page_state.dart';
 import 'package:seeds/v2/screens/profile_screens/profile/components/profile_bottom.dart';
 import 'package:seeds/v2/screens/profile_screens/profile/components/profile_header.dart';
@@ -27,14 +28,25 @@ class ProfileScreen extends StatelessWidget {
               case PageState.failure:
                 return const FullPageErrorIndicator();
               case PageState.success:
-                return ListView(
-                  children: <Widget>[
-                    const ProfileHeader(),
-                    const DividerJungle(thickness: 2),
-                    const ProfileMiddle(),
-                    const DividerJungle(thickness: 2),
-                    const ProfileBottom(),
-                  ],
+                return Scaffold(
+                  appBar: AppBar(
+                    title: Text(state.profile!.account, style: Theme.of(context).textTheme.headline6),
+                    actions: [
+                      IconButton(
+                        icon: SvgPicture.asset('assets/images/wallet/app_bar/scan_qr_code_icon.svg'),
+                        onPressed: () => NavigationService.of(context).navigateTo(Routes.scanQRCode),
+                      ),
+                    ],
+                  ),
+                  body: ListView(
+                    children: <Widget>[
+                      const ProfileHeader(),
+                      const DividerJungle(thickness: 2),
+                      const ProfileMiddle(),
+                      const DividerJungle(thickness: 2),
+                      const ProfileBottom(),
+                    ],
+                  ),
                 );
               default:
                 return const SizedBox.shrink();
