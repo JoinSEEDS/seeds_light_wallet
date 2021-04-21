d1229ef2c84adc26d8dbd07c80033dc33e3ec241
nik <nheger@gmail.com>
2021-04-21 19:59:06 +0800

    restore transactions cache old code

--- lib/providers/notifiers/transactions_notifier.dart
+++ lib/providers/notifiers/transactions_notifier.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 import 'package:seeds/models/models.dart';
 import 'package:seeds/providers/notifiers/auth_notifier.dart';
 import 'package:seeds/providers/services/http_service.dart';
+import 'package:seeds/utils/extensions/SafeHive.dart';
 
 
 class TransactionsNotifier extends ChangeNotifier {
   List<TransactionModel> transactions;
-
-  static List<TransactionModel> transactionsCache = [];
 
   HttpService _http;
 
@@ -19,29 +19,36 @@ class TransactionsNotifier extends ChangeNotifier {
     _http = http;
   }
 
+  static const cacheName = "transactions147";
+
+  Future fetchTransactionsCache() async {
+    Box cacheTransactions =
+        await SafeHive.safeOpenBox<TransactionModel>(cacheName);
+    if (cacheTransactions != null && cacheTransactions.isNotEmpty) {
+      transactions = cacheTransactions.values.toList();
+      notifyListeners();
+    }
+  }
 
   Future refreshTransactions() async {
+    Box cacheTransactions =
+        await SafeHive.safeOpenBox<TransactionModel>(cacheName);
 
-    int block = 0;
-    if (transactionsCache.length > 0) {
-      block = transactionsCache[0].blockNumber;
+    List<TransactionModel> actualTransactions = await _http.getTransactions();
+
+    if (actualTransactions.length > 0) {
+      await cacheTransactions.clear();
+      await cacheTransactions.addAll(actualTransactions);
     }
 
-    List<TransactionModel> newTransactions = await _http.getTransactionsMongo(blockHeight: block);
-
-    transactions = newTransactions + transactionsCache;
-
-    if (transactions.length > 300) {
-      transactions.sublist(0, 300);
-    }
-
-    transactionsCache = transactions;
+    transactions = cacheTransactions.values.toList();
 
     notifyListeners();
   }
 
+
   void logout() {
-    transactionsCache = [];
+    Hive.deleteBoxFromDisk(cacheName);
     transactions = null;
   }
 
