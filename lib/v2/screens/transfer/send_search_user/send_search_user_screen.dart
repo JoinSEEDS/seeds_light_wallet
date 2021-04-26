9d6ad81b331cb122f66996857b22a75404f48d12
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-04-26 14:00:02 -0600

    Pr comments

--- lib/v2/screens/transfer/send_search_user/send_search_user_screen.dart
+++ lib/v2/screens/transfer/send_search_user/send_search_user_screen.dart
@@ -3,39 +3,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/v2/components/search_user/search_user_widget.dart';
 import 'package:seeds/v2/datasource/remote/model/member_model.dart';
 import 'package:seeds/v2/screens/transfer/send_search_user/interactor/send_search_user_bloc.dart';
-import 'package:seeds/v2/screens/transfer/send_search_user/interactor/viewmodels/send_search_user_state.dart';
 
-/// SendScannerScreen SCREEN
+/// SendSearchUserScreen SCREEN
 class SendSearchUserScreen extends StatefulWidget {
   @override
   _SendSearchUserScreenState createState() => _SendSearchUserScreenState();
 }
 
 class _SendSearchUserScreenState extends State<SendSearchUserScreen> {
-  late SearchUserWidget _searchUserWidget;
-  final _sendSearchUserPageBloc = SendSearchUserPageBloc();
-
-  @override
-  void initState() {
-    super.initState();
-    _searchUserWidget = SearchUserWidget(resultCallBack: onResult);
-  }
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider<SendSearchUserPageBloc>(
-        create: (_) => _sendSearchUserPageBloc,
-        child: BlocBuilder<SendSearchUserPageBloc, SendSearchUserPageState>(
-          builder: (context, SendSearchUserPageState state) {
-            return Scaffold(
-              appBar: AppBar(title: const Text("Send")),
-              body: _searchUserWidget,
-            );
-          },
+        create: (_) => SendSearchUserPageBloc(),
+        child: Scaffold(
+          appBar: AppBar(title: const Text("Send")),
+          body: SearchUserWidget(resultCallBack: onResult),
         ));
   }
 
   Future<void> onResult(MemberModel scanResult) async {
+    // NEXT PR
     // _sendSearchUserPageBloc.add(ExecuteScanResult(scanResult: scanResult));
   }
 }
