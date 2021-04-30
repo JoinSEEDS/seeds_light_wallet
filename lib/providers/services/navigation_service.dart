13e9a62dbe07740b250a826e9f389d3b07597bce
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-04-29 21:44:14 -0600

    Send Seeds, Enter amount page (#690)
    * Send data cleanup
    
    * Initial commit
    
    * Initial UI work
    
    * Add events and states
    
    * Moved to bloc builder
    
    * Moved to bloc builder
    
    * Send search user 4 (#689)
    
    * ENter amount widget extract
    
    * Fix build
    
    * Remove test data
    
    * Citizenship last values (#687)
    
    * first advance
    
    * second advance
    
    * Refered accounts logic added
    
    * UI part
    
    * new methods for getReferredAccounts and isResidentOrCitizen (#682)
    
    * ProfileStatus enum added (#691)
    
    * Fix master (#692)
    
    * More fixes (#693)
    
    * Add more UI work and next button
    
    Co-authored-by: Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
    Co-authored-by: NIK <nheger@gmail.com>
    
    Co-authored-by: Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
    Co-authored-by: NIK <nheger@gmail.com>

--- lib/providers/services/navigation_service.dart
+++ lib/providers/services/navigation_service.dart
@@ -38,6 +38,7 @@ import 'package:seeds/v2/screens/transfer/send_confirmation/send_confirmation_sc
 import 'package:seeds/v2/screens/transfer/send_scanner/send_scanner_screen.dart';
 import 'package:seeds/widgets/page_not_found.dart';
 import 'package:seeds/v2/screens/transfer/send_search_user/send_search_user_screen.dart';
+import 'package:seeds/v2/screens/transfer/send_enter_data/send_enter_data_screen.dart';
 
 class Routes {
   static final app = 'App';
@@ -49,6 +50,7 @@ class Routes {
   static final claimCode = 'ClaimCode';
   static final welcome = 'Welcome';
   static final transfer = 'Transfer';
+  static final sendEnterData = 'SendEnterData';
   static final invites = 'Invites';
   static final createInvite = 'CreateInvite';
   static final proposals = 'Proposals';
@@ -111,6 +113,7 @@ class NavigationService {
     Routes.app: (_) => const App(),
     Routes.transferForm: (args) => TransferForm(args),
     Routes.transfer: (_) => SendSearchUserScreen(),
+    Routes.sendEnterData: (_) => SendEnterDataScreen(),
     Routes.invites: (_) => Invites(),
     Routes.createInvite: (_) => CreateInvite(),
     Routes.proposals: (_) => Proposals(),
--- lib/v2/screens/transfer/send_search_user/send_search_user_screen.dart
+++ lib/v2/screens/transfer/send_search_user/send_search_user_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:seeds/v2/components/search_user/search_user_widget.dart';
 import 'package:seeds/v2/datasource/remote/model/member_model.dart';
+import 'package:seeds/providers/services/navigation_service.dart';
 
 /// SendSearchUserScreen SCREEN
 class SendSearchUserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future<void> onResult(MemberModel selectedUser) async {
+      print("onResult:" + selectedUser.account);
+      await NavigationService.of(context).navigateTo(Routes.sendEnterData, selectedUser);
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text("Send")),
       body: Container(padding: const EdgeInsets.all(16), child: SearchUserWidget(resultCallBack: onResult)),
     );
-  }
-
-  Future<void> onResult(MemberModel selectedUser) async {
-    print("onResult:" + selectedUser.account);
   }
 }
