05aa6024bd007f770a942fc4c1b132b5792cc80a
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-05-07 09:18:08 -0600

    Cleanup+

--- lib/v2/datasource/remote/firebase/firebase_database_guardians_repository.dart
+++ lib/v2/datasource/remote/firebase/firebase_database_guardians_repository.dart
@@ -27,7 +27,8 @@ class FirebaseDatabaseGuardiansRepository extends FirebaseDatabaseService {
   Stream<List<GuardianModel>> getGuardiansForUser(String userId) {
     return usersCollection.doc(userId).collection(GUARDIANS_COLLECTION_KEY).snapshots().asyncMap(
         (QuerySnapshot event) => event.docs
-            .map((QueryDocumentSnapshot e) => GuardianModel.fromMap(e.data()!))
-            .toList()); // ignore: unnecessary_non_null_assertion
+            .map(
+                (QueryDocumentSnapshot e) => GuardianModel.fromMap(e.data()!)) // ignore: unnecessary_non_null_assertion
+            .toList());
   }
 }
