dabdb388a8bf10f45928f067a3ab227e3cb99592
Raul Urtecho <raul.urtecho512@gmail.com>
2021-05-07 17:40:17 -0500

    Explore folder moved

--- lib/v2/screens/explore_screens/explore/interactor/mappers/explore_state_mapper.dart
+++ lib/v2/screens/explore_screens/explore/interactor/mappers/explore_state_mapper.dart
@@ -4,7 +4,7 @@ import 'package:seeds/v2/datasource/remote/model/voice_model_alliance.dart';
 import 'package:seeds/v2/datasource/remote/model/voice_model_campaign.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
 import 'package:seeds/v2/domain-shared/result_to_state_mapper.dart';
-import 'package:seeds/v2/screens/explore/interactor/viewmodels/explore_state.dart';
+import 'package:seeds/v2/screens/explore_screens/explore/interactor/viewmodels/explore_state.dart';
 
 class ExploreStateMapper extends StateMapper {
   ExploreState mapResultsToState(ExploreState currentState, List<Result> results) {
