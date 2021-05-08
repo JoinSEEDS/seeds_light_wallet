98a916df093b8d640e4a7f47a05fc31b309f25a8
diadem <igor@berlenko.com>
2021-05-08 15:13:47 +0800

    web support

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,27 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
--- lib/v2/datasource/remote/firebase/firebase_push_notification_service.dart
+++ lib/v2/datasource/remote/firebase/firebase_push_notification_service.dart
@@ -16,38 +16,46 @@ class PushNotificationService {
 
   Future initialise() async {
     if (!_initialized) {
-      // Requesting Permission
-      await requestingPermissionForIOS();
-      // If the application has been opened from a terminated state
-      await FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? message) {
-        if (message != null) {
-          print("FirebaseMessaging getInitialMessage: ${message}");
-        }
-      });
-      await _firebaseMessaging.subscribeToTopic("PUSH_RC");
+      try {
+        // Requesting Permission
+        await requestingPermissionForIOS();
+        // If the application has been opened from a terminated state
+        await FirebaseMessaging.instance
+            .getInitialMessage()
+            .then((RemoteMessage? message) {
+          if (message != null) {
+            print("FirebaseMessaging getInitialMessage: ${message}");
+          }
+        });
+        await _firebaseMessaging.subscribeToTopic("PUSH_RC");
 
-      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-        print("FirebaseMessaging onMessage -> " + message.data.toString());
-        // If remote config changed, set them to stale. And fetch new config
-        if (message.data.containsKey("CONFIG_STATE")) {
-          remoteConfigurations.refresh();
-        }
-      });
+        FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+          print("FirebaseMessaging onMessage -> " + message.data.toString());
+          // If remote config changed, set them to stale. And fetch new config
+          if (message.data.containsKey("CONFIG_STATE")) {
+            remoteConfigurations.refresh();
+          }
+        });
 
-      // When user press the push notification and the app is in background (not terminated).
-      FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) => onMessageOpenedApp(message));
-      // When app is in background or Terminated.
-      // It must not be an anonymous function. It must be a top-level function
-      // (e.g. not a class method which requires initialization).
-      FirebaseMessaging.onBackgroundMessage(backgroundMessageHandler);
-      
-      // For testing purposes print the Firebase Messaging token.
-      token = await _firebaseMessaging.getToken();
-      print('FirebaseMessaging token: $token');
+        // When user press the push notification and the app is in background (not terminated).
+        FirebaseMessaging.onMessageOpenedApp
+            .listen((RemoteMessage message) => onMessageOpenedApp(message));
+        // When app is in background or Terminated.
+        // It must not be an anonymous function. It must be a top-level function
+        // (e.g. not a class method which requires initialization).
+        FirebaseMessaging.onBackgroundMessage(backgroundMessageHandler);
 
-      _firebaseMessaging.onTokenRefresh.listen((newToken) => token = newToken);
+        // For testing purposes print the Firebase Messaging token.
+        token = await _firebaseMessaging.getToken();
+        print('FirebaseMessaging token: $token');
 
-      _initialized = true;
+        _firebaseMessaging.onTokenRefresh
+            .listen((newToken) => token = newToken);
+
+        _initialized = true;
+      } catch (err) {
+        print(err);
+      }
     }
   }
 
@@ -63,13 +71,15 @@ class PushNotificationService {
     );
     if (settings.authorizationStatus == AuthorizationStatus.authorized) {
       print('User granted permission');
-    } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
       print('User granted provisional permission');
     } else {
       print('User declined or has not accepted permission');
     }
 
-    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
       alert: true, // Required to display a heads up notification
       badge: true,
       sound: true,
