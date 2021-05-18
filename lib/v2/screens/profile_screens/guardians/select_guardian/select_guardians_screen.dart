ad829316af7716b0c3b19810c79ad968b7e5d863
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-05-17 20:36:31 -0600

    A little optimization

--- lib/v2/screens/profile_screens/guardians/select_guardian/select_guardians_screen.dart
+++ lib/v2/screens/profile_screens/guardians/select_guardian/select_guardians_screen.dart
@@ -6,6 +6,7 @@ import 'package:seeds/v2/datasource/remote/model/firebase_models/guardian_model.
 import 'package:seeds/v2/datasource/remote/model/member_model.dart';
 import 'package:seeds/v2/design/app_theme.dart';
 import 'package:seeds/v2/navigation/navigation_service.dart';
+import 'package:seeds/v2/screens/profile_screens/guardians/select_guardian/components/selected_guardians_widget.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/select_guardian/interactor/select_guardians_bloc.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/select_guardian/interactor/viewmodels/select_guardians_events.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/select_guardian/interactor/viewmodels/select_guardians_state.dart';
@@ -35,25 +36,8 @@ class SelectGuardiansScreen extends StatelessWidget {
                         Padding(
                           padding: const EdgeInsets.only(bottom: 4, left: 16, right: 16),
                           child: state.selectedGuardians.isEmpty
-                              ? Container()
-                              : Wrap(
-                                  children: state.selectedGuardians
-                                      .toList()
-                                      .reversed
-                                      .map((user) => Padding(
-                                            padding: const EdgeInsets.only(right: 4),
-                                            child: ActionChip(
-                                              label: Text((user.nickname == null || user.nickname?.isEmpty == true)
-                                                  ? user.account
-                                                  : user.nickname!),
-                                              avatar: const Icon(Icons.highlight_off),
-                                              onPressed: () {
-                                                BlocProvider.of<SelectGuardiansBloc>(context).add(OnUserRemoved(user));
-                                              },
-                                            ),
-                                          ))
-                                      .toList(),
-                                ),
+                              ? const SizedBox.shrink()
+                              : const SelectedGuardiansWidget(),
                         ),
                         Expanded(child: SearchUserWidget(resultCallBack: (MemberModel selectedUser) {
                           BlocProvider.of<SelectGuardiansBloc>(context).add(OnUserSelected(selectedUser));
