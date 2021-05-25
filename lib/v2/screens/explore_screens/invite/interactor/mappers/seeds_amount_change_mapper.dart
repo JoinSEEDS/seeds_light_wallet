1ef3564bef4dd3ce21e2f7ff8bd76996a6148076
Raul Urtecho <raul.urtecho512@gmail.com>
2021-05-25 15:09:52 -0500

    Invite screen part 4

--- lib/v2/screens/explore_screens/invite/interactor/mappers/seeds_amount_change_mapper.dart
+++ lib/v2/screens/explore_screens/invite/interactor/mappers/seeds_amount_change_mapper.dart
@@ -1,5 +1,6 @@
 import 'package:seeds/v2/blocs/rates/viewmodels/rates_state.dart';
 import 'package:seeds/v2/datasource/local/settings_storage.dart';
+import 'package:seeds/v2/datasource/remote/model/balance_model.dart';
 import 'package:seeds/v2/domain-shared/result_to_state_mapper.dart';
 import 'package:seeds/v2/screens/explore_screens/invite/interactor/viewmodels/invite_state.dart';
 import 'package:seeds/v2/utils/rate_states_extensions.dart';
@@ -8,6 +9,7 @@ class SeedsAmountChangeMapper extends StateMapper {
   InviteState mapResultToState(InviteState currentState, RatesState rateState, String quantity) {
     double parsedQuantity = double.tryParse(quantity) ?? 0;
     double currentAvailable = currentState.availableBalance?.quantity ?? 0;
+    double newBalance = currentAvailable - parsedQuantity;
     String? alertMessage = _handleAlertMessage(currentAvailable, parsedQuantity);
 
     return currentState.copyWith(
@@ -15,6 +17,7 @@ class SeedsAmountChangeMapper extends StateMapper {
       isCreateInviteButtonEnabled: alertMessage == null && parsedQuantity > 0,
       quantity: parsedQuantity,
       alertMessage: alertMessage,
+      availableBalance: BalanceModel(newBalance),
     );
   }
 
