ab60a073a89779602739f48374067acb0003e6ef
Raul Urtecho <raul.urtecho512@gmail.com>
2021-05-27 15:32:15 -0500

    Fix results mappers

--- lib/v2/screens/explore_screens/invite/interactor/mappers/create_invite_result_state_mapper.dart
+++ lib/v2/screens/explore_screens/invite/interactor/mappers/create_invite_result_state_mapper.dart
@@ -6,12 +6,18 @@ import 'package:seeds/v2/screens/explore_screens/invite/interactor/viewmodels/in
 class CreateInviteResultStateMapper extends StateMapper {
   InviteState mapResultsToState(InviteState currentState, List<Result> results) {
     if (areAllResultsError(results)) {
-      return currentState.copyWith(pageState: PageState.failure, errorMessage: 'Error creating invite');
+      // The 2 calls are error --> ransaction fail show snackbar fail
+      print('Error transaction hash not retrieved');
+      return currentState.copyWith(
+        pageState: PageState.success,
+        pageCommand: ShowTransactionFailSnackBar(),
+        isCreateInviteButtonEnabled: false,
+      );
     } else {
       print('CreateInviteResultStateMapper mapResultsToState length = ${results.length}');
+      // Check if the error is in the transaction
       results.retainWhere((Result element) => element.isValue);
       var values = results.map((Result element) => element.asValue!.value).toList();
-
       TransactionResponse? response = values.firstWhere((i) => i is TransactionResponse, orElse: () => null);
 
       if (response != null && response.transactionId.isNotEmpty) {
