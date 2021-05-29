5b65b753701a3e3e8ed35f9b9c319ad83ac4c3b5
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-05-29 17:53:44 -0600

    Loading

--- lib/v2/screens/profile_screens/guardians/invite_guardians/interactor/invite_guardians_bloc.dart
+++ lib/v2/screens/profile_screens/guardians/invite_guardians/interactor/invite_guardians_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:seeds/v2/datasource/remote/model/member_model.dart';
+import 'package:seeds/v2/domain-shared/page_state.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/guardians_tabs/interactor/mappers/init_guardians_state_mapper.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/invite_guardians/interactor/mappers/invite_guardian_state_mapper.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/invite_guardians/interactor/usecases/send_guardian_invite_use_case.dart';
@@ -12,7 +13,7 @@ class InviteGuardiansBloc extends Bloc<InviteGuardiansEvent, InviteGuardiansStat
   @override
   Stream<InviteGuardiansState> mapEventToState(InviteGuardiansEvent event) async* {
     if(event is OnSendInviteTapped) {
-      yield state
+      yield state.copyWith(pageState: PageState.loading);
       var result = await SendGuardianInviteUseCase().run(state.selectedGuardians);
       yield InviteGuardiansStateMapper().mapResultToState(state, result);
     } else if (event is InviteGuardianClearPageCommand) {
