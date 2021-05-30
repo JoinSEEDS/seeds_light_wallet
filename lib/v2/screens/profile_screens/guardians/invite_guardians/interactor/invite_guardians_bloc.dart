529aa0ddd93209a2e67aa4b675fbe96ff5cba7ef
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-05-29 19:50:09 -0600

    Fix imports

--- lib/v2/screens/profile_screens/guardians/invite_guardians/interactor/invite_guardians_bloc.dart
+++ lib/v2/screens/profile_screens/guardians/invite_guardians/interactor/invite_guardians_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:seeds/v2/datasource/remote/model/member_model.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
-import 'package:seeds/v2/screens/profile_screens/guardians/guardians_tabs/interactor/mappers/init_guardians_state_mapper.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/invite_guardians/interactor/mappers/invite_guardian_state_mapper.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/invite_guardians/interactor/usecases/send_guardian_invite_use_case.dart';
 import 'package:seeds/v2/screens/profile_screens/guardians/invite_guardians/interactor/viewmodel/invite_guardians_events.dart';
