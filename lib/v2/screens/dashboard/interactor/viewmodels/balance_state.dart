7e3afead8e4dde20e85c4dc84917a373d4ca8fd3
nik <nheger@gmail.com>
2021-05-31 17:09:59 +0800

    remove displayString method

--- lib/v2/screens/dashboard/interactor/viewmodels/balance_state.dart
+++ lib/v2/screens/dashboard/interactor/viewmodels/balance_state.dart
@@ -1,41 +1,31 @@
 import 'package:equatable/equatable.dart';
+import 'package:seeds/v2/datasource/remote/model/token_model.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
-import 'package:seeds/v2/datasource/local/models/token_model.dart';
-import 'package:seeds/v2/utils/double_extension.dart';
 
 class BalanceState extends Equatable {
   final PageState pageState;
   final TokenModel token;
   final String? error;
   final double? amount;
+  final String cardDisplayText;
   
 
-  const BalanceState({required this.pageState, required this.token, this.error, this.amount});
+  const BalanceState({required this.pageState, required this.token, this.error, this.amount, required this.cardDisplayText});
 
   @override
-  List<Object?> get props => [token, error, amount, pageState];
+  List<Object?> get props => [token, error, amount, pageState, cardDisplayText];
 
-  BalanceState copyWith({PageState? pageState, TokenModel? token, String? error, double? amount}) {    
+  BalanceState copyWith({PageState? pageState, TokenModel? token, String? error, double? amount, String? cardDisplayText}) {    
     return BalanceState(
       pageState: pageState ?? this.pageState,
       token: token ?? this.token,
       error: error ?? this.error,
       amount: amount ?? this.amount,
+      cardDisplayText: cardDisplayText ?? this.cardDisplayText
     );
   }
 
-  String displayString() {
-    switch (pageState) {
-      case PageState.initial: return "...";
-      case PageState.loading: return "...";
-      case PageState.failure: return "...";
-      case PageState.success: return amount!.seedsFormatted + " " + token.symbol;        
-      default:
-      return "...";
-    }
-  }
-
   factory BalanceState.initial(TokenModel token) {
-    return BalanceState(pageState: PageState.initial, token: token);
+    return BalanceState(pageState: PageState.initial, token: token, cardDisplayText: "...");
   }
 }
