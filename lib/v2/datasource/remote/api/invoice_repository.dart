0493399c8140d6c762aeefda6e7e66e91b1e6531
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-06-03 13:13:39 -0600

    Fix some issues

--- lib/v2/datasource/remote/api/invoice_repository.dart
+++ lib/v2/datasource/remote/api/invoice_repository.dart
@@ -1,43 +1,42 @@
+import 'package:async/async.dart';
+
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:dart_esr/dart_esr.dart' as esr;
 import 'package:seeds/v2/datasource/remote/api/eos_repository.dart';
-import 'package:seeds/v2/datasource/remote/api/network_repository.dart';
 import 'package:seeds/v2/datasource/remote/firebase/firebase_remote_config.dart';
 import 'package:seeds/v2/domain-shared/app_constants.dart';
-import 'package:async/async.dart';
 
-class InviteRepository extends NetworkRepository with EosRepository {
-  Future<Result> createInvoice({
+class InvoiceRepository extends EosRepository {
+  Future<Result<dynamic>> createInvoice({
     required double quantity,
     required String accountName,
     String? memo,
-  }) async {
-    print('[eos] create invite $accountName ($quantity)');
+  }) {
+    print('[ESR] create invite accountName: $accountName quantity: ($quantity)');
 
-    var auth = [esr.ESRConstants.PlaceholderAuth];
+    List<esr.Authorization> auth = [esr.ESRConstants.PlaceholderAuth];
 
-    var data = {
+    Map<String, String> data = {
       'from': esr.ESRConstants.PlaceholderName,
       'to': accountName,
       'quantity': '${quantity.toStringAsFixed(4)}',
       'memo': memo ?? ''
     };
 
-    var action = esr.Action()
+    esr.Action action = esr.Action()
       ..account = 'token.seeds'
       ..name = 'transfer'
       ..authorization = auth
       ..data = data;
 
-    var args = esr.SigningRequestCreateArguments(action: action, chainId: chainId);
+    esr.SigningRequestCreateArguments args = esr.SigningRequestCreateArguments(action: action, chainId: chainId);
 
-    var request = await esr.SigningRequestManager.create(args,
-        options: esr.defaultSigningRequestEncodingOptions(
-          nodeUrl: remoteConfigurations.hyphaEndPoint,
-        ));
-
-    var result = request.encode();
-
-    return ValueResult(result);
+    return esr.SigningRequestManager.create(args,
+            options: esr.defaultSigningRequestEncodingOptions(
+              nodeUrl: remoteConfigurations.hyphaEndPoint,
+            ))
+        .then((esr.SigningRequestManager response) => ValueResult(response.encode()))
+        // ignore: return_of_invalid_type_from_catch_error
+        .catchError((error) => mapEosError(error));
   }
 }
