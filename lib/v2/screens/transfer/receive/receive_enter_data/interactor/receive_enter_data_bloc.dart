53b49d1f8dd8240162ba267f94b45f926a2e446f
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-06-14 19:14:43 -0600

    Claenup

--- lib/v2/screens/transfer/receive/receive_enter_data/interactor/receive_enter_data_bloc.dart
+++ lib/v2/screens/transfer/receive/receive_enter_data/interactor/receive_enter_data_bloc.dart
@@ -1,17 +1,17 @@
 import 'package:async/async.dart';
 import 'package:bloc/bloc.dart';
-
 import 'package:seeds/v2/blocs/rates/viewmodels/rates_state.dart';
+import 'package:seeds/v2/datasource/local/settings_storage.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
 import 'package:seeds/v2/domain-shared/shared_use_cases/get_available_balance_use_case.dart';
 import 'package:seeds/v2/screens/transfer/receive/receive_enter_data/interactor/usecases/receive_seeds_invoice_use_case.dart';
 import 'package:seeds/v2/screens/transfer/receive/receive_enter_data/interactor/viewmodels/receive_enter_data_events.dart';
 import 'package:seeds/v2/screens/transfer/receive/receive_enter_data/interactor/viewmodels/receive_enter_data_state.dart';
+import 'package:seeds/v2/utils/double_extension.dart';
+import 'package:seeds/v2/utils/rate_states_extensions.dart';
+
 import 'mappers/create_invoice_result_mapper.dart';
 import 'mappers/user_balance_state_mapper.dart';
-import 'package:seeds/v2/utils/double_extension.dart';
-import 'package:seeds/v2/datasource/local/settings_storage.dart';
-import 'package:seeds/v2/utils/rate_states_extensions.dart';
 
 /// --- BLOC
 class ReceiveEnterDataBloc extends Bloc<ReceiveEnterDataEvents, ReceiveEnterDataState> {
