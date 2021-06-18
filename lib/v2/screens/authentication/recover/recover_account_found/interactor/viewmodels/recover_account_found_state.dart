90ac94bcf58bc459c051e2ea531541072be01ff0
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-06-18 17:22:52 -0600

    Added view for all guardians in your account

--- lib/v2/screens/authentication/recover/recover_account_found/interactor/viewmodels/recover_account_found_state.dart
+++ lib/v2/screens/authentication/recover/recover_account_found/interactor/viewmodels/recover_account_found_state.dart
@@ -1,21 +1,53 @@
 import 'package:equatable/equatable.dart';
+import 'package:seeds/v2/datasource/remote/model/member_model.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
 
 class RecoverAccountFoundState extends Equatable {
   final PageState pageState;
+  final String linkToActivateGuardians;
+  final List<String> userGuardians;
+  final List<MemberModel> userGuardiansData;
+  final String? errorMessage;
 
-  const RecoverAccountFoundState({required this.pageState});
+  const RecoverAccountFoundState({
+    required this.pageState,
+    required this.linkToActivateGuardians,
+    required this.userGuardians,
+    required this.userGuardiansData,
+    this.errorMessage,
+  });
 
   @override
-  List<Object?> get props => [pageState];
+  List<Object?> get props => [
+        pageState,
+        linkToActivateGuardians,
+        userGuardians,
+        userGuardiansData,
+        errorMessage,
+      ];
 
-  RecoverAccountFoundState copyWith({PageState? pageState}) {
+  RecoverAccountFoundState copyWith({
+    PageState? pageState,
+    String? linkToActivateGuardians,
+    List<String>? userGuardians,
+    List<MemberModel>? userGuardiansData,
+    String? errorMessage,
+  }) {
     return RecoverAccountFoundState(
       pageState: pageState ?? this.pageState,
+      linkToActivateGuardians: linkToActivateGuardians ?? this.linkToActivateGuardians,
+      userGuardians: userGuardians ?? this.userGuardians,
+      userGuardiansData: userGuardiansData ?? this.userGuardiansData,
+      errorMessage: errorMessage,
     );
   }
 
-  factory RecoverAccountFoundState.initial() {
-    return const RecoverAccountFoundState(pageState: PageState.initial);
+  factory RecoverAccountFoundState.initial(List<String> userGuardians) {
+    return RecoverAccountFoundState(
+      pageState: PageState.initial,
+      linkToActivateGuardians: "",
+      userGuardians: userGuardians,
+      userGuardiansData: [],
+    );
   }
 }
