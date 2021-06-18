0f1d990eabec7b43bf57688c276f8273e0ef0264
Raul Urtecho <raul.urtecho512@gmail.com>
2021-06-18 15:47:45 -0500

    fix v1

--- lib/providers/services/eos_service.dart
+++ lib/providers/services/eos_service.dart
@@ -600,7 +600,7 @@ class EosService {
       ..authorization = auth
       ..data = data;
 
-    var args = esr.SigningRequestCreateArguments(action: action, chain_id: chain_id);
+    var args = esr.SigningRequestCreateArguments(action: action, chainId: chain_id);
 
     var request = await esr.SigningRequestManager.create(args,
         options: esr.defaultSigningRequestEncodingOptions(
