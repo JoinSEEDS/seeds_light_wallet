60f1beef6feb83e7489fe0570ad0065f11f64f05
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-06-22 11:59:36 -0400

    import fix

--- lib/v2/screens/profile_screens/guardians/guardians_tabs/components/remove_guardian_confirmation_dialog.dart
+++ lib/v2/screens/profile_screens/guardians/guardians_tabs/components/remove_guardian_confirmation_dialog.dart
@@ -3,7 +3,6 @@ import 'package:seeds/v2/components/custom_dialog.dart';
 import 'package:seeds/v2/components/profile_avatar.dart';
 import 'package:seeds/v2/constants/app_colors.dart';
 import 'package:seeds/v2/datasource/remote/model/firebase_models/guardian_model.dart';
-import 'package:seeds/v2/design/app_theme.dart';
 
 class RemoveGuardianConfirmationDialog extends StatelessWidget {
   final GuardianModel guardian;
