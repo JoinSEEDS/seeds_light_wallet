d8f4b3ef940bb845275cc17970cee40eb5675487
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-06-24 14:23:26 -0400

    fix import

--- lib/v2/screens/explore_screens/vote_screens/vote/vote_screen.dart
+++ lib/v2/screens/explore_screens/vote_screens/vote/vote_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:seeds/v2/constants/app_colors.dart';
 import 'package:seeds/v2/design/app_theme.dart';
 import 'package:seeds/v2/i18n/explore_screens/vote/vote.i18n.dart';
 import 'package:seeds/v2/screens/explore_screens/vote_screens/proposals/proposals_list.dart';
