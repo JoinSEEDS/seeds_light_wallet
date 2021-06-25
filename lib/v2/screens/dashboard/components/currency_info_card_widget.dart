b9c62c66b236d5c969f39795c7e156bb17220111
nik <nheger@gmail.com>
2021-06-25 16:27:49 +0800

    this widget is now receiving the balance
    since we need to fetch all balances anyway, this is done at top level

--- lib/v2/screens/dashboard/components/currency_info_card_widget.dart
+++ lib/v2/screens/dashboard/components/currency_info_card_widget.dart
@@ -8,14 +8,14 @@ import 'package:seeds/v2/screens/dashboard/interactor/viewmodels/balance_event.d
 import 'package:seeds/v2/screens/dashboard/interactor/viewmodels/balance_state.dart';
 
 class CurrencyInfoCardWidget extends StatelessWidget {
-  final TokenModel token;
+  final TokenBalanceModel tokenBalance;
   final double? cardWidth;
   final double? cardHeight;
   final Color? textColor;
 
   const CurrencyInfoCardWidget({
     Key? key,
-    required this.token,
+    required this.tokenBalance,
     this.cardWidth,
     this.cardHeight,
     this.textColor,
@@ -23,54 +23,49 @@ class CurrencyInfoCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<BalanceBloc>(
-      create: (_) => BalanceBloc(token: token)..add(const OnLoadBalance()),
-      child: BlocBuilder<BalanceBloc, BalanceState>(builder: (context, state) {
-        return Container(
-            width: cardWidth,
-            height: cardHeight,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(24),
-                image: DecorationImage(image: AssetImage(token.backgroundImage), fit: BoxFit.fill)),
-            child: Stack(children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                            child: Text(
-                          state.token.name,
-                          style: Theme.of(context).textTheme.headline7.copyWith(color: textColor),
-                        )),
-                        Container(
-                            width: 42,
-                            height: 42,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                image: DecorationImage(image: AssetImage(token.logo), fit: BoxFit.fill))),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Text("Balance", style: Theme.of(context).textTheme.subtitle2!.copyWith(color: textColor)),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Text(state.cardDisplayText,
-                        style: Theme.of(context).textTheme.headline5!.copyWith(color: textColor)),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Text("", style: Theme.of(context).textTheme.subtitle2!.copyWith(color: textColor))
-                  ],
-                ),
-              )
-            ]));
-      }),
-    );
+      return Container(
+          width: cardWidth,
+          height: cardHeight,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              image: DecorationImage(image: AssetImage(tokenBalance.token.backgroundImage), fit: BoxFit.fill)),
+          child: Stack(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: Text(
+                        tokenBalance.token.name,
+                        style: Theme.of(context).textTheme.headline7.copyWith(color: textColor),
+                      )),
+                      Container(
+                          width: 42,
+                          height: 42,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              image: DecorationImage(image: AssetImage(tokenBalance.token.logo), fit: BoxFit.fill))),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Text("Balance", style: Theme.of(context).textTheme.subtitle2!.copyWith(color: textColor)),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(tokenBalance.displayQuantity,
+                      style: Theme.of(context).textTheme.headline5!.copyWith(color: textColor)),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text("", style: Theme.of(context).textTheme.subtitle2!.copyWith(color: textColor))
+                ],
+              ),
+            )
+          ]));
   }
 }
