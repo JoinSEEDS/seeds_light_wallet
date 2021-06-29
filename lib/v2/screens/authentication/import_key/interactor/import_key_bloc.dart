79f825f9eb76057b29b1720db81af400e55ca872
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-06-29 09:28:31 -0400

    fix if logic

--- lib/v2/screens/authentication/import_key/interactor/import_key_bloc.dart
+++ lib/v2/screens/authentication/import_key/interactor/import_key_bloc.dart
@@ -31,11 +31,7 @@ class ImportKeyBloc extends Bloc<ImportKeyEvent, ImportKeyState> {
     } else if (event is AccountSelected) {
       _authenticationBloc.add(OnImportAccount(account: event.account, privateKey: state.privateKey.toString()));
     } else if (event is OnPrivateKeyChange) {
-      if (event.privateKeyChanged.isEmpty) {
-        yield state.copyWith(enableButton: false);
-      } else {
-        yield state.copyWith(enableButton: true);
-      }
+      yield state.copyWith(enableButton: event.privateKeyChanged.isEmpty);
     }
   }
 }
