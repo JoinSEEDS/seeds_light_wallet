f470aeeac535fec5d2d4cf77ac7ba22d0d5e635d
nik <nheger@gmail.com>
2021-07-03 12:29:44 +0800

    clean up code

--- lib/v2/screens/dashboard/token_cards_widget.dart
+++ lib/v2/screens/dashboard/token_cards_widget.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_controller.dart';
 import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dots_indicator/dots_indicator.dart';
