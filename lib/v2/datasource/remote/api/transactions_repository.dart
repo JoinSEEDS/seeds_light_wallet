af697fe8f19a98a6af619bf803724d12a05a33ec
nik <nheger@gmail.com>
2021-07-06 15:06:35 +0800

    follow other repo standards, clean up code

--- lib/v2/datasource/remote/api/transactions_repository.dart
+++ lib/v2/datasource/remote/api/transactions_repository.dart
@@ -1,23 +1,20 @@
-import 'package:async/src/result/result.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:seeds/v2/datasource/remote/api/network_repository.dart';
 import 'package:seeds/v2/datasource/remote/model/transaction_model.dart';
+import 'package:async/async.dart';
 
-class TransactionsRepository extends NetworkRepository {
+class TransactionsListRepository extends NetworkRepository {
   Future<Result> getTransactions(String userAccount) async {
-    final transactionsUrl = Uri.parse(
-        '$baseURL/v2/history/get_actions?account=$userAccount&act.name=transfer&skip=0&limit=100&sort=desc');
+    final transactionsUrl =
+        Uri.parse('$baseURL/v2/history/get_actions?account=$userAccount&act.name=transfer&skip=0&limit=100&sort=desc');
 
-    try {
-      final response = await get(transactionsUrl);
+    return http
+        .get(transactionsUrl)
+        .then((http.Response response) => mapHttpResponse(response, (dynamic body) {
+              List<dynamic> transfers = body['actions'].toList();
 
-      return mapHttpResponse(response, (body) {
-        List<dynamic> transfers = body['actions'].toList();
-
-        return transfers.map((transfer) => TransactionModel.fromJson(transfer));
-      });
-    } catch (err) {
-      return mapHttpError(err);
-    }
+              return List<TransactionModel>.of(transfers.map((transfer) => TransactionModel.fromJson(transfer)));
+            }))
+        .catchError((dynamic error) => mapHttpError(error));
   }
 }
