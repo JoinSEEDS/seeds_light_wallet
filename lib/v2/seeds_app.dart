0f0fdd9db1dcce49a942e9023ce09e64b79b8c57
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-07-07 10:54:04 -0600

    Formatting

--- lib/v2/seeds_app.dart
+++ lib/v2/seeds_app.dart
@@ -1,30 +1,14 @@
-import 'dart:async';
-import 'dart:isolate';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:i18n_extension/i18n_widget.dart';
 import 'package:provider/provider.dart';
 import 'package:seeds/providers/providers.dart';
-import 'package:seeds/utils/old_toolbox/toolbox_app.dart';
 import 'package:seeds/v2/blocs/authentication/viewmodels/bloc.dart';
 import 'package:seeds/v2/blocs/rates/viewmodels/bloc.dart';
-import 'package:seeds/v2/datasource/local/settings_storage.dart';
-import 'package:seeds/v2/datasource/remote/firebase/firebase_push_notification_service.dart';
-import 'package:seeds/v2/datasource/remote/firebase/firebase_remote_config.dart';
 import 'package:seeds/v2/design/app_theme.dart';
-import 'package:seeds/v2/domain-shared/bloc_observer.dart';
 import 'package:seeds/v2/main.dart';
-import 'package:seeds/v2/navigation/navigation_service.dart';
-import 'package:seeds/v2/screens/app/app.dart';
-import 'package:seeds/v2/screens/authentication/login_screen.dart';
-import 'package:seeds/v2/screens/authentication/verification/verification_screen.dart';
-import 'package:seeds/v2/screens/onboarding/onboarding_screen.dart';
-import 'package:seeds/widgets/splash_screen.dart';
 
 class SeedsMaterialApp extends MaterialApp {
   SeedsMaterialApp({required home, navigatorKey, onGenerateRoute})
@@ -53,16 +37,10 @@ class SeedsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<AuthenticationBloc>(
-          create: (BuildContext context) =>
-              AuthenticationBloc()..add(const InitAuthStatus()),
-        ),
-        BlocProvider<RatesBloc>(create: (BuildContext context) => RatesBloc()),
+        BlocProvider<AuthenticationBloc>(create: (_) => AuthenticationBloc()..add(const InitAuthStatus())),
+        BlocProvider<RatesBloc>(create: (_) => RatesBloc()),
       ],
-      child: MultiProvider(
-        providers: providers,
-        child: const MainScreen(),
-      ),
+      child: MultiProvider(providers: providers, child: const MainScreen()),
     );
   }
 }
