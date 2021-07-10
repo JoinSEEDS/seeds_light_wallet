1d7e7b68b867ebf1d99dfbf07b775bc222aa12b3
nik <nheger@gmail.com>
2021-07-10 10:15:21 +0800

    fix import

--- lib/v2/screens/dashboard/components/tokens_cards/tokens_cards.dart
+++ lib/v2/screens/dashboard/components/tokens_cards/tokens_cards.dart
@@ -6,10 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/v2/constants/app_colors.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
+import 'package:seeds/v2/screens/dashboard/components/tokens_cards/currency_info_card_widget.dart';
 import 'package:seeds/v2/screens/dashboard/components/tokens_cards/interactor/viewmodels/bloc.dart';
 import 'package:seeds/v2/screens/dashboard/interactor/viewmodels/bloc.dart';
-
-import 'currency_info_card_widget.dart';
 
 class TokenCards extends StatefulWidget {
   const TokenCards({Key? key}) : super(key: key);
