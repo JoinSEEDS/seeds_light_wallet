4f376e70d2065cdf6b61eba359147f9dbbac341a
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-07-12 08:47:22 -0500

    Guardian recover account (add snackBar) (#869)
    * add snackBar
    
    * fix gerys comment

--- lib/v2/screens/authentication/recover/recover_account_found/recover_account_found_screen.dart
+++ lib/v2/screens/authentication/recover/recover_account_found/recover_account_found_screen.dart
@@ -9,6 +9,7 @@ import 'package:seeds/v2/components/divider_jungle.dart';
 import 'package:seeds/v2/components/flat_button_long.dart';
 import 'package:seeds/v2/components/full_page_error_indicator.dart';
 import 'package:seeds/v2/components/full_page_loading_indicator.dart';
+import 'package:seeds/v2/components/snack_bar_info.dart';
 import 'package:seeds/v2/components/text_form_field_custom.dart';
 import 'package:seeds/v2/constants/app_colors.dart';
 import 'package:seeds/v2/design/app_theme.dart';
@@ -18,6 +19,7 @@ import 'package:seeds/v2/screens/authentication/recover/recover_account_found/in
 import 'package:seeds/v2/screens/authentication/recover/recover_account_found/interactor/viewmodels/recover_account_found_events.dart';
 import 'package:seeds/v2/screens/authentication/recover/recover_account_found/interactor/viewmodels/recover_account_found_state.dart';
 import 'package:seeds/v2/screens/authentication/recover/recover_account_search/interactor/viewmodels/recover_account_page_command.dart';
+import 'package:seeds/v2/screens/authentication/recover/recover_account_found/interactor/viewmodels/recover_account_found_page_command.dart';
 
 class RecoverAccountFoundScreen extends StatelessWidget {
   const RecoverAccountFoundScreen({Key? key}) : super(key: key);
@@ -32,7 +34,16 @@ class RecoverAccountFoundScreen extends StatelessWidget {
         ..add(FetchInitialData()),
       child: Scaffold(
         appBar: AppBar(title: const Text("Recover Account")),
-        body: BlocBuilder<RecoverAccountFoundBloc, RecoverAccountFoundState>(
+        body: BlocConsumer<RecoverAccountFoundBloc, RecoverAccountFoundState>(
+        listenWhen: (_, current) => current.pageCommand != null,
+        listener: (context, state) {
+          var pageCommand = state.pageCommand;
+
+          if (pageCommand is ShowLinkCopied) {
+            SnackBarInfo("Copied", ScaffoldMessenger.of(context)).show();
+          }
+
+        },
           builder: (context, state) {
             switch (state.pageState) {
               case PageState.initial:
@@ -48,27 +59,38 @@ class RecoverAccountFoundScreen extends StatelessWidget {
                       padding: const EdgeInsets.all(8.0),
                       child: Column(
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-                            child: TextFormFieldCustom(
-                              enabled: false,
-                              labelText: 'Link to Activate Key Guardians',
-                              suffixIcon: IconButton(
-                                icon: const Icon(
-                                  Icons.copy,
-                                  color: AppColors.white,
+                          Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+                                child: TextFormFieldCustom(
+                                  enabled: false,
+                                  labelText: 'Link to Activate Key Guardians',
+                                  suffixIcon: const SizedBox.shrink(),
+                                  controller: TextEditingController(text: state.linkToActivateGuardians),
                                 ),
-                                onPressed: () {
-                                  Clipboard.setData(ClipboardData(text: state.linkToActivateGuardians)).then(
-                                    (value) {
-                                      // TODO(gguij002): Fire event to bloc
-                                      // SnackBarInfo("Copied", ScaffoldMessenger.of(context)).show();
-                                    },
-                                  );
-                                },
                               ),
-                              controller: TextEditingController(text: state.linkToActivateGuardians),
-                            ),
+                              Positioned(
+                                  right: 4,
+                                  top: 10,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(right: 8, top: 8),
+                                    child: IconButton(
+                                      icon: const Icon(
+                                        Icons.copy,
+                                        color: AppColors.white,
+                                      ),
+                                      splashRadius: 30,
+                                      onPressed: () {
+                                        Clipboard.setData(ClipboardData(text: state.linkToActivateGuardians)).then(
+                                          (value) {
+                                            BlocProvider.of<RecoverAccountFoundBloc>(context).add(OnCopyIconTap());
+                                          },
+                                        );
+                                      },
+                                    ),
+                                  )),
+                            ],
                           ),
                           Padding(
                             padding: const EdgeInsets.all(16),
