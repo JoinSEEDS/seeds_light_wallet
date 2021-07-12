1aa1281a94e19c1e0cacf822950bfe6d5eb1eba3
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-07-12 09:41:42 -0500

    fix Ui for account unlock screen (#870)
    * fix ui for account unlock screen
    
    * fix for small devices
    
    * guardian-recover-iu-fix-2

--- lib/v2/screens/authentication/recover/recover_account_found/recover_account_found_screen.dart
+++ lib/v2/screens/authentication/recover/recover_account_found/recover_account_found_screen.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:seeds/v2/blocs/authentication/viewmodels/authentication_bloc.dart';
 import 'package:seeds/v2/components/divider_jungle.dart';
 import 'package:seeds/v2/components/flat_button_long.dart';
@@ -14,6 +13,7 @@ import 'package:seeds/v2/components/text_form_field_custom.dart';
 import 'package:seeds/v2/constants/app_colors.dart';
 import 'package:seeds/v2/design/app_theme.dart';
 import 'package:seeds/v2/domain-shared/page_state.dart';
+import 'package:seeds/v2/domain-shared/ui_constants.dart';
 import 'package:seeds/v2/screens/authentication/recover/recover_account_found/components/guardian_row_widget.dart';
 import 'package:seeds/v2/screens/authentication/recover/recover_account_found/interactor/recover_account_found_bloc.dart';
 import 'package:seeds/v2/screens/authentication/recover/recover_account_found/interactor/viewmodels/recover_account_found_events.dart';
@@ -100,9 +100,7 @@ class RecoverAccountFoundScreen extends StatelessWidget {
                                     style: Theme.of(context).textTheme.button1),
                                 Text("/" + state.userGuardians.length.toString(),
                                     style: Theme.of(context).textTheme.button1),
-                                const SizedBox(
-                                  width: 24,
-                                ),
+                                const SizedBox(width: 24),
                                 Flexible(
                                   child: Text(
                                     "Guardians have accepted your request to recover your account",
@@ -132,53 +130,80 @@ class RecoverAccountFoundScreen extends StatelessWidget {
                       ),
                     );
                   default:
-                    return Column(
-                      children: [
-                        Text(
-                          "All three of your Key Guardians have accepted your request to recover your account. You account will be unlocked in 24hrs. ",
-                          style: Theme.of(context).textTheme.subtitle2,
-                          textAlign: TextAlign.center,
+                    return Scaffold(
+                      bottomSheet: Padding(
+                        padding: const EdgeInsets.all(horizontalEdgePadding),
+                        child: FlatButtonLong(
+                          enabled: state.recoveryStatus == RecoveryStatus.READY_TO_CLAIM_ACCOUNT,
+                          title: "Claim account",
+                          onPressed: () {
+                            BlocProvider.of<RecoverAccountFoundBloc>(context).add(OnClaimAccountTap());
+                          },
                         ),
-                        SvgPicture.asset("assets/images/guardians/check_circle.svg"),
-                        const Text("Time Left Before Recovery Enabled"),
-                        Row(
-                          children: [
-                            Row(
-                              children: [
-                                Text('${state.currentRemainingTime?.hours ?? 0} ',
-                                    style: Theme.of(context).textTheme.headline5),
-                                Text(':', style: Theme.of(context).textTheme.subtitle3Opacity)
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text('${state.currentRemainingTime?.min ?? 0} ',
-                                    style: Theme.of(context).textTheme.headline5),
-                                Text(':', style: Theme.of(context).textTheme.subtitle3Opacity)
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text('${state.currentRemainingTime?.sec ?? 0} ',
-                                    style: Theme.of(context).textTheme.headline5),
-                              ],
-                            )
-                          ],
-                        ),
-                        state.recoveryStatus == RecoveryStatus.READY_TO_CLAIM_ACCOUNT
-                            ? Text("Account recovered " + state.userAccount)
-                            : const SizedBox.shrink(),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 24),
-                          child: FlatButtonLong(
-                            enabled: state.recoveryStatus == RecoveryStatus.READY_TO_CLAIM_ACCOUNT,
-                            title: "Claim account",
-                            onPressed: () {
-                              BlocProvider.of<RecoverAccountFoundBloc>(context).add(OnClaimAccountTap());
-                            },
+                      ),
+                      body: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: horizontalEdgePadding),
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 10),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  "All three of your Key Guardians have accepted your request to recover your account. \n You account will be unlocked in 24hrs. ",
+                                  style: Theme.of(context).textTheme.subtitle2LowEmphasis,
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              const SizedBox(height: 40),
+                              const Image(image: AssetImage('assets/images/guardians/check_circle.png')),
+                              const SizedBox(height: 100),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text('${state.currentRemainingTime?.hours ?? '00'}',
+                                          style: Theme.of(context).textTheme.headline4),
+                                      Padding(
+                                        padding: const EdgeInsets.only(bottom: 6),
+                                        child: Text(':', style: Theme.of(context).textTheme.headline4),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text('${state.currentRemainingTime?.min ?? '00'}',
+                                          style: Theme.of(context).textTheme.headline4),
+                                      Padding(
+                                        padding: const EdgeInsets.only(bottom: 6),
+                                        child: Text(':', style: Theme.of(context).textTheme.headline4),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text('${state.currentRemainingTime?.sec ?? '00'} ',
+                                          style: Theme.of(context).textTheme.headline4),
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 14),
+                                    child: Text(
+                                      "Hours Left",
+                                      style: Theme.of(context).textTheme.subtitle2,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              state.recoveryStatus == RecoveryStatus.READY_TO_CLAIM_ACCOUNT
+                                  ? Text("Account recovered " + state.userAccount)
+                                  : const SizedBox.shrink(),
+                              const SizedBox(height: 150),
+                            ],
                           ),
                         ),
-                      ],
+                      ),
                     );
                 }
             }
