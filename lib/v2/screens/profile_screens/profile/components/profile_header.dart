5efd686edcb109becc151cf4fd74f60f5684f9ba
Raul Urtecho <raul.urtecho512@gmail.com>
2021-07-23 12:32:03 -0500

    unused importe removed

--- lib/v2/screens/profile_screens/profile/components/profile_header.dart
+++ lib/v2/screens/profile_screens/profile/components/profile_header.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:seeds/v2/constants/app_colors.dart';
 import 'package:seeds/v2/design/app_theme.dart';
 import 'package:seeds/i18n/profile.i18n.dart';
 import 'package:seeds/v2/navigation/navigation_service.dart';
