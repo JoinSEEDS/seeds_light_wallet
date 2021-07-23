c956196ccbf749acdaad3e71b63448fddd8b406d
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-07-23 15:08:15 -0500

    Removed from profile unimplemented regions and badges (#923)
    * Remuved from profile unimplemented regios and badges
    
    * unused importe removed
    
    * add new desing
    
    * beauty code
    
    Co-authored-by: gguij004 <51983923+gguij004@users.noreply.github.com>

--- lib/v2/screens/profile_screens/profile/components/profile_header.dart
+++ lib/v2/screens/profile_screens/profile/components/profile_header.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:seeds/v2/constants/app_colors.dart';
 import 'package:seeds/v2/design/app_theme.dart';
 import 'package:seeds/i18n/profile.i18n.dart';
 import 'package:seeds/v2/navigation/navigation_service.dart';
@@ -76,110 +75,14 @@ class ProfileHeader extends StatelessWidget {
                           Text(
                             state.profile!.statusString.i18n,
                             style: Theme.of(context).textTheme.headline7LowEmphasis,
-                          )
+                          ),
                         ],
                       ),
                     ),
                   ),
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                        decoration: const BoxDecoration(
-                          border: Border(
-                            right: BorderSide(color: AppColors.lightGreen2, width: 2),
-                          ),
-                        ),
-                        child: InkWell(
-                          onTap: () {
-                            NavigationService.of(context).navigateTo(Routes.contribution, state.score);
-                          },
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  'Contribution Score'.i18n,
-                                  style: Theme.of(context).textTheme.subtitle4,
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  '${state.score?.contributionScore?.value ?? '00'}/99',
-                                  textAlign: TextAlign.center,
-                                  style: Theme.of(context).textTheme.headline7LowEmphasis,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                        child: Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 8.0),
-                            child: Text(
-                              'Badges Earned'.i18n,
-                              style: Theme.of(context).textTheme.subtitle4,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Stack(
-                            children: [
-                              Row(
-                                children: [
-                                  const Icon(
-                                    Icons.circle,
-                                    size: 36,
-                                    color: Colors.transparent,
-                                  ),
-                                  const Icon(
-                                    Icons.circle,
-                                    size: 36,
-                                    color: Colors.transparent,
-                                  ),
-                                ],
-                              ),
-                              const Positioned(
-                                width: 36,
-                                child: Icon(
-                                  Icons.circle_notifications,
-                                  size: 36,
-                                  color: Colors.blue,
-                                ),
-                              ),
-                              const Positioned(
-                                width: 72,
-                                child: Icon(
-                                  Icons.account_circle_rounded,
-                                  size: 36,
-                                  color: Colors.orange,
-                                ),
-                              ),
-                              const Positioned(
-                                width: 108,
-                                child: Icon(
-                                  Icons.add_circle,
-                                  size: 36,
-                                  color: Colors.grey,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
-                    ))
-                  ],
-                ),
-              )
+              const SizedBox(height: 10)
             ],
           ),
         );
