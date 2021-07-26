0cc9720fc9bb033540453be6286ce6be5f476d10
Raul Urtecho <raul.urtecho512@gmail.com>
2021-07-26 17:09:04 -0500

    Chache logic to handle vote amount in label

--- lib/v2/screens/explore_screens/vote_screens/proposals/components/vote_amount_label/interactor/usecases/get_vote_use_case.dart
+++ lib/v2/screens/explore_screens/vote_screens/proposals/components/vote_amount_label/interactor/usecases/get_vote_use_case.dart
@@ -1,8 +1,21 @@
 import 'package:async/async.dart';
+import 'package:hive/hive.dart';
 import 'package:seeds/v2/datasource/remote/api/proposals_repository.dart';
+import 'package:seeds/v2/datasource/remote/model/vote_model.dart';
 
 class GetVoteUseCase {
-  Future<Result> run(int proposalId, String account) {
-    return ProposalsRepository().getVote(proposalId, account);
+  Future<Result> run(int proposalId, String account) async {
+    var box = await Hive.openBox<VoteModel>("votes.1.box");
+    VoteModel? voteModel = box.get(proposalId);
+    if (voteModel == null) {
+      var result = await ProposalsRepository().getVote(proposalId, account);
+      if (result.isValue) {
+        voteModel = result.asValue!.value as VoteModel;
+        if (voteModel.isVoted) {
+          await box.put(proposalId, voteModel);
+        }
+      }
+    }
+    return ValueResult(voteModel);
   }
 }
