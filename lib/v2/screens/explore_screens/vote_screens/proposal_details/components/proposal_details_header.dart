9d03596e32bc5f575e2e75b10831bc9372305a62
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-07-26 19:13:06 -0500

    Fix voting UI (#927)
    * fix image for good this time :)P
    
    * add gradiant to make back arrow always visible
    
    * fix campaing type label
    
    * remove comment code

--- lib/v2/screens/explore_screens/vote_screens/proposal_details/components/proposal_details_header.dart
+++ lib/v2/screens/explore_screens/vote_screens/proposal_details/components/proposal_details_header.dart
@@ -1,8 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:seeds/v2/constants/app_colors.dart';
+import 'package:seeds/v2/domain-shared/ui_constants.dart';
 import 'package:seeds/v2/images/vote/proposal_category.dart';
-import 'package:seeds/v2/design/app_theme.dart';
 import 'package:seeds/v2/screens/explore_screens/vote_screens/proposal_details/interactor/viewmodels/bloc.dart';
 
 class ProposalDetailsHeader extends StatelessWidget {
@@ -10,32 +11,58 @@ class ProposalDetailsHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return BlocBuilder<ProposalDetailsBloc, ProposalDetailsState>(
       buildWhen: (previous, current) => previous.currentIndex != current.currentIndex,
       builder: (context, state) {
         return Container(
-          height: 280,
+          clipBehavior: Clip.hardEdge,
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(defaultCardBorderRadius),
+                  bottomRight: Radius.circular(defaultCardBorderRadius))),
+          height: width,
           child: Stack(
             fit: StackFit.expand,
             children: [
               FittedBox(
-                fit: BoxFit.fitWidth,
+                clipBehavior: Clip.hardEdge,
+                fit: BoxFit.cover,
                 child: CachedNetworkImage(
                   imageUrl: state.proposals[state.currentIndex].image,
                   errorWidget: (_, __, ___) => const SizedBox.shrink(),
                 ),
               ),
-              Positioned(left: 0, top: 0, right: 0, child: AppBar(backgroundColor: Colors.transparent)),
               Positioned(
-                top: kToolbarHeight + 42,
+                  left: 0,
+                  top: 0,
+                  right: 0,
+                  child: Container(
+                    width: width,
+                    height: 200,
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [AppColors.black, Colors.transparent],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
+                  )),
+              Positioned(
+                  left: 0,
+                  top: 0,
+                  right: 0,
+                  child: AppBar(
+                    backgroundColor: Colors.transparent,
+                  )),
+              Positioned(
+                bottom: kToolbarHeight + 42,
                 left: 0,
                 child: CustomPaint(
-                  size: const Size(82, 22),
+                  size: const Size(100, 40),
                   painter: const ProposalCategory(),
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
                     child: Text(state.proposals[state.currentIndex].campaignTypeLabel,
-                        style: Theme.of(context).textTheme.subtitle3OpacityEmphasis),
+                        style: Theme.of(context).textTheme.subtitle2),
                   ),
                 ),
               ),
