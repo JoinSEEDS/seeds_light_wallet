5e96f3237873de914f94947b6097583779082818
Raul Urtecho <raul.urtecho512@gmail.com>
2021-07-27 16:45:42 -0500

    cache repo

--- lib/v2/screens/explore_screens/vote_screens/proposals/components/vote_amount_label/interactor/usecases/get_vote_use_case.dart
+++ lib/v2/screens/explore_screens/vote_screens/proposals/components/vote_amount_label/interactor/usecases/get_vote_use_case.dart
@@ -1,18 +1,20 @@
 import 'package:async/async.dart';
 import 'package:hive/hive.dart';
+import 'package:seeds/v2/datasource/local/cache_repository.dart';
 import 'package:seeds/v2/datasource/remote/api/proposals_repository.dart';
 import 'package:seeds/v2/datasource/remote/model/vote_model.dart';
 
 class GetVoteUseCase {
   Future<Result> run(int proposalId, String account) async {
     var box = await Hive.openBox<VoteModel>("votes.1.box");
-    VoteModel? voteModel = box.get(proposalId);
+    var cache = CacheRepository<VoteModel>(box);
+    VoteModel? voteModel = cache.get(proposalId);
     if (voteModel == null) {
       var result = await ProposalsRepository().getVote(proposalId, account);
       if (result.isValue) {
         voteModel = result.asValue!.value as VoteModel;
         if (voteModel.isVoted) {
-          await box.put(proposalId, voteModel);
+          await cache.add(proposalId, voteModel);
         }
       }
     }
