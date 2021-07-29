2847d5d49531b50e1895ff6054174a096bde27aa
Farzad Barzin <fbarzin@users.noreply.github.com>
2021-07-29 16:54:16 -0400

    Add phone number part3 (#902)
    * handles failure and success on account creation.
    
    * Uses onboardingPrivateKey when there is no private key on the storage.
    
    * Removes changes to ImagePicker.
    
    * pr comments
    
    Co-authored-by: Gerardo Guijarro <gguijarro_c@chewy.com>

--- lib/v2/screens/sign_up/add_phone_number/usecases/add_phone_number_usecase.dart
+++ lib/v2/screens/sign_up/add_phone_number/usecases/add_phone_number_usecase.dart
@@ -1,34 +1,35 @@
 import 'package:async/async.dart';
+import 'package:seeds/utils/string_extension.dart';
 import 'package:seeds/v2/datasource/remote/api/signup_repository.dart';
 import 'package:seeds/v2/datasource/remote/firebase/firebase_user_repository.dart';
-import 'package:seeds/utils/string_extension.dart';
 
 class AddPhoneNumberUseCase {
   AddPhoneNumberUseCase({
     required SignupRepository signupRepository,
     required FirebaseUserRepository firebaseUserRepository,
-  })  : _signupRepository = signupRepository,
+  })   : _signupRepository = signupRepository,
         _firebaseUserRepository = firebaseUserRepository;
 
   final SignupRepository _signupRepository;
   final FirebaseUserRepository _firebaseUserRepository;
 
-  Future<Result> run(
-      {required String inviteSecret,
-      required String displayName,
-      required String username,
-      String? phoneNumber}) async {
+  Future<Result> run({
+    required String inviteSecret,
+    required String displayName,
+    required String username,
+    String? phoneNumber,
+  }) async {
     final Result result = await _signupRepository.createAccount(
-        accountName: username,
-        inviteSecret: inviteSecret,
-        displayName: displayName);
+      accountName: username,
+      inviteSecret: inviteSecret,
+      displayName: displayName,
+    );
 
     // Phone number is optional.
     if (result.isValue && !phoneNumber.isNullOrEmpty) {
       try {
         // Add phone number
-        await _firebaseUserRepository.saveUserPhoneNumber(
-            username, phoneNumber);
+        await _firebaseUserRepository.saveUserPhoneNumber(username, phoneNumber);
       } catch (error) {
         print('Failed to save the phone number: $error');
       }
