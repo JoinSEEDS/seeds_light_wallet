32663df967942473b566d1562e50d8a3ebf6f5ce
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-08-06 13:24:23 -0400

    change mapper name

--- lib/v2/screens/profile_screens/citizenship/interactor/mappers/seeds_transactions_mapper.dart
+++ lib/v2/screens/profile_screens/citizenship/interactor/mappers/seeds_transactions_mapper.dart
@@ -3,7 +3,7 @@ import 'package:seeds/v2/domain-shared/page_state.dart';
 import 'package:seeds/v2/domain-shared/result_to_state_mapper.dart';
 import 'package:seeds/v2/screens/profile_screens/citizenship/interactor/viewmodels/citizenship_state.dart';
 
-class SetSeedsTransactionsMapper extends StateMapper {
+class SeedsTransactionsMapper extends StateMapper {
   CitizenshipState mapResultToState(CitizenshipState currentState, Result result) {
     if (result.isError) {
       return currentState.copyWith(pageState: PageState.failure, errorMessage: "Error seeds transaction history");
