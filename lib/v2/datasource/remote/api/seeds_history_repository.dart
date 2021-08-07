0901be52932f286643eb79d44067e0b1d7f5d4c4
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-08-06 23:53:53 -0400

    add something

--- lib/v2/datasource/remote/api/seeds_history_repository.dart
+++ lib/v2/datasource/remote/api/seeds_history_repository.dart
@@ -4,6 +4,7 @@ import 'package:seeds/v2/datasource/remote/api/network_repository.dart';
 import 'package:seeds/v2/datasource/remote/model/seeds_history_model.dart';
 import 'package:seeds/v2/domain-shared/app_constants.dart';
 
+///Seeds History Table
 class SeedsHistoryRepository extends NetworkRepository {
   Future<Result<dynamic>> getNumberOfTransactions(String userAccount) {
     print('[http] get seeds seeds history for account: $userAccount ');
