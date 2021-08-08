692a716ed966e2af5f04d80619ef9eddc823f904
NIK <nheger@gmail.com>
2021-08-08 11:15:27 +0800

    Resident and visitor count bugfix (#1009)
    * resident count needs to check for citizens too
    
    citizens also count as residents (only better)
    
    * fix visitor count
    
    everyone was once a visitor

--- lib/v2/screens/profile_screens/citizenship/interactor/mappers/set_values_mapper.dart
+++ lib/v2/screens/profile_screens/citizenship/interactor/mappers/set_values_mapper.dart
@@ -62,8 +62,8 @@ class SetValuesStateMapper extends StateMapper {
       return currentState.copyWith(
         pageState: PageState.success,
         progressTimeline: timeline,
-        invitedVisitors: profiles.where((i) => i.status == ProfileStatus.visitor).length,
-        invitedResidents: profiles.where((i) => i.status == ProfileStatus.resident).length,
+        invitedVisitors: profiles.length,
+        invitedResidents: profiles.where((i) => i.status == ProfileStatus.citizen || i.status == ProfileStatus.resident).length,
       );
     }
   }
