b31893a6a4e1e0818b31c593b3eb8b9200d40912
nik <nheger@gmail.com>
2021-08-14 18:48:10 +0800

    renamed class

--- lib/v2/datasource/local/models/scan_qr_code_result_data.dart
+++ lib/v2/datasource/local/models/scan_qr_code_result_data.dart
@@ -1,7 +1,7 @@
-class ScanQrCodeResultData {
+class ScanESRResultData {
   final Map<String, dynamic> data;
   final String accountName;
-  final String name;
+  final String actionName;
 
-  ScanQrCodeResultData({required this.data, required this.accountName, required this.name});
+  ScanESRResultData({required this.data, required this.accountName, required this.actionName});
 }
