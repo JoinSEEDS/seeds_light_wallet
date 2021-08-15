6e99ee7afb4953513abba9fddb5ae047341946e1
Sky Blu <i@buildco.de>
2021-08-15 15:12:23 +0800

    Re-apply the fixes

--- lib/v2/i18n/onboarding/onboarding.i18n.dart
+++ lib/v2/i18n/onboarding/onboarding.i18n.dart
@@ -6,13 +6,13 @@ extension Localization on String {
         'es_es': {
           'Join Now': 'Únete ahora',
           "Better\nThan Free\nTransactions": 'Mejor\nQue Transacciones\nGratis',
-          "Make payments globally without any fees.\nEarn rewards when you support\n‘Regenerative’ organisations and people.":
+          "Make payments globally without any fees.\nEarn rewards when you support\n‘Regenerative’ organizations and people.":
               "Realiza pagos a nivel mundial sin ningún cargo.\nObtenga recompensas cuando apoye a\norganizaciones y personas 'regenerativas'.",
           "Citizen\nCampaigns": 'Campañas\nciudadanas',
           "Participate and vote directly on social and\nenvironmental impact projects you care\nabout.":
               'Participa y vota directamente en proyectos\nde impacto social y ambiental que te\ninteresen.',
           "Regenerative\nEconomy": 'Economía\nRegenerativa',
-          "Unite with a global movement of\norganisations and people to regenerate our\nplanet and heal our economy.":
+          "Unite with a global movement of\norganizations and people to regenerate our\nplanet and heal our economy.":
               'Únete a un movimiento global de\norganizaciones y personas para regenerar\nnuestro planeta y sanar nuestra economía.',
         }
       };
