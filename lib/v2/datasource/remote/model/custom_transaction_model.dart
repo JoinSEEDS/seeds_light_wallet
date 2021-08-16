b3e6eaea67c76ba42a01b02db52bfe7f506a9a99
nik <nheger@gmail.com>
2021-08-16 12:26:54 +0800

    add timestamp

--- lib/v2/datasource/remote/model/custom_transaction_model.dart
+++ lib/v2/datasource/remote/model/custom_transaction_model.dart
@@ -5,11 +5,13 @@ class CustomTransactionModel {
   final String action;
   final Map<String, dynamic> data;
   final String? transactionId;
+  final DateTime? timestamp;
 
   List<SendInfoLineItems> get lineItems =>
       data.entries.map((e) => SendInfoLineItems(label: e.key, text: e.value.toString())).toList();
 
-  CustomTransactionModel({required this.account, required this.action, required this.data, this.transactionId});
+  CustomTransactionModel(
+      {required this.account, required this.action, required this.data, this.transactionId, this.timestamp});
   factory CustomTransactionModel.fromTxData(
       String account, String action, Map<String, dynamic> data, String transactionId) {
     return CustomTransactionModel(account: account, action: action, data: data, transactionId: transactionId);
