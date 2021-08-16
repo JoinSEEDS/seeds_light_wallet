7790b83c324556e22507d2950d81c3d714343bc0
nik <nheger@gmail.com>
2021-08-16 12:28:21 +0800

    HUSD has precision 2, formatting

--- lib/v2/datasource/remote/model/token_model.dart
+++ lib/v2/datasource/remote/model/token_model.dart
@@ -34,37 +34,42 @@ class TokenModel extends Equatable {
 }
 
 const SeedsToken = TokenModel(
-    chainName: "Telos",
-    contract: "token.seeds",
-    symbol: "SEEDS",
-    name: "Seeds",
-    backgroundImage: 'assets/images/wallet/currency_info_cards/seeds/background.jpg',
-    logo: 'assets/images/wallet/currency_info_cards/seeds/logo.jpg',
-    balanceSubTitle: 'Wallet Balance');
+  chainName: "Telos",
+  contract: "token.seeds",
+  symbol: "SEEDS",
+  name: "Seeds",
+  backgroundImage: 'assets/images/wallet/currency_info_cards/seeds/background.jpg',
+  logo: 'assets/images/wallet/currency_info_cards/seeds/logo.jpg',
+  balanceSubTitle: 'Wallet Balance',
+);
 
 const HusdToken = TokenModel(
-    chainName: "Telos",
-    contract: "husd.hypha",
-    symbol: "HUSD",
-    name: "HUSD",
-    backgroundImage: 'assets/images/wallet/currency_info_cards/hypha/background.jpg',
-    logo: 'assets/images/wallet/currency_info_cards/hypha/logo.jpg',
-    balanceSubTitle: 'Wallet Balance');
+  chainName: "Telos",
+  contract: "husd.hypha",
+  symbol: "HUSD",
+  name: "HUSD",
+  backgroundImage: 'assets/images/wallet/currency_info_cards/hypha/background.jpg',
+  logo: 'assets/images/wallet/currency_info_cards/hypha/logo.jpg',
+  balanceSubTitle: 'Wallet Balance',
+  precision: 2,
+);
 
 const HyphaToken = TokenModel(
-    chainName: "Telos",
-    contract: "token.hypha",
-    symbol: "HYPHA",
-    name: "Hypha",
-    backgroundImage: 'assets/images/wallet/currency_info_cards/hypha/background.jpg',
-    logo: 'assets/images/wallet/currency_info_cards/hypha/logo.jpg',
-    balanceSubTitle: 'Wallet Balance');
+  chainName: "Telos",
+  contract: "token.hypha",
+  symbol: "HYPHA",
+  name: "Hypha",
+  backgroundImage: 'assets/images/wallet/currency_info_cards/hypha/background.jpg',
+  logo: 'assets/images/wallet/currency_info_cards/hypha/logo.jpg',
+  balanceSubTitle: 'Wallet Balance',
+);
 
 const LocalScaleToken = TokenModel(
-    chainName: "Telos",
-    contract: "token.local",
-    symbol: "LSCL",
-    name: "LocalScale",
-    backgroundImage: 'assets/images/wallet/currency_info_cards/hypha/background.jpg',
-    logo: 'assets/images/wallet/currency_info_cards/hypha/logo.jpg',
-    balanceSubTitle: 'Wallet Balance');
+  chainName: "Telos",
+  contract: "token.local",
+  symbol: "LSCL",
+  name: "LocalScale",
+  backgroundImage: 'assets/images/wallet/currency_info_cards/hypha/background.jpg',
+  logo: 'assets/images/wallet/currency_info_cards/hypha/logo.jpg',
+  balanceSubTitle: 'Wallet Balance',
+);
