66c5e82dbc1ecd0456794e7f505fa4a6c3dcbb08
NIK <nheger@gmail.com>
2021-08-17 21:15:47 +0800

    Added generic transaction model (ESR fix 2) (#1081)
    * fix QR receive
    
    Receive needs to show the token symbol, ie SEEDS. Moved token string formatting to TokenModel because each token is different. Some have precision 2, some 4.
    
    * Added generic transaction model for non-transfers
    
    Changed transaction timestamp from String to DateTime for more efficiency and sanity
    Conversion method to convert a generic transaction into a transfer transaction
    
    * Update README.md
    
    * Revert "Update README.md"
    
    This reverts commit 1b3f05b3c0484aed5d615dcba64a1d82df3ea0de.

--- lib/v2/datasource/remote/model/transaction_model.dart
+++ lib/v2/datasource/remote/model/transaction_model.dart
@@ -1,14 +1,19 @@
 import 'package:equatable/equatable.dart';
+import 'package:seeds/v2/datasource/remote/model/generic_transaction_model.dart';
+import 'package:seeds/v2/domain-shared/app_constants.dart';
+import 'package:seeds/v2/utils/read_times_tamp.dart';
+import 'package:seeds/v2/utils/string_extension.dart';
 
 class TransactionModel extends Equatable {
   final String from;
   final String to;
   final String quantity;
   final String memo;
-  final String timestamp;
-  final String transactionId;
+  final DateTime timestamp;
+  final String? transactionId;
 
   String get symbol => quantity.split(" ")[1];
+  double get doubleQuantity => quantity.quantityAsDouble;
 
   const TransactionModel(
       {required this.from,
@@ -27,7 +32,7 @@ class TransactionModel extends Equatable {
       to: json['act']['data']['to'],
       quantity: json['act']['data']['quantity'],
       memo: json['act']['data']['memo'],
-      timestamp: json['@timestamp'],
+      timestamp: parseTimestamp(json['@timestamp']),
       transactionId: json['trx_id'],
     );
   }
@@ -38,10 +43,32 @@ class TransactionModel extends Equatable {
       to: json['act']['data']['to'],
       quantity: json['act']['data']['quantity'],
       memo: json['act']['data']['memo'],
-      timestamp: json['block_time'],
+      timestamp: parseTimestamp(json['block_time']),
       transactionId: json['trx_id'],
       //json["block_num"], // can add this later - neat but changes cache structure
     );
+  }
+
+  static TransactionModel? fromTransaction(GenericTransactionModel genericModel) {
+    if (genericModel.action == transfer_action) {
+      final data = genericModel.data;
+      final String? from = data['from'];
+      final String? to = data['to'];
+      final String? quantity = data['quantity'];
+      final String memo = data['memo'] ?? "";
+      if (from != null && to != null && quantity != null) {
+        return TransactionModel(
+          from: from,
+          to: to,
+          quantity: quantity,
+          memo: memo,
+          timestamp: genericModel.timestamp ?? DateTime.now().toUtc(),
+          transactionId: genericModel.transactionId,
+        );
+      }
+    }
+
+    return null;
   }
 
   factory TransactionModel.fromTxData(Map<String, dynamic> data, String transactionId) {
@@ -50,7 +77,7 @@ class TransactionModel extends Equatable {
       to: data['to'],
       quantity: data['quantity'],
       memo: data['memo'],
-      timestamp: '0',
+      timestamp: DateTime.now().toUtc(),
       transactionId: transactionId,
     );
   }
