45603cde2b5b1292cec47809eacf763696cf3814
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-08-17 21:06:47 -0500

    Add husd logo (#1087)
    * add husd logo
    
    * add background

--- lib/v2/datasource/remote/model/token_model.dart
+++ lib/v2/datasource/remote/model/token_model.dart
@@ -48,8 +48,8 @@ const HusdToken = TokenModel(
   contract: "husd.hypha",
   symbol: "HUSD",
   name: "HUSD",
-  backgroundImage: 'assets/images/wallet/currency_info_cards/hypha/background.jpg',
-  logo: 'assets/images/wallet/currency_info_cards/hypha/logo.jpg',
+  backgroundImage: 'assets/images/wallet/currency_info_cards/husd/background.jpg',
+  logo: 'assets/images/wallet/currency_info_cards/husd/logo.jpg',
   balanceSubTitle: 'Wallet Balance',
   precision: 2,
 );
