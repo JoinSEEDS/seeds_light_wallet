6d03563c87c1a9e50e29f62d9bfb0801e7eb1721
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-08-20 16:01:51 -0400

    ok

--- .github/workflows/dangerfile.js
+++ .github/workflows/dangerfile.js
@@ -1,15 +1,4 @@
-# Note that this DangerFile is for the gem configuration for CircleCI. When we switch to GitHub Actions we can use the
-# DangerFile.df.kts file instead.
-#
-# Once we migrate, the following can be cleaned up:
-#
-#   1. This file (DangerFile) should be removed
-#     * Port over any rules that we want to DangerFile.df.kts
-#
-#   2. Remove "gem 'danger'" from the gemfile
-#
+import {message, danger} from "danger"
 
-# PR size check
-if git.insertions > 0 && git.lines_of_code > 500
-  fail("PR has more than 500 line changes")
-end
+const modifiedMD = danger.git.modified_files.join("- ")
+message("Changed Files in this PR: \n - " + modifiedMD)
