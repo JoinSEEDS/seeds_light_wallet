237ae4b8cc2ebac54cb7772d6a4ff83efea5b4ca
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-08-20 16:09:32 -0400

    another danger 24

--- dangerfile.js
+++ dangerfile.js
@@ -3,7 +3,7 @@ import {message, danger} from "danger"
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
 
-var bigPRThreshold = 600;
+var bigPRThreshold = 25;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
   warn(':exclamation: Big PR (' + ++errorCount + ')');
   markdown('> (' + errorCount + ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
