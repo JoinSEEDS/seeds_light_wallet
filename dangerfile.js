03d239a1a5e5722a5cf61579a9fcaaf048d479ca
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-08-20 16:13:10 -0400

    another danger 24

--- dangerfile.js
+++ dangerfile.js
@@ -5,6 +5,6 @@ message("Changed Files in this PR: \n - " + modifiedMD)
 
 var bigPRThreshold = 25;
 if (danger.github.pr.additions + danger.github.pr.deletions > bigPRThreshold) {
-  warn(':exclamation: Big PR (' + ++errorCount + ')');
-  markdown('> (' + errorCount + ') : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
+  warn(':exclamation: Big PR ');
+  markdown('> KAKA : Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.');
 }
