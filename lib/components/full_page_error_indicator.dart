2e626feb844ad9e01e774d99c657f45760f5be7b
Sky Blu <i@buildco.de>
2021-08-21 17:32:34 +0800

    Fix 'Recover guardians with no guardians bricks app #1072'

--- lib/components/full_page_error_indicator.dart
+++ lib/components/full_page_error_indicator.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:seeds/components/flat_button_long.dart';
 import 'package:seeds/constants/app_colors.dart';
 import 'package:seeds/i18n/components/components.i18n.dart';
 
 class FullPageErrorIndicator extends StatelessWidget {
   final String? errorMessage;
+  final String? buttonTitle;
+  final Function()? buttonOnPressed;
 
-  const FullPageErrorIndicator({this.errorMessage, Key? key}) : super(key: key);
+  const FullPageErrorIndicator({this.errorMessage, this.buttonTitle, this.buttonOnPressed, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text(
         errorMessage ?? "Oops, Something Went Wrong".i18n,
         style: Theme.of(context).textTheme.subtitle2!.copyWith(color: AppColors.red1),
       ),
-    );
+      if (buttonTitle != null && buttonOnPressed != null)
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: FlatButtonLong(title: buttonTitle!, onPressed: buttonOnPressed),
+        ),
+    ]);
   }
 }
