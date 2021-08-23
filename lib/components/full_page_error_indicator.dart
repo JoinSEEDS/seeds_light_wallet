4d29f800ce2eef386af5e98fc51969698854684b
Sky Blu <i@buildco.de>
2021-08-23 23:04:49 +0800

    Implement PR comments

--- lib/components/full_page_error_indicator.dart
+++ lib/components/full_page_error_indicator.dart
@@ -6,20 +6,24 @@ import 'package:seeds/i18n/components/components.i18n.dart';
 class FullPageErrorIndicator extends StatelessWidget {
   final String? errorMessage;
   final String? buttonTitle;
-  final Function()? buttonOnPressed;
+  final VoidCallback? buttonOnPressed;
 
   const FullPageErrorIndicator({this.errorMessage, this.buttonTitle, this.buttonOnPressed, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Text(
-        errorMessage ?? "Oops, Something Went Wrong".i18n,
-        style: Theme.of(context).textTheme.subtitle2!.copyWith(color: AppColors.red1),
+      Expanded(
+        child: Center(
+          child: Text(
+            errorMessage ?? "Oops, Something Went Wrong".i18n,
+            style: Theme.of(context).textTheme.subtitle2!.copyWith(color: AppColors.red1),
+          ),
+        ),
       ),
       if (buttonTitle != null && buttonOnPressed != null)
         Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 32),
           child: FlatButtonLong(title: buttonTitle!, onPressed: buttonOnPressed),
         ),
     ]);
