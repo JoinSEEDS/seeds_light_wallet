2e3bd8e0c6cf243c767f66bdcbccd649c49be188
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-08-26 16:44:33 -0500

    Cache architecture in the whole app (#1120)
    * Cache architecture in the whole app
    
    * comments
    
    * changes from review
    
    * fix
    
    * fix2

---
+++ lib/datasource/local/models/vote_model_adapter.dart
@@ -0,0 +1,25 @@
+import 'package:hive/hive.dart';
+import 'package:seeds/datasource/remote/model/vote_model.dart';
+
+class VoteModelAdapter extends TypeAdapter<VoteModel> {
+  @override
+  final typeId = 3;
+
+  @override
+  VoteModel read(BinaryReader reader) {
+    final fields = [];
+    reader.readByte();
+    fields.add(reader.readInt());
+    reader.readByte();
+    fields.add(reader.readBool());
+    return VoteModel(amount: fields[0], isVoted: fields[1]);
+  }
+
+  @override
+  void write(BinaryWriter writer, VoteModel obj) {
+    writer.writeByte(0);
+    writer.writeInt(obj.amount);
+    writer.writeByte(1);
+    writer.writeBool(obj.isVoted);
+  }
+}
