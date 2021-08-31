98d5e7e1f76c3c3bc494dae2cd16a1262eb626d3
nik <nheger@gmail.com>
2021-08-31 11:54:34 +0800

    remove model from repository

--- lib/screens/explore_screens/plant_seeds/interactor/usecases/get_available_balance_and_planted_use_case.dart
+++ lib/screens/explore_screens/plant_seeds/interactor/usecases/get_available_balance_and_planted_use_case.dart
@@ -2,6 +2,7 @@ import 'package:async/async.dart';
 import 'package:seeds/datasource/local/settings_storage.dart';
 import 'package:seeds/datasource/remote/api/balance_repository.dart';
 import 'package:seeds/datasource/remote/api/planted_repository.dart';
+import 'package:seeds/datasource/remote/model/token_model.dart';
 
 class GetAvailableBalanceAndPlantedDataUseCase {
   final BalanceRepository _balanceRepository = BalanceRepository();
@@ -10,7 +11,7 @@ class GetAvailableBalanceAndPlantedDataUseCase {
   Future<List<Result>> run() {
     final account = settingsStorage.accountName;
     final futures = [
-      _balanceRepository.getTokenBalance(account),
+      _balanceRepository.getTokenBalance(account, tokenContract: SeedsToken.contract, symbol: SeedsToken.symbol),
       _plantedRepository.getPlanted(account),
     ];
     return Future.wait(futures);
