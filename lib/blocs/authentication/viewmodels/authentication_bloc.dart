2446e6a3fda9e8c28338ad9da2c6b667fab0cb0d
NIK <nheger@gmail.com>
2021-09-02 18:00:38 +0800

    fix filename

--- lib/blocs/authentication/viewmodels/authentication_bloc.dart
+++ lib/blocs/authentication/viewmodels/authentication_bloc.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:seeds/blocs/authentication/mappers/auth_status_state_mapper.dart';
 import 'package:seeds/blocs/authentication/viewmodels/bloc.dart';
 import 'package:seeds/datasource/local/settings_storage.dart';
-import 'package:seeds/domain-shared/shared_use_cases/remove_account_use_case%20copy%203.dart';
+import 'package:seeds/domain-shared/shared_use_cases/remove_account_use_case.dart';
 import 'package:seeds/domain-shared/shared_use_cases/save_account_use_case.dart';
 
 /// --- BLOC
