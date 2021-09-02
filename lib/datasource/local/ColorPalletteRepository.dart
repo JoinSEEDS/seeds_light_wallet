fc698ec2d03ce2af66496711dae1cf4f92e8c868
NIK <nheger@gmail.com>
2021-09-02 18:17:47 +0800

    remove comments

--- lib/datasource/local/ColorPalletteRepository.dart
+++ lib/datasource/local/ColorPalletteRepository.dart
@@ -3,7 +3,7 @@ import 'package:palette_generator/palette_generator.dart';
 
 class ColorPaletteRepository {
   static final Map<String, Color?> _cache = {};
-// Calculate dominant color from ImageProvider
+
   Future<Color?> getImagePalette(ImageProvider imageProvider) async {
     final PaletteGenerator paletteGenerator = await PaletteGenerator.fromImageProvider(imageProvider);
     return paletteGenerator.dominantColor?.color;
@@ -13,12 +13,9 @@ class ColorPaletteRepository {
     if (_cache[assetName] != null) {
       return _cache[assetName];
     }
-    //final stopwatch = Stopwatch()..start();
     final imageProvider = AssetImage(assetName);
-    // print('PaletteGenerator AssetImage executed in ${stopwatch.elapsed}');
     final PaletteGenerator paletteGenerator = await PaletteGenerator.fromImageProvider(imageProvider);
     final Color? color = paletteGenerator.dominantColor?.color;
-    //print('PaletteGenerator executed in ${stopwatch.elapsed}');// about 0.1 seconds - not great
 
     _cache[assetName] = color;
     return color;
