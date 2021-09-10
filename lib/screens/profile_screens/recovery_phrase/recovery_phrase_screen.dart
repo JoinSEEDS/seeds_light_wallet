aa3b60c35df427be75139ae4a6ac12925ca8087b
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-09-10 17:47:06 -0500

    Make ui good

--- lib/screens/profile_screens/recovery_phrase/recovery_phrase_screen.dart
+++ lib/screens/profile_screens/recovery_phrase/recovery_phrase_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:seeds/components/flat_button_long.dart';
 import 'package:seeds/design/app_theme.dart';
 import 'package:seeds/domain-shared/ui_constants.dart';
 import 'package:seeds/screens/profile_screens/recovery_phrase/interactor/viewmodels/recovery_phrase_bloc.dart';
@@ -16,6 +17,10 @@ class RecoveryPhraseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('12-word Recovery Phrase')),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(16),
+          child: FlatButtonLong(title: 'I’ve written it down', onPressed: (){}),
+        ),
         body: BlocProvider(
             create: (context) => RecoveryPhraseBloc(),
             child: BlocBuilder<RecoveryPhraseBloc, RecoveryPhraseState>(builder: (context, state) {
@@ -43,6 +48,7 @@ class RecoveryPhraseScreen extends StatelessWidget {
                                 right: ((index + 1) % NUMBER_OF_COLUMNS == 0) ? 0 : 8),
                             child: TextField(
                               autocorrect: false,
+                              enabled: false,
                               decoration: InputDecoration(
                                 floatingLabelBehavior: FloatingLabelBehavior.always,
                                 labelText: (index + 1).toString(),
