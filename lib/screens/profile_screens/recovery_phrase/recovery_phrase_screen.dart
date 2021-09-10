61f7aa51cb5504e52fa6062e78e2b5ab76d91038
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-09-10 17:36:33 -0500

    Added UI

--- lib/screens/profile_screens/recovery_phrase/recovery_phrase_screen.dart
+++ lib/screens/profile_screens/recovery_phrase/recovery_phrase_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:seeds/design/app_theme.dart';
+import 'package:seeds/domain-shared/ui_constants.dart';
 import 'package:seeds/screens/profile_screens/recovery_phrase/interactor/viewmodels/recovery_phrase_bloc.dart';
 import 'package:seeds/screens/profile_screens/recovery_phrase/interactor/viewmodels/recovery_phrase_state.dart';
+
+const NUMBER_OF_WORDS = 12;
+const NUMBER_OF_COLUMNS = 3;
 
 class RecoveryPhraseScreen extends StatelessWidget {
   const RecoveryPhraseScreen({Key? key}) : super(key: key);
@@ -13,7 +19,43 @@ class RecoveryPhraseScreen extends StatelessWidget {
         body: BlocProvider(
             create: (context) => RecoveryPhraseBloc(),
             child: BlocBuilder<RecoveryPhraseBloc, RecoveryPhraseState>(builder: (context, state) {
-              return Container();
+              return Padding(
+                padding: const EdgeInsets.all(horizontalEdgePadding),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Get a pen and paper before you start. Write down or copy these words in the right order and save them somewhere safe.',
+                        style: Theme.of(context).textTheme.subtitle3,
+                      ),
+                      const SizedBox(height: 24),
+                      GridView.count(
+                        padding: const EdgeInsets.only(top: 16),
+                        // to disable GridView's scrolling
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        crossAxisCount: NUMBER_OF_COLUMNS,
+                        childAspectRatio: NUMBER_OF_COLUMNS / 2,
+                        children: List.generate(NUMBER_OF_WORDS, (index) {
+                          return Padding(
+                            padding: EdgeInsets.only(
+                                left: (index % NUMBER_OF_COLUMNS == 0) ? 0 : 8,
+                                right: ((index + 1) % NUMBER_OF_COLUMNS == 0) ? 0 : 8),
+                            child: TextField(
+                              autocorrect: false,
+                              decoration: InputDecoration(
+                                floatingLabelBehavior: FloatingLabelBehavior.always,
+                                labelText: (index + 1).toString(),
+                                border: const OutlineInputBorder(),
+                              ),
+                            ),
+                          );
+                        }),
+                      ),
+                    ],
+                  ),
+                ),
+              );
             })));
   }
 }
