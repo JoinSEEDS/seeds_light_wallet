d61cec4c0eef18ab6d1c4a9ff4d4930d393634d6
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-09-12 14:44:34 -0500

    Pop on button click

--- lib/screens/profile_screens/recovery_phrase/recovery_phrase_screen.dart
+++ lib/screens/profile_screens/recovery_phrase/recovery_phrase_screen.dart
@@ -19,7 +19,11 @@ class RecoveryPhraseScreen extends StatelessWidget {
         appBar: AppBar(title: const Text('12-word Recovery Phrase')),
         bottomNavigationBar: Padding(
           padding: const EdgeInsets.all(16),
-          child: FlatButtonLong(title: 'I’ve written it down', onPressed: (){}),
+          child: FlatButtonLong(
+              title: 'I’ve written it down',
+              onPressed: () {
+                Navigator.of(context).pop();
+              }),
         ),
         body: BlocProvider(
             create: (context) => RecoveryPhraseBloc(),
