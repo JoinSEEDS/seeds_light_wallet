f150563e3291aaf4459cc32a67b44b868cc93061
nik <nheger@gmail.com>
2021-09-15 20:27:42 +0800

    added getTelosAccounts to member repository

--- lib/datasource/remote/model/member_model.dart
+++ lib/datasource/remote/model/member_model.dart
@@ -4,14 +4,24 @@ class MemberModel extends Equatable {
   final String account;
   final String nickname;
   final String image;
+  final bool isSeedsUser;
 
-  const MemberModel({required this.account, required this.nickname, required this.image});
+  const MemberModel({required this.account, required this.nickname, required this.image, this.isSeedsUser = true});
 
   factory MemberModel.fromJson(Map<String, dynamic> json) {
     return MemberModel(
       account: json['account'],
       nickname: json['nickname'],
       image: json['image'],
+    );
+  }
+
+  factory MemberModel.fromTelosAccount(String account) {
+    return MemberModel(
+      account: account,
+      nickname: "",
+      image: "",
+      isSeedsUser: false,
     );
   }
 
