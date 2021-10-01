4c3c3da6af229a25c7c94e349656abdb334da7f3
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-10-01 15:15:11 -0500

    Switch accouts part 3 (#1223)
    * Prevent verification on resume after user disables pincode
    
    * Flutter sdk upgrade v2.5
    
    * update flutter version in workflows
    
    * switch accoutn logic
    
    * privateKeyBackedUp flag moved to functions
    
    * more settings storage instructions moved to functions
    
    * const defaults removed
    
    * Keep current passcode on switch account
    
    * Fix comments

---
+++ lib/screens/profile_screens/profile/components/switch_account_bottom_sheet/interactor/usecases/import_accounts_use_case.dart
@@ -0,0 +1,27 @@
+import 'package:async/async.dart';
+import 'package:seeds/datasource/local/settings_storage.dart';
+import 'package:seeds/datasource/remote/api/key_accounts_repository.dart';
+import 'package:seeds/datasource/remote/api/profile_repository.dart';
+
+class ImportAccountsUseCase {
+  final KeyAccountsRepository _keyAccountsRepository = KeyAccountsRepository();
+  final ProfileRepository _profileRepository = ProfileRepository();
+
+  Future<List<Result>> run(String publicKey) async {
+    final accountsResponse = await _keyAccountsRepository.getKeyAccountsMongo(publicKey);
+    if (accountsResponse.isError) {
+      final List<Result> items = [accountsResponse];
+      return items;
+    } else {
+      final List<String> currentKeyAccounts = accountsResponse.asValue!.value;
+      final List<String> savedAccounts = settingsStorage.accountsList;
+      // Remove duplicated accounts
+      savedAccounts.removeWhere((i) => currentKeyAccounts.contains(i));
+      final List<String> accounts = currentKeyAccounts + savedAccounts;
+
+      final List<Future<Result>> futures = accounts.map((account) => _profileRepository.getProfile(account)).toList();
+
+      return Future.wait(futures);
+    }
+  }
+}
--- lib/screens/profile_screens/profile/components/switch_account_bottom_sheet/interactor/viewmodels/switch_account_bloc.dart
+++ lib/screens/profile_screens/profile/components/switch_account_bottom_sheet/interactor/viewmodels/switch_account_bloc.dart
@@ -3,19 +3,21 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+import 'package:seeds/blocs/authentication/viewmodels/bloc.dart';
 import 'package:seeds/datasource/local/settings_storage.dart';
 import 'package:seeds/datasource/remote/model/profile_model.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/i18n/authentication/import_key/import_key.i18n.dart';
 import 'package:seeds/screens/authentication/import_key/interactor/usecases/check_private_key_use_case.dart';
-import 'package:seeds/screens/authentication/import_key/interactor/usecases/import_key_use_case.dart';
 import 'package:seeds/screens/profile_screens/profile/components/switch_account_bottom_sheet/interactor/mappers/find_accounts_result_state_mapper.dart';
+import 'package:seeds/screens/profile_screens/profile/components/switch_account_bottom_sheet/interactor/usecases/import_accounts_use_case.dart';
 
 part 'switch_account_event.dart';
 part 'switch_account_state.dart';
 
 class SwitchAccountBloc extends Bloc<SwitchAccountEvent, SwitchAccountState> {
-  SwitchAccountBloc() : super(SwitchAccountState.initial());
+  final AuthenticationBloc _authenticationBloc;
+  SwitchAccountBloc(this._authenticationBloc) : super(SwitchAccountState.initial());
 
   @override
   Stream<SwitchAccountState> mapEventToState(SwitchAccountEvent event) async* {
@@ -25,9 +27,12 @@ class SwitchAccountBloc extends Bloc<SwitchAccountEvent, SwitchAccountState> {
       if (publicKey == null || publicKey.isEmpty) {
         yield state.copyWith(pageState: PageState.failure, errorMessage: "Private key is not valid".i18n);
       } else {
-        final results = await ImportKeyUseCase().run(publicKey);
+        final results = await ImportAccountsUseCase().run(publicKey);
         yield FindAccountsResultStateMapper().mapResultsToState(state, results);
       }
-    } else if (event is OnAccountSelected) {}
+    } else if (event is OnAccountSelected) {
+      yield state.copyWith(currentAcccout: event.profile);
+      _authenticationBloc.add(OnSwitchAccount(event.profile.account));
+    }
   }
 }
