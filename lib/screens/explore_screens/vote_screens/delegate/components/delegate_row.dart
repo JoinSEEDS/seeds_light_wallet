5840cc79f5b6810936878b12f958488132cebfa0
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-10-04 15:54:02 -0500

    basic ui and bloc (#1234)

---
+++ lib/screens/explore_screens/vote_screens/delegate/components/delegate_row.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:seeds/components/profile_avatar.dart';
+import 'package:seeds/design/app_theme.dart';
+
+class DelegateRow extends StatelessWidget {
+  final String account;
+  final String nickname;
+
+  const DelegateRow({Key? key, required this.account, required this.nickname}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ProfileAvatar(size: 40, account: account, nickname: nickname),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, right: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Delegated to',
+                    textAlign: TextAlign.start, style: Theme.of(context).textTheme.subtitle3OpacityEmphasis),
+                const SizedBox(height: 2),
+                Text(account, style: Theme.of(context).textTheme.subtitle2Green3LowEmphasis)
+              ],
+            ),
+          ),
+        ),
+        TextButton(
+            onPressed: () {}, child: Text("Remove", style: Theme.of(context).textTheme.subtitle3OpacityEmphasisRed))
+      ],
+    );
+  }
+}
