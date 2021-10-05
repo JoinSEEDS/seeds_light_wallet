8858716d43cccd5667ed322a8249afc0ac57bbe8
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-10-05 18:49:22 -0500

    Switch accounts part 4 (#1236)

--- lib/screens/profile_screens/profile/components/switch_account_bottom_sheet/switch_account_bottom_sheet.dart
+++ lib/screens/profile_screens/profile/components/switch_account_bottom_sheet/switch_account_bottom_sheet.dart
@@ -18,99 +18,108 @@ class SwithAccountBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => SwitchAccountBloc(BlocProvider.of<AuthenticationBloc>(context))..add(const FindAccountsByKey()),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: const EdgeInsets.only(top: 8.0, bottom: 22.0),
-              width: 54,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: const DividerJungle(thickness: 4, height: 4),
-              ),
-            ),
-            BlocConsumer<SwitchAccountBloc, SwitchAccountState>(
-              listenWhen: (_, current) => current.pageState == PageState.failure,
-              listener: (context, state) {
-                Navigator.of(context).pop();
-                SnackBarInfo(state.errorMessage ?? '', ScaffoldMessenger.of(context)).show();
-              },
-              builder: (context, state) {
-                switch (state.pageState) {
-                  case PageState.loading:
-                    return const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 20.0),
-                      child: Center(child: CircularProgressIndicator()),
-                    );
-                  case PageState.success:
-                    return ListView(
-                      shrinkWrap: true,
-                      children: [
-                        for (var i in state.accounts)
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10.0),
-                            child: Row(
-                              children: [
-                                ProfileAvatar(
-                                  size: 60,
-                                  image: i.image,
-                                  nickname: i.nickname,
-                                  account: i.account,
-                                ),
-                                const SizedBox(width: 8.0),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+      child: DraggableScrollableSheet(
+          expand: false,
+          builder: (context, scrollController) {
+            return Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 22.0),
+                    width: 54,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: const DividerJungle(thickness: 4, height: 4),
+                    ),
+                  ),
+                  BlocConsumer<SwitchAccountBloc, SwitchAccountState>(
+                    listenWhen: (_, current) => current.pageState == PageState.failure,
+                    listener: (context, state) {
+                      Navigator.of(context).pop();
+                      SnackBarInfo(state.errorMessage ?? '', ScaffoldMessenger.of(context)).show();
+                    },
+                    builder: (context, state) {
+                      switch (state.pageState) {
+                        case PageState.loading:
+                          return const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 20.0),
+                            child: Center(child: CircularProgressIndicator()),
+                          );
+                        case PageState.success:
+                          return Expanded(
+                            child: ListView.builder(
+                              itemCount: state.accounts.length + 1,
+                              itemBuilder: (_, index) {
+                                if (index == state.accounts.length) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                                    child: InkWell(
+                                      onTap: () {},
+                                      child: Row(
+                                        children: [
+                                          const CustomPaint(size: Size(60, 60), painter: AddAccountCircle()),
+                                          const SizedBox(width: 8.0),
+                                          Expanded(
+                                            child: Text('Add new account',
+                                                style: Theme.of(context).textTheme.buttonHighEmphasis),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                                  child: Row(
                                     children: [
-                                      Text(i.account, style: Theme.of(context).textTheme.buttonHighEmphasis),
-                                      Padding(
-                                        padding: const EdgeInsets.only(top: 8.0),
-                                        child: Text(
-                                          i.nickname ?? '',
-                                          style: Theme.of(context).textTheme.subtitle2OpacityEmphasis,
+                                      ProfileAvatar(
+                                        size: 60,
+                                        image: state.accounts[index].image,
+                                        nickname: state.accounts[index].nickname,
+                                        account: state.accounts[index].account,
+                                      ),
+                                      const SizedBox(width: 8.0),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(state.accounts[index].account,
+                                                style: Theme.of(context).textTheme.buttonHighEmphasis),
+                                            Padding(
+                                              padding: const EdgeInsets.only(top: 8.0),
+                                              child: Text(
+                                                state.accounts[index].nickname ?? '',
+                                                style: Theme.of(context).textTheme.subtitle2OpacityEmphasis,
+                                              ),
+                                            )
+                                          ],
                                         ),
-                                      )
+                                      ),
+                                      Radio<ProfileModel>(
+                                        activeColor: AppColors.green3,
+                                        value: state.accounts[index],
+                                        groupValue: state.currentAcccout,
+                                        onChanged: (value) {
+                                          BlocProvider.of<SwitchAccountBloc>(context).add(OnAccountSelected(value!));
+                                        },
+                                      ),
                                     ],
                                   ),
-                                ),
-                                Radio<ProfileModel>(
-                                  activeColor: AppColors.green3,
-                                  value: i,
-                                  groupValue: state.currentAcccout,
-                                  onChanged: (value) {
-                                    BlocProvider.of<SwitchAccountBloc>(context).add(OnAccountSelected(value!));
-                                  },
-                                ),
-                              ],
+                                );
+                              },
                             ),
-                          ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10.0, bottom: 40.0),
-                          child: InkWell(
-                            onTap: () {},
-                            child: Row(
-                              children: [
-                                const CustomPaint(size: Size(60, 60), painter: AddAccountCircle()),
-                                const SizedBox(width: 8.0),
-                                Expanded(
-                                  child: Text('Add new account', style: Theme.of(context).textTheme.buttonHighEmphasis),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  default:
-                    return const SizedBox.shrink();
-                }
-              },
-            ),
-          ],
-        ),
-      ),
+                          );
+                        default:
+                          return const SizedBox.shrink();
+                      }
+                    },
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 
