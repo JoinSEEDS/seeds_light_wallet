48ba1f96736b6d6a844c8d56ed357c184e38e5a3
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-10-12 19:30:14 -0400

    delegate new ui

--- lib/screens/explore_screens/vote_screens/delegate/delegate_screen.dart
+++ lib/screens/explore_screens/vote_screens/delegate/delegate_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/components/full_page_error_indicator.dart';
 import 'package:seeds/components/full_page_loading_indicator.dart';
 import 'package:seeds/domain-shared/page_state.dart';
+import 'package:seeds/domain-shared/ui_constants.dart';
 import 'package:seeds/navigation/navigation_service.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/components/delegate_card.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_bloc.dart';
@@ -16,7 +17,7 @@ class DelegateScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Trust & Delegates'),
+        title: const Text('Delegate'),
       ),
       body: BlocProvider(
         create: (context) => DelegateBloc()..add(const LoadDelegateData()),
@@ -31,15 +32,26 @@ class DelegateScreen extends StatelessWidget {
               case PageState.failure:
                 return const FullPageErrorIndicator();
               case PageState.success:
-                return Column(
-                  children: <Widget>[
-                    DelegateCard(
-                        onTap: () {
-                          NavigationService.of(context).navigateTo(Routes.delegateAUser);
-                        },
-                        activeDelegate: state.activeDelegate,
-                        delegate: state.delegate)
-                  ],
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: horizontalEdgePadding),
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 30 ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                            'Delegating your vote means to entrust the power of your vote to another Citizen.  Please choose your delegate carefully!',
+                            style: Theme.of(context).textTheme.subtitle2),
+                      ),
+                      const SizedBox(height: 30 ),
+                      DelegateCard(
+                          onTap: () {
+                            NavigationService.of(context).navigateTo(Routes.delegateAUser);
+                          },
+                          activeDelegate: state.activeDelegate,
+                          delegate: state.delegate)
+                    ],
+                  ),
                 );
               default:
                 return const SizedBox.shrink();
