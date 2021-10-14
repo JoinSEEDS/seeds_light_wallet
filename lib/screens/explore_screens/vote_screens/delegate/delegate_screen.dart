97ab5fb320bc226be676f1a12412989abd586700
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-10-13 20:50:16 -0500

    Issue 1209 remove delegate (#1249)
    * add usecase and mapper for remove delegate
    
    * fix imports
    
    * remove bloc
    
    * add ,

--- lib/screens/explore_screens/vote_screens/delegate/delegate_screen.dart
+++ lib/screens/explore_screens/vote_screens/delegate/delegate_screen.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/components/full_page_error_indicator.dart';
 import 'package:seeds/components/full_page_loading_indicator.dart';
+import 'package:seeds/components/snack_bar_info.dart';
+import 'package:seeds/domain-shared/page_command.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/navigation/navigation_service.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/components/delegate_card.dart';
+import 'package:seeds/screens/explore_screens/vote_screens/delegate/components/remove_delegate_success_dialog.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_bloc.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_event.dart';
+import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_page_commands.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_state.dart';
 
 class DelegateScreen extends StatelessWidget {
@@ -21,7 +25,23 @@ class DelegateScreen extends StatelessWidget {
       body: BlocProvider(
         create: (context) => DelegateBloc()..add(const LoadDelegateData()),
         child: BlocConsumer<DelegateBloc, DelegateState>(
-          listener: (context, state) {},
+          listener: (context, state) {
+            final pageCommand = state.pageCommand;
+
+            if (pageCommand is ShowDelegateRemovalSuccess) {
+              showDialog<void>(
+                context: context,
+                barrierDismissible: false,
+                builder: (_) {
+                  return const RemoveDelegateSuccessDialog();
+                },
+              );
+            } else if (pageCommand is ShowErrorMessage) {
+              SnackBarInfo(pageCommand.message, ScaffoldMessenger.of(context)).show();
+            }
+
+            BlocProvider.of<DelegateBloc>(context).add(ClearPageCommand());
+          },
           builder: (context, DelegateState state) {
             switch (state.pageState) {
               case PageState.initial:
@@ -34,6 +54,9 @@ class DelegateScreen extends StatelessWidget {
                 return Column(
                   children: <Widget>[
                     DelegateCard(
+                        onTapRemove: () {
+                          BlocProvider.of<DelegateBloc>(context).add(const RemoveDelegate());
+                        },
                         onTap: () {
                           NavigationService.of(context).navigateTo(Routes.delegateAUser);
                         },
--- lib/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_bloc.dart
+++ lib/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_bloc.dart
@@ -2,6 +2,8 @@ import 'package:bloc/bloc.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/domain-shared/result_to_state_mapper.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/mapper/delegate_load_data_mapper.dart';
+import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/mapper/remove_delegate_mapper.dart';
+import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/usecase/remove_delegate_use_case.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/usecases/delegate_load_data_usecase.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_event.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate/interactor/viewmodels/delegate_state.dart';
@@ -15,6 +17,10 @@ class DelegateBloc extends Bloc<DelegateEvent, DelegateState> {
       yield state.copyWith(pageState: PageState.loading);
       final Result results = await DelegateLoadDataUseCase().run();
       yield DelegateLoadDataStateMapper().mapResultToState(state, results);
+    } else if (event is RemoveDelegate) {
+      yield state.copyWith(pageState: PageState.loading);
+      final Result result = await RemoveDelegateUseCase().run();
+      yield RemoveDelegateResultMapper().mapResultToState(state, result);
     }
   }
 }
