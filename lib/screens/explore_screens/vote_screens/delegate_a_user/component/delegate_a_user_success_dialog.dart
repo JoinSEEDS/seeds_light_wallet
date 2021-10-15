03bccd6559edd51128b5dec988b1f84e2ec51902
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-10-14 20:16:07 -0400

    update delegate screen after selecting one

--- lib/screens/explore_screens/vote_screens/delegate_a_user/component/delegate_a_user_success_dialog.dart
+++ lib/screens/explore_screens/vote_screens/delegate_a_user/component/delegate_a_user_success_dialog.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:seeds/components/custom_dialog.dart';
+import 'package:seeds/navigation/navigation_service.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate_a_user/interactor/delegate_a_user_bloc.dart';
 import 'package:seeds/screens/explore_screens/vote_screens/delegate_a_user/interactor/viewmodel/delegate_a_user_state.dart';
 
@@ -21,8 +22,9 @@ class DelegateAUserSuccessDialog extends StatelessWidget {
           icon: SvgPicture.asset('assets/images/security/success_outlined_icon.svg'),
           singleLargeButtonTitle: "Dismiss",
           onSingleLargeButtonPressed: () {
-            Navigator.of(context).pop();
-            Navigator.of(context).pop();
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+            NavigationService.of(context).navigateTo(Routes.vote);
+            NavigationService.of(context).navigateTo(Routes.delegate);
           },
           children: [
             Text('Delegate Chosen!', style: Theme.of(context).textTheme.headline6),
