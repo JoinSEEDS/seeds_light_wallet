c43891b688989aa8cecc3eabf4761e9699f7793f
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-10-19 20:11:56 -0400

    add missing fields

--- lib/datasource/remote/model/refund_model.dart
+++ lib/datasource/remote/model/refund_model.dart
@@ -1,24 +1,36 @@
 import 'package:equatable/equatable.dart';
 
 class RefundModel extends Equatable {
+  final int requestID;
   final int refundID;
   final double amount;
+  final int requestTime;
+  final int weeksDelay;
 
   const RefundModel({
+    required this.requestID,
     required this.refundID,
     required this.amount,
+    required this.requestTime,
+    required this.weeksDelay,
   });
 
   factory RefundModel.fromJson(Map<String, dynamic> json) {
     return RefundModel(
+      requestID: json['request_id'],
       refundID: json['refund_id'],
       amount: json['amount'],
+      requestTime: json['request_time'],
+      weeksDelay: json['weeks_delay'],
     );
   }
 
   @override
   List<Object?> get props => [
+        requestID,
         refundID,
         amount,
+        requestTime,
+        weeksDelay,
       ];
 }
