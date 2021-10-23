5f2581f9fa89e2d5ce382a331e0012db787a48ab
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-10-23 07:23:37 -0500

    get unplanted refunds call (#1268)
    * add network call
    
    * add missing fields
    
    * fix var name

--- lib/datasource/remote/api/planted_repository.dart
+++ lib/datasource/remote/api/planted_repository.dart
@@ -2,6 +2,7 @@ import 'package:async/async.dart';
 import 'package:http/http.dart' as http;
 import 'package:seeds/datasource/remote/api/network_repository.dart';
 import 'package:seeds/datasource/remote/model/planted_model.dart';
+import 'package:seeds/datasource/remote/model/refund_model.dart';
 import 'package:seeds/domain-shared/app_constants.dart';
 
 class PlantedRepository extends NetworkRepository {
@@ -24,4 +25,25 @@ class PlantedRepository extends NetworkRepository {
             }))
         .catchError((error) => mapHttpError(error));
   }
+
+  Future<Result> getRefunds(String userAccount) {
+    print('[http] get seeds getRefunds $userAccount');
+
+    final plantedURL = Uri.parse('$baseURL/v1/chain/get_table_rows');
+
+    final request = createRequest(
+      code: accountHarvest,
+      scope: userAccount,
+      table: tableRefunds,
+      limit: 1000,
+    );
+
+    return http
+        .post(plantedURL, headers: headers, body: request)
+        .then((http.Response response) => mapHttpResponse(response, (dynamic body) {
+      return RefundModel.fromJson(body);
+    }))
+        .catchError((error) => mapHttpError(error));
+  }
+
 }
