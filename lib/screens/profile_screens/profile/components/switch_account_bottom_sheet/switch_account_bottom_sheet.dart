702351945ad0d4a7583cf1e57952bbb6ed7a0cec
Raul Urtecho <raul.urtecho512@gmail.com>
2021-10-25 20:12:07 -0500

    UI

--- lib/screens/profile_screens/profile/components/switch_account_bottom_sheet/switch_account_bottom_sheet.dart
+++ lib/screens/profile_screens/profile/components/switch_account_bottom_sheet/switch_account_bottom_sheet.dart
@@ -25,118 +25,119 @@ class SwithAccountBottomSheet extends StatelessWidget {
       child: DraggableScrollableSheet(
         expand: false,
         builder: (context, scrollController) {
-          return Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(top: 8.0, bottom: 22.0),
-                  width: 54,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: const DividerJungle(thickness: 4, height: 4),
-                  ),
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 22.0),
+                width: 54,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: const DividerJungle(thickness: 4, height: 4),
                 ),
-                BlocConsumer<SwitchAccountBloc, SwitchAccountState>(
-                  listenWhen: (_, current) => current.pageState == PageState.failure,
-                  listener: (context, state) {
-                    Navigator.of(context).pop();
-                    SnackBarInfo(state.errorMessage ?? '', ScaffoldMessenger.of(context)).show();
-                  },
-                  builder: (context, state) {
-                    switch (state.pageState) {
-                      case PageState.loading:
-                        return const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 20.0),
-                          child: Center(child: CircularProgressIndicator()),
-                        );
-                      case PageState.success:
-                        return Expanded(
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            controller: scrollController,
-                            itemCount: state.accounts.length + 1,
-                            itemBuilder: (_, index) {
-                              if (index == state.accounts.length) {
-                                return Padding(
-                                  padding: const EdgeInsets.only(top: 10.0, bottom: 30.0),
-                                  child: InkWell(
-                                    onTap: () {
-                                      // Pass true to import screens to indicate from switch accounts
-                                      if (state.isRecoverPharseEnabled) {
-                                        NavigationService.of(context).navigateTo(Routes.importWords, true);
-                                      } else {
-                                        NavigationService.of(context).navigateTo(Routes.importKey, true);
-                                      }
-                                    },
-                                    child: Row(
-                                      children: [
-                                        const CustomPaint(size: Size(60, 60), painter: AddAccountCircle()),
-                                        const SizedBox(width: 8.0),
-                                        Expanded(
-                                          child: Text('Add new account',
-                                              style: Theme.of(context).textTheme.buttonHighEmphasis),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              }
+              ),
+              BlocConsumer<SwitchAccountBloc, SwitchAccountState>(
+                listenWhen: (_, current) => current.pageState == PageState.failure,
+                listener: (context, state) {
+                  Navigator.of(context).pop();
+                  SnackBarInfo(state.errorMessage ?? '', ScaffoldMessenger.of(context)).show();
+                },
+                builder: (context, state) {
+                  switch (state.pageState) {
+                    case PageState.loading:
+                      return const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        child: Center(child: CircularProgressIndicator()),
+                      );
+                    case PageState.success:
+                      return Expanded(
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          controller: scrollController,
+                          itemCount: state.accounts.length + 1,
+                          itemBuilder: (_, index) {
+                            if (index == state.accounts.length) {
                               return Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                                padding: const EdgeInsets.only(top: 10.0, bottom: 30.0),
                                 child: InkWell(
-                                  onTap: state.currentAcccout == state.accounts[index]
-                                      ? null
-                                      : () {
-                                          BlocProvider.of<SwitchAccountBloc>(context)
-                                              .add(OnAccountSelected(state.accounts[index]));
-                                        },
+                                  onTap: () {
+                                    // Pass true to import screens to indicate from switch accounts
+                                    if (state.isRecoverPharseEnabled) {
+                                      NavigationService.of(context).navigateTo(Routes.importWords, true);
+                                    } else {
+                                      NavigationService.of(context).navigateTo(Routes.importKey, true);
+                                    }
+                                  },
                                   child: Row(
                                     children: [
-                                      ProfileAvatar(
-                                        size: 60,
-                                        image: state.accounts[index].image,
-                                        nickname: state.accounts[index].nickname,
-                                        account: state.accounts[index].account,
-                                      ),
+                                      const SizedBox(width: 20.0),
+                                      const CustomPaint(size: Size(60, 60), painter: AddAccountCircle()),
                                       const SizedBox(width: 8.0),
                                       Expanded(
-                                        child: Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text(state.accounts[index].account,
-                                                style: Theme.of(context).textTheme.buttonHighEmphasis),
-                                            Padding(
-                                              padding: const EdgeInsets.only(top: 8.0),
-                                              child: Text(
-                                                state.accounts[index].nickname ?? '',
-                                                style: Theme.of(context).textTheme.subtitle2OpacityEmphasis,
-                                              ),
-                                            )
-                                          ],
-                                        ),
+                                        child: Text('Add new account',
+                                            style: Theme.of(context).textTheme.buttonHighEmphasis),
                                       ),
-                                      Radio<ProfileModel>(
-                                        activeColor: AppColors.green3,
-                                        value: state.accounts[index],
-                                        groupValue: state.currentAcccout,
-                                        onChanged: (_) {},
-                                      ),
+                                      const SizedBox(width: 20.0),
                                     ],
                                   ),
                                 ),
                               );
-                            },
-                          ),
-                        );
-                      default:
-                        return const SizedBox.shrink();
-                    }
-                  },
-                ),
-              ],
-            ),
+                            }
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10.0),
+                              child: InkWell(
+                                onTap: state.currentAcccout == state.accounts[index]
+                                    ? null
+                                    : () {
+                                        BlocProvider.of<SwitchAccountBloc>(context)
+                                            .add(OnAccountSelected(state.accounts[index]));
+                                      },
+                                child: Row(
+                                  children: [
+                                    const SizedBox(width: 20.0),
+                                    ProfileAvatar(
+                                      size: 60,
+                                      image: state.accounts[index].image,
+                                      nickname: state.accounts[index].nickname,
+                                      account: state.accounts[index].account,
+                                    ),
+                                    const SizedBox(width: 8.0),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(state.accounts[index].account,
+                                              style: Theme.of(context).textTheme.buttonHighEmphasis),
+                                          Padding(
+                                            padding: const EdgeInsets.only(top: 8.0),
+                                            child: Text(
+                                              state.accounts[index].nickname ?? '',
+                                              style: Theme.of(context).textTheme.subtitle2OpacityEmphasis,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Radio<ProfileModel>(
+                                      activeColor: AppColors.green3,
+                                      value: state.accounts[index],
+                                      groupValue: state.currentAcccout,
+                                      onChanged: (_) {},
+                                    ),
+                                    const SizedBox(width: 20.0),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    default:
+                      return const SizedBox.shrink();
+                  }
+                },
+              ),
+            ],
           );
         },
       ),
