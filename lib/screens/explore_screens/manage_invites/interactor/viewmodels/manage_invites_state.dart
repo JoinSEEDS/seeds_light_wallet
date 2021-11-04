6071ecc1501e0440f7130463ad3007f2fce8709c
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-11-04 08:33:29 -0600

    Merge conflicts

--- lib/screens/explore_screens/manage_invites/interactor/viewmodels/manage_invites_state.dart
+++ lib/screens/explore_screens/manage_invites/interactor/viewmodels/manage_invites_state.dart
@@ -1,6 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:seeds/domain-shared/page_state.dart';
-import 'package:seeds/screens/explore_screens/manage_invites/interactor/mappers/get_invites_state_mapper.dart';
+import 'package:seeds/screens/explore_screens/manage_invites/interactor/viewdata/InvitesItemsData.dart';
 
 class ManageInvitesState extends Equatable {
   final PageState pageState;
@@ -10,8 +10,7 @@ class ManageInvitesState extends Equatable {
 
   Iterable<InvitesItemsData> get claimedInvites => invitesItemData.where((element) => element.invite.isClaimed);
 
-  Iterable<InvitesItemsData> get unclaimedInvites =>
-      invitesItemData.where((element) => !element.invite.isClaimed);
+  Iterable<InvitesItemsData> get unclaimedInvites => invitesItemData.where((element) => !element.invite.isClaimed);
 
   @override
   List<Object?> get props => [
--- lib/screens/explore_screens/manage_invites/manage_invites_screen.dart
+++ lib/screens/explore_screens/manage_invites/manage_invites_screen.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/components/flat_button_long.dart';
 import 'package:seeds/components/full_page_loading_indicator.dart';
-import 'package:seeds/datasource/remote/model/invite_model.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/i18n/profile_screens/guardians/guardians.i18n.dart';
 import 'package:seeds/screens/explore_screens/manage_invites/interactor/manage_invites_bloc.dart';
-import 'package:seeds/screens/explore_screens/manage_invites/interactor/mappers/get_invites_state_mapper.dart';
+import 'package:seeds/screens/explore_screens/manage_invites/interactor/viewdata/InvitesItemsData.dart';
 import 'package:seeds/screens/explore_screens/manage_invites/interactor/viewmodels/manage_invites_events.dart';
 import 'package:seeds/screens/explore_screens/manage_invites/interactor/viewmodels/manage_invites_state.dart';
 
@@ -68,9 +67,9 @@ class ManageInvitesScreen extends StatelessWidget {
                             ListView(
                               children: state.unclaimedInvites
                                   .map((InvitesItemsData invite) => ListTile(
-                                leading: Text(invite.invite.inviteAmount),
-                                title: Text(invite.invite.inviteHash),
-                              ))
+                                        leading: Text(invite.invite.inviteAmount),
+                                        title: Text(invite.invite.inviteHash),
+                                      ))
                                   .toList(),
                             ),
                           ],
