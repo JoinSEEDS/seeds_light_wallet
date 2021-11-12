17cf4a154f80904706eb2e3664ab1fb0677f1ccd
NIK <nheger@gmail.com>
2021-11-12 16:27:20 +0800

    for debugging the wait state enter / exit functionality

--- lib/datasource/remote/model/vote_cycle_model.dart
+++ lib/datasource/remote/model/vote_cycle_model.dart
@@ -17,7 +17,14 @@ class VoteCycleModel {
     return VoteCycleModel(
       propcycle: json['propcycle'],
       startTime: json['start_time'],
-      endTime: json['end_time'],
+
+      /// to debug: switch between + and - 60 lines below
+      /// start with +60 - then it will count down to 0, then reload
+      /// then switch to -60 - it will be in wait mode, and poll every 60 seconds
+      /// then switch back to +60 again - then it will exit wait mode and count down again
+      endTime: DateTime.now().millisecondsSinceEpoch ~/ 1000 + 60,
+      //endTime: DateTime.now().millisecondsSinceEpoch ~/ 1000 - 60,
+      // endTime: json['end_time'], // actual code...
       activeProps: List<int>.from(json['active_props']),
       evalProps: List<int>.from(json['eval_props']),
     );
