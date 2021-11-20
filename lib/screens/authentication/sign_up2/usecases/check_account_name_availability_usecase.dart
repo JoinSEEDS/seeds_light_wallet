357d697544243d6807129295bde7548d1a199156
home <home@MacBook-Pro-de-home.local>
2021-11-19 23:18:35 -0600

    Only UI

--- lib/screens/authentication/sign_up2/usecases/check_account_name_availability_usecase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:async/async.dart';
-import 'package:seeds/datasource/remote/api/signup_repository.dart';
-
-class CheckAccountNameAvailabilityUseCase {
-  Future<Result> run(String accountName) => SignupRepository().isUsernameTaken(accountName);
-}
