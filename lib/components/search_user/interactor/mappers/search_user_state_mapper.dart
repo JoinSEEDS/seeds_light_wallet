3a80ac44143353b617861b652742a5a775d3b17e
NIK <nheger@gmail.com>
2021-11-25 17:01:27 +0800

    added full name search feature

--- lib/components/search_user/interactor/mappers/search_user_state_mapper.dart
+++ lib/components/search_user/interactor/mappers/search_user_state_mapper.dart
@@ -4,36 +4,43 @@ import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/domain-shared/result_to_state_mapper.dart';
 
 class SearchUserStateMapper extends StateMapper {
-  SearchUserState mapResultToState(
-      SearchUserState currentState, Result seedsMembersResult, Result telosResult, List<String>? noShowUsers) {
-    if (seedsMembersResult.isError && telosResult.isError) {
+  SearchUserState mapResultToState({
+    required SearchUserState currentState,
+    required Result<List<MemberModel>> seedsMembersResult,
+    required Result<List<MemberModel>> telosResult,
+    required Result<List<MemberModel>> fullNameResult,
+    List<String>? noShowUsers,
+  }) {
+    if (seedsMembersResult.isError && telosResult.isError && fullNameResult.isError) {
       return currentState.copyWith(pageState: PageState.failure, errorMessage: 'Error Searching for User');
     } else {
-      List<MemberModel> seedsUsers = [];
-      List<MemberModel> telosUsers = [];
+      final List<MemberModel> seedsUsers = seedsMembersResult.asValue?.value ?? [];
+      final List<MemberModel> telosUsers = telosResult.asValue?.value ?? [];
+      final List<MemberModel> fullNameUsers = fullNameResult.asValue?.value ?? [];
 
-      if (seedsMembersResult.isValue) {
-        seedsUsers = seedsMembersResult.asValue?.value as List<MemberModel>;
-      }
+      final existingSet = <String>{};
+      final noShowSet = Set.from(noShowUsers ?? []);
 
-      if (telosResult.isValue) {
-        final seedsSet = Set.from(seedsUsers.map((e) => e.account));
-        telosUsers = telosResult.asValue?.value as List<MemberModel>;
-        telosUsers.removeWhere((element) => seedsSet.contains(element.account));
-      }
+      /// This is the order in which the list will display
+      /// - first account name matches
+      /// - second full name matches
+      /// - third Telos account name matches
+      final users = seedsUsers + fullNameUsers + telosUsers;
 
-      final users = seedsUsers + telosUsers;
-      if (noShowUsers != null) {
-        for (final noShowUser in noShowUsers) {
-          users.removeWhere((element) => element.account == noShowUser);
+      final uniqueUsers = <MemberModel>[];
+
+      for (final member in users) {
+        if (!(existingSet.contains(member.account) || noShowSet.contains(member.account))) {
+          uniqueUsers.add(member);
+          existingSet.add(member.account);
         }
       }
 
       if (currentState.showOnlyCitizenshipStatus != null) {
-        users.removeWhere((element) => element.citizenshipStatus != currentState.showOnlyCitizenshipStatus);
+        uniqueUsers.removeWhere((element) => element.citizenshipStatus != currentState.showOnlyCitizenshipStatus);
       }
 
-      return currentState.copyWith(pageState: PageState.success, users: users);
+      return currentState.copyWith(pageState: PageState.success, users: uniqueUsers);
     }
   }
 }
