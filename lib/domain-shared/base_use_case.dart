49c7c542db70d95e95689b6bdd6caf93401d1d9d
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-12-04 15:27:11 -0600

    Comments

--- lib/domain-shared/base_use_case.dart
+++ lib/domain-shared/base_use_case.dart
@@ -3,6 +3,14 @@ import 'package:async/async.dart';
 export 'package:async/src/result/error.dart';
 export 'package:async/src/result/result.dart';
 
+/// Use [InputUseCase] or [NoInputUseCase] whenever you want to interact with the system through
+/// CRUD (Create, Read, Update, Delete) data-related operations that also need to return a single data value back.
+/// <p>
+/// As the two use case alternatives suggest, consider [NoInputUseCase] if you don't need to pass any
+/// parameters to the use case at all, or [InputUseCase] otherwise.
+/// <p>
+/// When depending on multiple input parameters, define an private data class in the use case so this can be
+/// used as an Input parameter, as the example above. We need to remember that Generics have limitations.
 abstract class _BaseUseCase<T> {
   bool areAllResultsError(List<Result> results) {
     return results.where((Result element) => element.isValue).isEmpty;
