f2fba26de8e592c63e049fdba91376a2d008d1a6
Gerardo Guijarro <gguijarro_c@chewy.com>
2021-12-06 17:08:22 -0500

    Use case Architecture (#1373)
    * Use case Architecture
    
    * Input use case
    
    * Comments
    
    * Use case comment

--- lib/screens/profile_screens/profile/interactor/usecases/get_profile_values_use_case.dart
+++ lib/screens/profile_screens/profile/interactor/usecases/get_profile_values_use_case.dart
@@ -1,11 +1,22 @@
-import 'package:async/async.dart';
 import 'package:seeds/datasource/local/settings_storage.dart';
 import 'package:seeds/datasource/remote/api/profile_repository.dart';
+import 'package:seeds/datasource/remote/model/organization_model.dart';
+import 'package:seeds/datasource/remote/model/profile_model.dart';
+import 'package:seeds/datasource/remote/model/score_model.dart';
+import 'package:seeds/datasource/remote/model/transaction_response.dart';
+import 'package:seeds/domain-shared/base_use_case.dart';
 
-class GetProfileValuesUseCase {
+const int _profileResultIndex = 0;
+const int _contributionScoreResultIndex = 1;
+const int _organizationResultIndex = 2;
+const int _canResidentResultIndex = 3;
+const int _canCitizenResultIndex = 4;
+
+class GetProfileValuesUseCase extends NoInputUseCase<GetProfileValuesResponse> {
   final ProfileRepository _profileRepository = ProfileRepository();
 
-  Future<List<Result>> run() {
+  @override
+  Future<Result<GetProfileValuesResponse>> run() async {
     final account = settingsStorage.accountName;
     final futures = [
       _profileRepository.getProfile(account),
@@ -14,6 +25,37 @@ class GetProfileValuesUseCase {
       _profileRepository.canResident(account),
       _profileRepository.canCitizen(account),
     ];
-    return Future.wait(futures);
+
+    final List<Result> response = await Future.wait(futures);
+
+    if (areAllResultsError(response)) {
+      return Result.error("Error fetching Profile Data");
+    } else {
+      final GetProfileValuesResponse result = GetProfileValuesResponse(
+        profileModel: response[_profileResultIndex].valueOrNull,
+        scoreModel: response[_contributionScoreResultIndex].valueOrNull,
+        organizationModel: response[_organizationResultIndex].valueOrNull,
+        canResident: response[_canResidentResultIndex].valueOrNull,
+        canCitizen: response[_canCitizenResultIndex].valueOrNull,
+      );
+
+      return Result.value(result);
+    }
   }
+}
+
+class GetProfileValuesResponse {
+  final ProfileModel? profileModel;
+  final ScoreModel? scoreModel;
+  final List<OrganizationModel>? organizationModel;
+  final TransactionResponse? canResident;
+  final TransactionResponse? canCitizen;
+
+  GetProfileValuesResponse({
+    required this.profileModel,
+    required this.scoreModel,
+    required this.organizationModel,
+    required this.canResident,
+    required this.canCitizen,
+  });
 }
