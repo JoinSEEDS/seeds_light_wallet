7442041eb99937fae32d82fb8c59fc6f0733092a
gguij004 <51983923+gguij004@users.noreply.github.com>
2021-12-21 16:52:04 -0500

    1369 vouch for a member (#1422)
    * vouch for a member usecase and mapper
    
    * remove already vouched from list
    
    * fix comments
    
    * remove error from state
    
    * fix checks

--- lib/screens/explore_screens/vouch_for_a_member/vouch_for_a_member_screen.dart
+++ lib/screens/explore_screens/vouch_for_a_member/vouch_for_a_member_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:seeds/components/full_page_error_indicator.dart';
 import 'package:seeds/components/full_page_loading_indicator.dart';
 import 'package:seeds/components/search_user/search_user.dart';
 import 'package:seeds/components/snack_bar_info.dart';
+import 'package:seeds/datasource/remote/model/member_model.dart';
 import 'package:seeds/domain-shared/page_command.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/screens/explore_screens/vouch_for_a_member/components/vouch_for_member_confirmation_dialog.dart';
-import 'package:seeds/screens/explore_screens/vouch_for_a_member/components/vouch_success_dialog.dart';
 import 'package:seeds/screens/explore_screens/vouch_for_a_member/interactor/viewmodel/vouch_for_a_member_bloc.dart';
 import 'package:seeds/screens/explore_screens/vouch_for_a_member/interactor/viewmodel/vouch_for_a_member_page_commands.dart';
 
@@ -16,63 +15,59 @@ class VouchForAMemberScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => VouchForAMemberBloc(),
-      child: BlocConsumer<VouchForAMemberBloc, VouchForAMemberState>(
-        listenWhen: (_, current) => current.pageCommand != null,
-        listener: (context, state) {
-          final pageCommand = state.pageCommand;
+    final alreadyVouched = ModalRoute.of(context)?.settings.arguments as List<MemberModel>?;
 
-          if (pageCommand is ShowVouchForMemberConfirmation) {
-            showDialog<void>(
-              context: context,
-              barrierDismissible: false,
-              builder: (_) {
-                return BlocProvider.value(
-                  value: BlocProvider.of<VouchForAMemberBloc>(context),
-                  child: const VouchForMemberConfirmationDialog(),
-                );
-              },
-            );
-          } else if (pageCommand is ShowVouchForMemberSuccess) {
-            showDialog<void>(
-              context: context,
-              barrierDismissible: false,
-              builder: (_) {
-                return const VouchSuccessDialog();
-              },
-            );
-          } else if (pageCommand is ShowErrorMessage) {
-            SnackBarInfo(pageCommand.message, ScaffoldMessenger.of(context)).show();
-          }
-          BlocProvider.of<VouchForAMemberBloc>(context).add(const ClearPageCommand());
-        },
-        builder: (context, VouchForAMemberState state) {
-          switch (state.pageState) {
-            case PageState.loading:
-              return const Scaffold(body: FullPageLoadingIndicator());
-            case PageState.failure:
-              return const FullPageErrorIndicator();
-            case PageState.success:
-              return Scaffold(
-                appBar: AppBar(title: const Text("Vouch")),
-                body: Column(
+    return Scaffold(
+      appBar: AppBar(title: const Text("Vouch")),
+      body: BlocProvider(
+        create: (_) => VouchForAMemberBloc(alreadyVouched ?? []),
+        child: BlocConsumer<VouchForAMemberBloc, VouchForAMemberState>(
+          listenWhen: (_, current) => current.pageCommand != null,
+          listener: (context, state) {
+            final pageCommand = state.pageCommand;
+
+            if (pageCommand is ShowVouchForMemberConfirmation) {
+              showDialog<void>(
+                context: context,
+                barrierDismissible: false,
+                builder: (_) {
+                  return BlocProvider.value(
+                    value: BlocProvider.of<VouchForAMemberBloc>(context),
+                    child: const VouchForMemberConfirmationDialog(),
+                  );
+                },
+              );
+            } else if (pageCommand is ShowVouchForMemberSuccess) {
+              Navigator.of(context).pop(true);
+            } else if (pageCommand is ShowErrorMessage) {
+              SnackBarInfo(pageCommand.message, ScaffoldMessenger.of(context)).show();
+            }
+            BlocProvider.of<VouchForAMemberBloc>(context).add(const ClearPageCommand());
+          },
+          builder: (context, VouchForAMemberState state) {
+            switch (state.pageState) {
+              case PageState.loading:
+                return const FullPageLoadingIndicator();
+              case PageState.failure:
+              case PageState.success:
+                return Column(
                   children: [
                     Expanded(
                       child: SearchUser(
-                        noShowUsers: state.noShowUsers,
+                        noShowUsers: state.noShowUsers.toList(),
                         onUserSelected: (selectedUser) {
                           BlocProvider.of<VouchForAMemberBloc>(context).add(OnUserSelected(selectedUser));
                         },
                       ),
                     ),
                   ],
-                ),
-              );
-            default:
-              return const SizedBox.shrink();
-          }
-        },
+                );
+
+              default:
+                return const SizedBox.shrink();
+            }
+          },
+        ),
       ),
     );
   }
