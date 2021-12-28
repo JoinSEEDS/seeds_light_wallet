20173d81708f81530d79d0699fdd1f357e6d8b1f
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-12-28 10:25:14 -0600

    Recover account search bloc to 7.2 (#1421)

--- lib/screens/authentication/recover/recover_account_search/interactor/mappers/fetch_account_info_state_mapper.dart
+++ lib/screens/authentication/recover/recover_account_search/interactor/mappers/fetch_account_info_state_mapper.dart
@@ -2,10 +2,10 @@ import 'package:seeds/datasource/remote/model/member_model.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/domain-shared/result_to_state_mapper.dart';
 import 'package:seeds/i18n/authentication/recover/recover.i18n.dart';
-import 'package:seeds/screens/authentication/recover/recover_account_search/interactor/viewmodels/recover_account_state.dart';
+import 'package:seeds/screens/authentication/recover/recover_account_search/interactor/viewmodels/recover_account_search_bloc.dart';
 
 class FetchAccountInfoStateMapper extends StateMapper {
-  RecoverAccountState mapResultToState(RecoverAccountState currentState, Result userInfo, String userName) {
+  RecoverAccountSearchState mapResultToState(RecoverAccountSearchState currentState, Result userInfo, String userName) {
     if (userInfo.isError) {
       return currentState.copyWith(pageState: PageState.failure, errorMessage: "Error Loading Account".i18n);
     } else {
