39b74b01026ddb0d2143ba535d9a6bb6aeb929c5
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2021-12-30 17:59:26 -0600

    Pick image bloc to 7.2v (#1431)

--- lib/screens/profile_screens/profile/components/edit_profile_pic_bottom_sheet/edit_profile_pic_bottom_sheet.dart
+++ lib/screens/profile_screens/profile/components/edit_profile_pic_bottom_sheet/edit_profile_pic_bottom_sheet.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:seeds/i18n/profile_screens/profile/profile.i18n.dart';
-import 'package:seeds/screens/profile_screens/profile/components/edit_profile_pic_bottom_sheet/interactor/viewmodels/bloc.dart';
+import 'package:seeds/screens/profile_screens/profile/components/edit_profile_pic_bottom_sheet/interactor/viewmodels/pick_image_bloc.dart';
 
 class EditProfilePicBottomSheet extends StatelessWidget {
   const EditProfilePicBottomSheet({Key? key}) : super(key: key);
