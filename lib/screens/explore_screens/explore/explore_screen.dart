b365b6b1add536c9fbc79a67ca5bd097046e57cd
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2022-01-07 10:36:47 -0600

    Explore UI enhancement (#1468)
    * Explore screen UI enhancement
    
    * remove comment

--- lib/screens/explore_screens/explore/explore_screen.dart
+++ lib/screens/explore_screens/explore/explore_screen.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:seeds/constants/app_colors.dart';
 import 'package:seeds/datasource/local/settings_storage.dart';
 import 'package:seeds/domain-shared/app_constants.dart';
 import 'package:seeds/domain-shared/page_command.dart';
-import 'package:seeds/domain-shared/ui_constants.dart';
 import 'package:seeds/i18n/explore_screens/explore/explore.i18n.dart';
 import 'package:seeds/images/explore/invite_person.dart';
+import 'package:seeds/images/explore/p2p.dart';
 import 'package:seeds/images/explore/plant_seeds.dart';
+import 'package:seeds/images/explore/seeds_symbol.dart';
 import 'package:seeds/images/explore/vote.dart';
 import 'package:seeds/images/explore/vouch.dart';
 import 'package:seeds/navigation/navigation_service.dart';
 import 'package:seeds/screens/explore_screens/explore/components/explore_card.dart';
-import 'package:seeds/screens/explore_screens/explore/components/explore_link_card.dart';
 import 'package:seeds/screens/explore_screens/explore/interactor/viewmodels/explore_bloc.dart';
+import 'package:seeds/screens/explore_screens/explore/interactor/viewmodels/explore_item.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 /// Explore SCREEN
 class ExploreScreen extends StatelessWidget {
+  final List<ExploreItem> _exploreItems = const [
+    ExploreItem(
+      title: 'Invite a Friend',
+      icon:
+          Padding(padding: EdgeInsets.only(left: 6.0), child: CustomPaint(size: Size(40, 40), painter: InvitePerson())),
+      route: Routes.createInvite,
+    ),
+    ExploreItem(title: 'Vouch', icon: CustomPaint(size: Size(35, 41), painter: Vouch()), route: Routes.vouch),
+    ExploreItem(
+      title: 'Vote',
+      icon: Padding(padding: EdgeInsets.only(right: 6.0), child: CustomPaint(size: Size(40, 40), painter: Vote())),
+      route: Routes.vote,
+    ),
+    ExploreItem(
+      title: 'Plant Seeds',
+      icon: CustomPaint(size: Size(31, 41), painter: PlantSeeds()),
+      route: Routes.plantSeeds,
+    ),
+    ExploreItem(
+      title: 'Unplant Seeds',
+      icon: CustomPaint(size: Size(31, 41), painter: PlantSeeds()),
+      route: Routes.unPlantSeeds,
+    ),
+    ExploreItem(
+      title: 'P2P app',
+      icon: CustomPaint(size: Size(24, 24), painter: P2P()),
+      iconUseCircleBackground: false,
+      route: '',
+    ),
+    ExploreItem(
+      title: 'Get Seeds',
+      icon: CustomPaint(size: Size(9, 22), painter: SeedsSymbol()),
+      backgroundIconColor: AppColors.white,
+      gradient: LinearGradient(
+        colors: [AppColors.green1, AppColors.darkGreen2],
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+      ),
+      iconUseCircleBackground: false,
+      route: '',
+    ),
+  ];
   const ExploreScreen({Key? key}) : super(key: key);
 
   @override
@@ -35,95 +79,28 @@ class ExploreScreen extends StatelessWidget {
         builder: (context, _) {
           return Scaffold(
             appBar: AppBar(title: Text('Explore'.i18n)),
-            bottomSheet: Padding(
-              padding: const EdgeInsets.all(horizontalEdgePadding),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ExploreLinkCard(
-                      backgroundImage: 'assets/images/explore/get_seeds_card.jpg',
-                      onTap: () => launch('$urlBuySeeds${settingsStorage.accountName}', forceSafariVC: false),
-                    ),
-                  ),
-                  const Expanded(child: SizedBox.shrink()),
-                ],
-              ),
-            ),
-            body: ListView(
-              padding: const EdgeInsets.all(horizontalEdgePadding),
+            body: GridView.count(
+              padding: const EdgeInsets.all(18),
+              crossAxisSpacing: 18,
+              mainAxisSpacing: 18,
+              crossAxisCount: 2,
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: ExploreCard(
-                        onTap: () {
-                          BlocProvider.of<ExploreBloc>(context).add(const OnExploreCardTapped(Routes.createInvite));
-                        },
-                        title: 'Invite a Friend'.i18n,
-                        icon: const Padding(
-                          padding: EdgeInsets.only(left: 6.0),
-                          child: CustomPaint(size: Size(40, 40), painter: InvitePerson()),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    const Expanded(
-                      child: SizedBox.shrink(),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: ExploreCard(
-                        onTap: () {
-                          BlocProvider.of<ExploreBloc>(context).add(const OnExploreCardTapped(Routes.vouch));
-                        },
-                        title: 'Vouch'.i18n,
-                        icon: const CustomPaint(size: Size(31, 41), painter: Vouch()),
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Expanded(
-                      child: ExploreCard(
-                        onTap: () {
-                          BlocProvider.of<ExploreBloc>(context).add(const OnExploreCardTapped(Routes.vote));
-                        },
-                        title: 'Vote'.i18n,
-                        icon: const Padding(
-                          padding: EdgeInsets.only(right: 6.0),
-                          child: CustomPaint(size: Size(40, 40), painter: Vote()),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: ExploreCard(
-                        onTap: () {
-                          BlocProvider.of<ExploreBloc>(context).add(const OnExploreCardTapped(Routes.plantSeeds));
-                        },
-                        title: 'Plant Seeds'.i18n,
-                        icon: const CustomPaint(size: Size(31, 41), painter: PlantSeeds()),
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Expanded(
-                      child: ExploreCard(
-                        onTap: () {
-                          BlocProvider.of<ExploreBloc>(context).add(const OnExploreCardTapped(Routes.unPlantSeeds));
-                        },
-                        title: 'Unplant Seeds'.i18n,
-                        icon: const CustomPaint(size: Size(31, 41), painter: PlantSeeds()),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 150),
+                for (final i in _exploreItems)
+                  ExploreCard(
+                    title: i.title.i18n,
+                    icon: i.icon,
+                    backgroundIconColor: i.backgroundIconColor,
+                    iconUseCircleBackground: i.iconUseCircleBackground,
+                    backgroundImage: i.backgroundImage,
+                    gradient: i.gradient,
+                    onTap: () {
+                      if (i.route.isNotEmpty) {
+                        BlocProvider.of<ExploreBloc>(context).add(const OnExploreCardTapped(Routes.createInvite));
+                      } else {
+                        launch('$urlBuySeeds${settingsStorage.accountName}', forceSafariVC: false);
+                      }
+                    },
+                  )
               ],
             ),
           );
