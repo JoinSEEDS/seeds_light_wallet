c9760ff421ac5fca61db0b2ddf04eeef69a45e42
Nikolaus Heger <nheger@gmail.com>
2022-01-18 13:37:37 +0800

    Seeds Global Passport compatible 12 words key generation (#1488)
    * Added test for Seeds Global Passport words
    
    Added stub for conversion
    
    * helper comments
    
    * merge mnemonic tests
    
    * remove unused code
    
    * remove unused file
    
    * added code for global passport compatible 12 words

--- test/utils/mnemonic_test.dart
+++ test/utils/mnemonic_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:seeds/datasource/local/auth_service.dart';
 import 'package:seeds/utils/mnemonic_code/mnemonic_code.dart';
 
 void main() {
@@ -15,6 +16,21 @@ void main() {
       print("secret2: $secret2");
 
       expect(secret, secret2);
+    });
+  });
+
+  group('Seeds Global Passport Mnemonic', () {
+    test('Convert words from Seeds Global Passport', () async {
+      final mnemonic = "upon what runway husband grief bomb evoke bicycle episode century crystal jazz";
+      final expectedPrivateKey = "5HpYGg6hfeD2a16PUoAf22AMzPFUuqDpBCMSbYGRmeDppJ8dyM9";
+      final expectedPublicKey = "EOS5DmdNG4mv2tFrJY1yqctExqa34G2anXfaQ5sXeAwZTSnRFVH6Y";
+
+      final authService = AuthService();
+
+      final eosKey = authService.createPrivateKeyFrom12WordsBip39(mnemonic.split(' '));
+
+      expect(eosKey.toString(), expectedPrivateKey);
+      expect(eosKey.toEOSPublicKey().toString(), expectedPublicKey);
     });
   });
 }
