e88cb345b998ef08d5b02613250c7ccf010910f7
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-01-20 02:52:43 -0500

    Issue 1470 2 f 2 - Complete contribution details flow (#1490)
    * add subtitle component
    
    * detail screen UI
    
    * add inkwell
    
    * add clear page command
    
    * add spacer
    
    * address gerys cmments

--- lib/screens/profile_screens/contribution/interactor/viewmodels/scores_view_model.dart
+++ lib/screens/profile_screens/contribution/interactor/viewmodels/scores_view_model.dart
@@ -1,6 +1,25 @@
 import 'package:equatable/equatable.dart';
 import 'package:seeds/datasource/remote/model/score_model.dart';
 
+enum ScoreType { contributionScore, communityScore, reputationScore, plantedScore, transactionScore }
+
+extension ScoreTypeExtension on ScoreType {
+  String get value {
+    switch (this) {
+      case ScoreType.contributionScore:
+        return 'Contribution';
+      case ScoreType.communityScore:
+        return 'Community';
+      case ScoreType.reputationScore:
+        return 'Reputation';
+      case ScoreType.plantedScore:
+        return 'Planted Seeds';
+      case ScoreType.transactionScore:
+        return 'Transaction';
+    }
+  }
+}
+
 class ScoresViewModel extends Equatable {
   final ScoreModel? contributionScore;
   final ScoreModel? communityScore;
