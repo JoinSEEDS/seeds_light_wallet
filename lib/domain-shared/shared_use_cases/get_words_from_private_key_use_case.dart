84cd718f68c276a2626f6ce091de33e0ea72706f
NIK <nheger@gmail.com>
2022-01-25 18:57:53 +0800

    update to handle passport 12 words phrases

--- lib/domain-shared/shared_use_cases/get_words_from_private_key_use_case.dart
+++ lib/domain-shared/shared_use_cases/get_words_from_private_key_use_case.dart
@@ -1,28 +1,29 @@
 import 'package:seeds/datasource/local/settings_storage.dart';
 import 'package:seeds/screens/authentication/import_key/interactor/usecases/generate_key_from_recovery_words_use_case.dart';
+import 'package:seeds/screens/authentication/import_key/interactor/usecases/generate_key_from_seeds_passport_words_use_case.dart';
 
 class GetWordsFromPrivateKey {
   /// If the private key has words, it returns the list of words
-  /// separated by a dash (-), otherwise an empty list.
+  /// otherwise an empty list.
   List<String> run() {
     final String privateKey = settingsStorage.privateKey!;
 
-    ///-------IS PRIVATE KEY 12 WORDS TYPE
-    // Convert all words into private keys
-    final List<String> privateKeysListFromWords = settingsStorage.recoveryWords
-        .map((i) => GenerateKeyFromRecoveryWordsUseCase().run(i.split('-')))
-        .toList()
-        .map((i) => '${i.eOSPrivateKey}')
-        .toList();
-    // Verify if there is a match with the selected private key
-    final bool is12WordsAccount = privateKeysListFromWords.contains(privateKey);
-    if (is12WordsAccount) {
-      final int wordsIndex = privateKeysListFromWords.indexOf(privateKey);
-      // Since the storage recover words and privateKeysListFromWords are map 1:1
-      // the index will be the same to get the target words
-      return settingsStorage.recoveryWords[wordsIndex].split('-');
+    final wordsString = settingsStorage.recoveryWords.firstWhere((item) {
+      final words = item.toList();
+      return GenerateKeyFromRecoveryWordsUseCase().run(words).eOSPrivateKey.toString() == privateKey ||
+          GenerateKeyFromSeedsPassportWordsUseCase().run(words).eOSPrivateKey.toString() == privateKey;
+    });
+
+    if (wordsString.isNotEmpty) {
+      return wordsString.toList();
     } else {
       return [];
     }
+  }
+}
+
+extension WordListString on String {
+  List<String> toList() {
+    return split("-");
   }
 }
