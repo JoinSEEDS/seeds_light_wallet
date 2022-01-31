caa4b390101a092942e09ab1aaf6dcc650d0e774
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2022-01-31 14:21:55 -0600

    Detect internet connection feature (#1538)
    * first advance
    
    * Second advance

--- lib/blocs/root/root_state.dart
+++ lib/blocs/root/root_state.dart
@@ -2,15 +2,21 @@ part of 'root_bloc.dart';
 
 class RootState extends Equatable {
   final BusEvent? busEvent;
+  final InternetConnectionStatus internetConnectionStatus;
 
-  const RootState({this.busEvent});
+  const RootState({this.busEvent, required this.internetConnectionStatus});
 
   @override
   List<Object?> get props => [busEvent];
 
-  RootState copyWith({BusEvent? busEvent}) {
-    return RootState(busEvent: busEvent);
+  RootState copyWith({BusEvent? busEvent, InternetConnectionStatus? internetConnectionStatus}) {
+    return RootState(
+      busEvent: busEvent,
+      internetConnectionStatus: internetConnectionStatus ?? this.internetConnectionStatus,
+    );
   }
 
-  factory RootState.initial() => const RootState();
+  factory RootState.initial() {
+    return const RootState(internetConnectionStatus: InternetConnectionStatus.connected);
+  }
 }
