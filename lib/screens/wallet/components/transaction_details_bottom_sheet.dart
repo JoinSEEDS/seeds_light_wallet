10f0261e71011405b22267d4424000142a10a07b
Raul <raul.urtecho512@gmail.com>
2022-02-09 21:18:05 -0600

    fix for small devices

--- lib/screens/wallet/components/transaction_details_bottom_sheet.dart
+++ lib/screens/wallet/components/transaction_details_bottom_sheet.dart
@@ -30,8 +30,6 @@ class TransactionDetailsBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final account = settingsStorage.accountName;
-    final memo =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.";
     return BlocProvider(
       create: (_) =>
           MemberBloc(transaction.to == account ? transaction.from : transaction.to)..add(const OnLoadMemberData()),
@@ -46,80 +44,89 @@ class TransactionDetailsBottomSheet extends StatelessWidget {
             case PageState.success:
               return Stack(
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const SizedBox(height: 12.0),
-                      Text(
-                        DateFormat("EEE dd MMM y kk:mm:ss").format(transaction.timestamp),
-                        style: Theme.of(context).textTheme.subtitle2HighEmphasis,
-                      ),
-                      const SizedBox(height: 16.0),
-                      ProfileAvatar(
-                        size: 60,
-                        account: state.currentAccount,
-                        nickname: state.localizedDisplayName(context),
-                        image: state.profileImageURL,
-                        decoration: const BoxDecoration(shape: BoxShape.circle, color: AppColors.lightGreen2),
-                      ),
-                      const SizedBox(height: 16.0),
-                      Column(
-                        children: [
-                          Text(
-                            state.localizedDisplayName(context),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context).textTheme.headline7,
-                          ),
-                          const SizedBox(height: 8.0),
-                          Text(
-                            state.currentAccount,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: Theme.of(context).textTheme.subtitle2OpacityEmphasis,
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 20.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          if (account == transaction.to)
-                            Text('+', style: Theme.of(context).textTheme.subtitle1Green1)
-                          else
-                            Text('-', style: Theme.of(context).textTheme.subtitle1Red2),
-                          const SizedBox(width: 4),
-                          Text(transaction.quantity.seedsFormatted, style: Theme.of(context).textTheme.headline5)
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
-                        child: Text(
-                          "Memo: ${transaction.memo.isEmpty ? '---' : memo}",
-                          maxLines: 6, // <-- memo has a max of 256 characteres
-                          textAlign: TextAlign.center,
-                          overflow: TextOverflow.ellipsis,
+                  SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(height: 12.0),
+                        Text(
+                          DateFormat("EEE dd MMM y kk:mm:ss").format(transaction.timestamp),
+                          style: Theme.of(context).textTheme.subtitle2HighEmphasis,
                         ),
-                      ),
-                      InkWell(
-                        customBorder: const CircleBorder(),
-                        onTap: () => Share.share('https://telos.bloks.io/transaction/${transaction.transactionId}'),
-                        child: Container(
-                          padding: const EdgeInsets.all(16),
-                          decoration: const BoxDecoration(color: AppColors.green1, shape: BoxShape.circle),
-                          child: SvgPicture.asset('assets/images/wallet/share_transaction_id.svg'),
+                        const SizedBox(height: 16.0),
+                        ProfileAvatar(
+                          size: 60,
+                          account: state.currentAccount,
+                          nickname: state.localizedDisplayName(context),
+                          image: state.profileImageURL,
+                          decoration: const BoxDecoration(shape: BoxShape.circle, color: AppColors.lightGreen2),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 16.0),
+                        Column(
+                          children: [
+                            Text(
+                              state.localizedDisplayName(context),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: Theme.of(context).textTheme.headline7,
+                            ),
+                            const SizedBox(height: 8.0),
+                            Text(
+                              state.currentAccount,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: Theme.of(context).textTheme.subtitle2OpacityEmphasis,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            if (account == transaction.to)
+                              Text('+', style: Theme.of(context).textTheme.subtitle1Green1)
+                            else
+                              Text('-', style: Theme.of(context).textTheme.subtitle1Red2),
+                            const SizedBox(width: 4),
+                            Text(transaction.quantity.seedsFormatted, style: Theme.of(context).textTheme.headline5)
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 40.0),
+                          child: Text(
+                            "Memo: ${transaction.memo.isEmpty ? '---' : transaction.memo}",
+                            maxLines: 6, // <-- memo has a max of 256 characteres
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        InkWell(
+                          customBorder: const CircleBorder(),
+                          onTap: () => Share.share('https://telos.bloks.io/transaction/${transaction.transactionId}'),
+                          child: Container(
+                            padding: const EdgeInsets.all(16),
+                            decoration: const BoxDecoration(color: AppColors.green1, shape: BoxShape.circle),
+                            child: SvgPicture.asset('assets/images/wallet/share_transaction_id.svg'),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   Align(
                     alignment: Alignment.bottomCenter,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Image.asset('assets/images/wallet/leaves_bttm_left.png'),
-                        Image.asset('assets/images/wallet/leaves_bttm_right.png'),
+                        Image.asset(
+                          'assets/images/wallet/leaves_bttm_left.png',
+                          width: MediaQuery.of(context).size.width * 0.45,
+                        ),
+                        Image.asset(
+                          'assets/images/wallet/leaves_bttm_right.png',
+                          width: MediaQuery.of(context).size.width * 0.45,
+                        ),
                       ],
                     ),
                   ),
