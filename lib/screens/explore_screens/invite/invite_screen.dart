142ebff29b94d06c4428c325adcca9af3fc500fc
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-02-18 03:55:07 -0500

    clear imports

--- lib/screens/explore_screens/invite/invite_screen.dart
+++ lib/screens/explore_screens/invite/invite_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/blocs/rates/viewmodels/rates_bloc.dart';
