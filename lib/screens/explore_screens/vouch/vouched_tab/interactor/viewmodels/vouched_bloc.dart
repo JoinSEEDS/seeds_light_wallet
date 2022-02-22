ff3d8915f33db79653c73579fe03e34204c2e7d5
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-02-22 16:17:01 -0500

    load citizenship status on vouch screen (#1606)
    * load citizenship status from chain
    
    * add base use case
    
    * remove logic from bloc
    
    * remove import
    
    * push
    
    * organize use cases
    
    * fix lint

--- lib/screens/explore_screens/vouch/vouched_tab/interactor/viewmodels/vouched_bloc.dart
+++ lib/screens/explore_screens/vouch/vouched_tab/interactor/viewmodels/vouched_bloc.dart
@@ -1,12 +1,11 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:seeds/datasource/local/settings_storage.dart';
 import 'package:seeds/datasource/remote/model/profile_model.dart';
 import 'package:seeds/domain-shared/page_command.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/domain-shared/result_to_state_mapper.dart';
-import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/mappers/load_vouched_list_state_mapper.dart';
-import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/usecases/load_vouched_list_use_case.dart';
+import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/mappers/vouched_initial_data_state_mapper.dart';
+import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/usecases/get_vouched_initial_data_use_case.dart';
 import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/viewmodels/vouched_page_commands.dart';
 
 part 'vouched_event.dart';
@@ -14,21 +13,18 @@ part 'vouched_event.dart';
 part 'vouched_state.dart';
 
 class VouchedBloc extends Bloc<VouchedEvent, VouchedState> {
-  VouchedBloc() : super(VouchedState.initial(settingsStorage.isVisitor)) {
+  VouchedBloc() : super(VouchedState.initial()) {
     on<LoadUserVouchedList>(_loadUserVouchedList);
     on<ClearPageCommand>((_, emit) => emit(state.copyWith()));
   }
 
   Future<void> _loadUserVouchedList(LoadUserVouchedList event, Emitter<VouchedState> emit) async {
-    if (!state.canVouch) {
-      emit(state.copyWith(pageState: PageState.success, pageCommand: ShowNotQualifiedToVouch()));
-    } else {
-      event.showVouchForMemberSuccess
-          ? emit(state.copyWith(pageState: PageState.loading, pageCommand: ShowVouchForMemberSuccess()))
-          : emit(state.copyWith(pageState: PageState.loading));
-
-      final Result<List<ProfileModel>> results = await LoadVouchedListUseCase().run();
-      emit(LoadVouchedListStateMapper().mapResultToState(state, results));
+    if (event.showVouchForMemberSuccess) {
+      emit(state.copyWith(pageState: PageState.loading, pageCommand: ShowVouchForMemberSuccess()));
     }
+
+    emit(state.copyWith(pageState: PageState.loading));
+    final Result<VouchedDto> result = await GetVouchedInitialDataUseCase().run();
+    emit(VouchedInitialDataStateMapper().mapResultToState(state, result));
   }
 }
