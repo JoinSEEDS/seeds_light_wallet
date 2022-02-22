3410cd92dceb2c4d8a5ce387511811c33abb9b63
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-02-22 13:34:15 -0500

    organize use cases

--- lib/screens/explore_screens/vouch/vouched_tab/interactor/viewmodels/vouched_bloc.dart
+++ lib/screens/explore_screens/vouch/vouched_tab/interactor/viewmodels/vouched_bloc.dart
@@ -4,10 +4,8 @@ import 'package:seeds/datasource/remote/model/profile_model.dart';
 import 'package:seeds/domain-shared/page_command.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/domain-shared/result_to_state_mapper.dart';
-import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/mappers/get_user_citizenship_status_mapper.dart';
-import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/mappers/load_vouched_list_state_mapper.dart';
-import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/usecases/get_user_citizenship_status_use_case.dart';
-import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/usecases/load_vouched_list_use_case.dart';
+import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/mappers/vouched_initial_data_state_mapper.dart';
+import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/usecases/get_vouched_initial_data_use_case.dart';
 import 'package:seeds/screens/explore_screens/vouch/vouched_tab/interactor/viewmodels/vouched_page_commands.dart';
 
 part 'vouched_event.dart';
@@ -26,10 +24,7 @@ class VouchedBloc extends Bloc<VouchedEvent, VouchedState> {
     }
 
     emit(state.copyWith(pageState: PageState.loading));
-    final Result<ProfileModel> result = await GetUserCitizenshipStatusUseCase().run();
-    emit(GetUserCitizenshipStatusStateMapper().mapResultToState(state, result));
-
-    final Result<List<ProfileModel>> results = await LoadVouchedListUseCase().run();
-    emit(LoadVouchedListStateMapper().mapResultToState(state, results));
+    final Result<VouchedDto> result = await GetVouchedInitialDataUseCase().run();
+    emit(VouchedInitialDataStateMapper().mapResultToState(state, result));
   }
 }
