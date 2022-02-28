2dabefbb86b485581bdba855a9866930482e8b71
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-02-28 16:45:12 -0500

    Upload region Image UI [4/5] (#1651)
    * Select region screen
    
    * add padding
    
    * revert change to navigation
    
    * add ui
    
    * remove dependency

---
+++ lib/components/dotted_border/dash_painter.dart
@@ -0,0 +1,135 @@
+part of 'dotted_border.dart';
+
+typedef PathBuilder = Path Function(Size);
+
+class _DashPainter extends CustomPainter {
+  final double strokeWidth;
+  final List<double> dashPattern;
+  final Color color;
+  final BorderType borderType;
+  final Radius radius;
+  final StrokeCap strokeCap;
+  final PathBuilder? customPath;
+
+  _DashPainter({
+    this.strokeWidth = 2,
+    this.dashPattern = const <double>[3, 1],
+    this.color = Colors.black,
+    this.borderType = BorderType.rect,
+    this.radius = Radius.zero,
+    this.strokeCap = StrokeCap.butt,
+    this.customPath,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..strokeWidth = strokeWidth
+      ..color = color
+      ..strokeCap = strokeCap
+      ..style = PaintingStyle.stroke;
+
+    Path _path;
+    if (customPath != null) {
+      _path = dashPath(
+        customPath!(size),
+        dashArray: CircularIntervalList(dashPattern),
+      );
+    } else {
+      _path = _getPath(size);
+    }
+
+    canvas.drawPath(_path, paint);
+  }
+
+  /// Returns a [Path] based on the the [borderType] parameter
+  Path _getPath(Size size) {
+    Path path;
+    switch (borderType) {
+      case BorderType.circle:
+        path = _getCirclePath(size);
+        break;
+      case BorderType.rRect:
+        path = _getRRectPath(size, radius);
+        break;
+      case BorderType.rect:
+        path = _getRectPath(size);
+        break;
+      case BorderType.oval:
+        path = _getOvalPath(size);
+        break;
+    }
+
+    return dashPath(path, dashArray: CircularIntervalList(dashPattern));
+  }
+
+  /// Returns a circular path of [size]
+  Path _getCirclePath(Size size) {
+    final double w = size.width;
+    final double h = size.height;
+    final double s = size.shortestSide;
+
+    return Path()
+      ..addRRect(
+        RRect.fromRectAndRadius(
+          Rect.fromLTWH(
+            w > s ? (w - s) / 2 : 0,
+            h > s ? (h - s) / 2 : 0,
+            s,
+            s,
+          ),
+          Radius.circular(s / 2),
+        ),
+      );
+  }
+
+  /// Returns a Rounded Rectangular Path with [radius] of [size]
+  Path _getRRectPath(Size size, Radius radius) {
+    return Path()
+      ..addRRect(
+        RRect.fromRectAndRadius(
+          Rect.fromLTWH(
+            0,
+            0,
+            size.width,
+            size.height,
+          ),
+          radius,
+        ),
+      );
+  }
+
+  /// Returns a path of [size]
+  Path _getRectPath(Size size) {
+    return Path()
+      ..addRect(
+        Rect.fromLTWH(
+          0,
+          0,
+          size.width,
+          size.height,
+        ),
+      );
+  }
+
+  /// Return an oval path of [size]
+  Path _getOvalPath(Size size) {
+    return Path()
+      ..addOval(
+        Rect.fromLTWH(
+          0,
+          0,
+          size.width,
+          size.height,
+        ),
+      );
+  }
+
+  @override
+  bool shouldRepaint(_DashPainter oldDelegate) {
+    return oldDelegate.strokeWidth != strokeWidth ||
+        oldDelegate.color != color ||
+        oldDelegate.dashPattern != dashPattern ||
+        oldDelegate.borderType != borderType;
+  }
+}
