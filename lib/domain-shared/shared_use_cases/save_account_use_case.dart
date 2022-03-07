243765684b8b0ee21d8cd18b22c071c4cf684c47
home <home@MacBook-Pro-de-home.local>
2022-03-06 21:17:20 -0600

    Clear Webview cache on logout and switch account

--- lib/domain-shared/shared_use_cases/save_account_use_case.dart
+++ lib/domain-shared/shared_use_cases/save_account_use_case.dart
@@ -1,11 +1,13 @@
 import 'package:seeds/datasource/local/models/auth_data_model.dart';
 import 'package:seeds/datasource/local/settings_storage.dart';
+import 'package:seeds/datasource/local/web_view_cache_service.dart';
 import 'package:seeds/domain-shared/shared_use_cases/account_use_case.dart';
 
 class SaveAccountUseCase extends AccountUseCase {
   Future<void> run({required String accountName, required AuthDataModel authData}) async {
     final String oldAccountName = settingsStorage.accountName;
     await settingsStorage.saveAccount(accountName, authData);
+    await const WebViewCacheService().clearCache();
     await updateFirebaseToken(oldAccount: oldAccountName, newAccount: accountName);
   }
 }
