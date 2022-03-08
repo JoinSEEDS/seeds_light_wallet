b824e668256300cc18d5e557846bacd2ef6a11a1
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2022-03-08 08:36:02 -0600

    Join region 1 (#1671)
    Co-authored-by: home <home@MacBook-Pro-de-home.local>

--- lib/screens/explore_screens/regions_screens/join_region/join_region_screen.dart
+++ lib/screens/explore_screens/regions_screens/join_region/join_region_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:seeds/components/flat_button_long.dart';
 import 'package:seeds/components/regions_map/regions_map.dart';
+import 'package:seeds/design/app_colors.dart';
+import 'package:seeds/design/app_theme.dart';
 import 'package:seeds/utils/build_context_extension.dart';
 
 class JoinRegionScreen extends StatelessWidget {
@@ -8,8 +11,36 @@ class JoinRegionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(title: Text(context.loc.joinRegionAppBarTitle)),
-      body: const RegionsMap(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 20.0),
+          child: Column(
+            children: [
+              const Text('Search for your closest region by inputting your address or nearest city.'),
+              const SizedBox(height: 20.0),
+              SizedBox(height: MediaQuery.of(context).size.height / 3, child: const RegionsMap()),
+              const Spacer(),
+              RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    const TextSpan(text: 'Select'),
+                    TextSpan(
+                        text: ' Create New ',
+                        style: Theme.of(context).textTheme.buttonWhiteL.copyWith(color: AppColors.canopy)),
+                    const TextSpan(
+                        text:
+                            'Select if you do not see your region and would like to start your own. Heads up, you’ll need 1,000 seeds to create a region!'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              FlatButtonLong(title: 'Create new', onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
