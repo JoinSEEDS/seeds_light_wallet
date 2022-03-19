cb4ce148bd761b295bbe44346e4ddbbbfd430a28
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-03-19 11:56:14 -0400

    change build number (#1691)

--- android/app/src/main/kotlin/com/joinseeds/seedswallet/MainActivity.kt
+++ android/app/src/main/kotlin/com/joinseeds/seedswallet/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.joinseeds.seedswallet
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+class MainActivity : FlutterActivity() {
 }
