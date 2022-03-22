1254939957461c5442f0b962b8e16c16d4356377
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-03-22 09:20:28 -0400

    add ui

--- lib/screens/create_region_screens/review_region_information/review_region_information.dart
+++ lib/screens/create_region_screens/review_region_information/review_region_information.dart
@@ -3,14 +3,40 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/components/flat_button_long.dart';
 import 'package:seeds/components/full_page_error_indicator.dart';
 import 'package:seeds/components/full_page_loading_indicator.dart';
+import 'package:seeds/design/app_colors.dart';
+import 'package:seeds/design/app_theme.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/screens/create_region_screens/interactor/viewmodels/create_region_bloc.dart';
+import 'package:seeds/screens/create_region_screens/review_region_information/components/review_region_header.dart';
 import 'package:seeds/utils/build_context_extension.dart';
 
-// TODO(gguij004): No UI for this page yet.
-
-class ReviewRegion extends StatelessWidget {
+class ReviewRegion extends StatefulWidget {
   const ReviewRegion({Key? key}) : super(key: key);
+
+  @override
+  _ReviewRegionState createState() => _ReviewRegionState();
+}
+
+class _ReviewRegionState extends State<ReviewRegion> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 3, initialIndex: 2);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void onTap() {
+    setState(() {
+      _tabController.index = 2;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +60,37 @@ class ReviewRegion extends StatelessWidget {
                   bottomNavigationBar: SafeArea(
                       minimum: const EdgeInsets.all(16),
                       child: FlatButtonLong(
-                          title: "Create Region",
+                          title: "Publish",
                           onPressed: () =>
                               BlocProvider.of<CreateRegionBloc>(context).add(const OnCreateRegionTapped()))),
-                  body: SafeArea(minimum: const EdgeInsets.all(16), child: Column())));
+                  body: SafeArea(
+                      child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const ReviewRegionHeader(),
+                        SizedBox(
+                          height: 50,
+                          child: AppBar(
+                            shape: const Border(bottom: BorderSide(color: AppColors.lightGreen2)),
+                            bottom: TabBar(
+                              controller: _tabController,
+                              physics: const NeverScrollableScrollPhysics(),
+                              onTap: (value) => onTap(),
+                              indicatorWeight: 4.0,
+                              unselectedLabelStyle: Theme.of(context).textTheme.buttonOpacityEmphasis,
+                              labelStyle: Theme.of(context).textTheme.buttonLowEmphasis,
+                              tabs: [
+                                const Tab(child: FittedBox(child: Text("Chat"))),
+                                const Tab(child: FittedBox(child: Text("Events"))),
+                                const Tab(child: FittedBox(child: Text("About"))),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Padding(padding: const EdgeInsets.all(16), child: Text(state.regionDescription))
+                      ],
+                    ),
+                  ))));
         default:
           return const SizedBox.shrink();
       }
