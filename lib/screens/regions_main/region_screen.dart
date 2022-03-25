c1be9ce631616a16b154f355b1c77d0606ff2603
Raul <raul.urtecho512@gmail.com>
2022-03-24 19:39:53 -0600

    Check, Join region logic and navigation

--- lib/screens/regions_main/region_screen.dart
+++ lib/screens/regions_main/region_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/screens/regions_main/components/tab_about.dart';
 import 'package:seeds/screens/regions_main/components/tab_events.dart';
 import 'package:seeds/screens/regions_main/components/tab_messages.dart';
+import 'package:seeds/screens/regions_main/interactor/viewmodel/region_bloc.dart';
 
 class RegionScreen extends StatefulWidget {
   const RegionScreen({Key? key}) : super(key: key);
@@ -13,44 +15,53 @@ class RegionScreen extends StatefulWidget {
 class _RegionScreenState extends State<RegionScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: DefaultTabController(
-        length: 3,
-        child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                actions: [
-                  TextButton(
-                    child: const Text("Join"),
-                    onPressed: () {},
-                  )
-                ],
-                expandedHeight: 200.0,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                    centerTitle: false,
-                    title: const Text("state.regionAddress"),
-                    background: Image.network(
-                      "https://picsum.photos/400/300",
-                      fit: BoxFit.cover,
-                    )),
+    final regionId = ModalRoute.of(context)!.settings.arguments as String?;
+    return BlocProvider(
+      create: (_) => RegionBloc(regionId ?? '', regionId != null),
+      child: Scaffold(
+        body: BlocBuilder<RegionBloc, RegionState>(
+          builder: (context, state) {
+            return DefaultTabController(
+              length: 3,
+              child: NestedScrollView(
+                headerSliverBuilder: (context, isInnerBoxScrolled) {
+                  return [
+                    SliverAppBar(
+                      actions: [
+                        if (state.isBrowseView)
+                          TextButton(
+                            child: const Text("Join"),
+                            onPressed: () {},
+                          )
+                      ],
+                      expandedHeight: 200.0,
+                      pinned: true,
+                      flexibleSpace: FlexibleSpaceBar(
+                          centerTitle: false,
+                          title: const Text("state.regionAddress"),
+                          background: Image.network(
+                            "https://picsum.photos/400/300",
+                            fit: BoxFit.cover,
+                          )),
+                    ),
+                    SliverPersistentHeader(
+                      delegate: _SliverAppBarDelegate(
+                        const TabBar(
+                          tabs: [
+                            Tab(text: "Chat"),
+                            Tab(text: "Events"),
+                            Tab(text: "About"),
+                          ],
+                        ),
+                      ),
+                      pinned: true,
+                    ),
+                  ];
+                },
+                body: const SafeArea(child: TabBarView(children: [MessagesTab(), EventsTab(), AboutTab()])),
               ),
-              SliverPersistentHeader(
-                delegate: _SliverAppBarDelegate(
-                  const TabBar(
-                    tabs: [
-                      Tab(text: "Chat"),
-                      Tab(text: "Events"),
-                      Tab(text: "About"),
-                    ],
-                  ),
-                ),
-                pinned: true,
-              ),
-            ];
+            );
           },
-          body: const SafeArea(child: TabBarView(children: [MessagesTab(), EventsTab(), AboutTab()])),
         ),
       ),
     );
