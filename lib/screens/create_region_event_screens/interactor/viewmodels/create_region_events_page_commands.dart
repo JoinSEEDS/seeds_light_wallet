946426713c296469774b1fefa7aa8262cf72900a
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-03-29 18:37:05 -0400

    clean code

--- lib/screens/create_region_event_screens/interactor/viewmodels/create_region_events_page_commands.dart
+++ lib/screens/create_region_event_screens/interactor/viewmodels/create_region_events_page_commands.dart
@@ -5,7 +5,3 @@ class OnRegionEventCreated extends PageCommand {}
 class ReturnToRegionScreen extends PageCommand {}
 
 class RemoveAuthenticationScreen extends PageCommand {}
-
-class ShowPickDate extends PageCommand {}
-
-class ShowPickTime extends PageCommand {}
