f749f57fcf878f3e7cbeee02f299652039bd68bb
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-03-28 21:18:37 -0400

    event review and publish ui

---
+++ lib/screens/create_region_event_screens/components/review_and_publish_region_event_bottom.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:seeds/screens/create_region_event_screens/interactor/viewmodels/create_region_event_bloc.dart';
+
+class ReviewAndPublishRegionEventBottom extends StatelessWidget {
+  const ReviewAndPublishRegionEventBottom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CreateRegionEventBloc, CreateRegionEventState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Details", style: Theme.of(context).textTheme.headline6),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  const Icon(Icons.location_on_outlined),
+                  Flexible(child: Text(state.currentPlace!.placeText, overflow: TextOverflow.ellipsis)),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Text(state.eventDescription, style: Theme.of(context).textTheme.subtitle2)
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
