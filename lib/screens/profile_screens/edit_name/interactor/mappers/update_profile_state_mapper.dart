e64c2d94a79bec1043bbad2de71d983b231812f9
Gerardo Guijarro <gguijarro_c@chewy.com>
2022-03-30 12:54:23 -0400

    Refactor edit name (#1733)
    * Refactor edit name
    
    * Fix merge conflicts
    
    * Lint
    
    * Lint
    
    * Better handle loading, and errir
    
    * One line instruction
    
    * One line instruction

--- lib/screens/profile_screens/edit_name/interactor/mappers/update_profile_state_mapper.dart
+++ lib/screens/profile_screens/edit_name/interactor/mappers/update_profile_state_mapper.dart
@@ -1,15 +1,21 @@
-import 'package:seeds/datasource/remote/model/transaction_response.dart';
+import 'package:seeds/domain-shared/page_command.dart';
 import 'package:seeds/domain-shared/page_state.dart';
 import 'package:seeds/domain-shared/result_to_state_mapper.dart';
 import 'package:seeds/screens/profile_screens/edit_name/interactor/viewmodels/edit_name_bloc.dart';
+import 'package:seeds/screens/profile_screens/edit_name/interactor/viewmodels/page_commands.dart';
 
 class UpdateProfileStateMapper extends StateMapper {
   EditNameState mapResultToState(EditNameState currentState, Result result) {
     if (result.isError) {
-      return currentState.copyWith(pageState: PageState.failure, errorMessage: result.asError!.error.toString());
+      return currentState.copyWith(
+        pageState: PageState.failure,
+        pageCommand: ShowErrorMessage("Something went wrong trying to save your name"),
+      );
     } else {
-      final TransactionResponse res = result.asValue!.value;
-      return currentState.copyWith(pageState: PageState.success, name: res.data!.nickname);
+      return currentState.copyWith(
+        pageState: PageState.success,
+        pageCommand: EditNameSuccess(),
+      );
     }
   }
 }
