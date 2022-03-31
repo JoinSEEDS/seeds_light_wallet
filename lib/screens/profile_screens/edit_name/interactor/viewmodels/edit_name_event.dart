e144aadeb63709b827eb791bcc488200afe716f4
Gerardo Guijarro <gguijarro_c@chewy.com>
2022-03-30 20:49:23 -0600

    Added event freezed

--- lib/screens/profile_screens/edit_name/interactor/viewmodels/edit_name_event.dart
+++ lib/screens/profile_screens/edit_name/interactor/viewmodels/edit_name_event.dart
@@ -3,6 +3,8 @@ part of 'edit_name_bloc.dart';
 @freezed
 class EditNameEvent with _$EditNameEvent {
   const factory EditNameEvent.onNameChanged(String name) = _OnNameChanged;
+
   const factory EditNameEvent.submitName() = _SubmitName;
+
   const factory EditNameEvent.clearPageCommand() = _ClearPageCommand;
 }
