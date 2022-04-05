436d6fe4cf512ca160882b3408dba56cb073d7ac
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-04-05 19:35:39 -0400

    add extension when validating Id

--- lib/screens/create_region_screens/interactor/usecases/validate_region_id_usecase.dart
+++ lib/screens/create_region_screens/interactor/usecases/validate_region_id_usecase.dart
@@ -1,5 +1,6 @@
 import 'package:seeds/datasource/remote/api/region_repository.dart';
 import 'package:seeds/datasource/remote/model/region_model.dart';
+import 'package:seeds/domain-shared/app_constants.dart';
 import 'package:seeds/domain-shared/base_use_case.dart';
 
 class ValidateRegionIdUseCase extends InputUseCase<RegionModel?, String> {
@@ -7,6 +8,6 @@ class ValidateRegionIdUseCase extends InputUseCase<RegionModel?, String> {
 
   @override
   Future<Result<RegionModel?>> run(String input) async {
-    return _regionRepository.getRegionById(input);
+    return _regionRepository.getRegionById("$input$regionIdExtension");
   }
 }
