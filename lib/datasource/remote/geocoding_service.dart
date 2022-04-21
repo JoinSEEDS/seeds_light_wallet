bc2a449e8d40a431004a769f050a9c57b95f77f0
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2022-04-21 18:52:07 -0500

    Maps fix no address found for coords (#1770)
    * Region event card UI
    
    * Gery comments
    
    * Bug fix: map not drawed on address not found from coords

--- lib/datasource/remote/geocoding_service.dart
+++ lib/datasource/remote/geocoding_service.dart
@@ -10,7 +10,7 @@ class GeocodingService {
     try {
       return Result.value(await placemarkFromCoordinates(lat, lng, localeIdentifier: localeIdentifier));
     } catch (e) {
-      print('Error getting places from coordinates');
+      print('Error getting places from coordinates $e');
       return Result.error(e);
     }
   }
