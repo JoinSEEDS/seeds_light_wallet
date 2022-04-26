a6870272b6362baf73dc804688b337c2d3e87155
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2022-04-25 19:48:42 -0500

    Only users joined a region can join a event (#1778)

--- lib/screens/explore_screens/regions_screens/region_event_details/interactor/viewmodels/region_event_details_bloc.dart
+++ lib/screens/explore_screens/regions_screens/region_event_details/interactor/viewmodels/region_event_details_bloc.dart
@@ -2,8 +2,10 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:seeds/datasource/local/settings_storage.dart';
 import 'package:seeds/datasource/remote/model/firebase_models/region_event_model.dart';
+import 'package:seeds/datasource/remote/model/region_member_model.dart';
 import 'package:seeds/domain-shared/page_command.dart';
 import 'package:seeds/domain-shared/page_state.dart';
+import 'package:seeds/domain-shared/shared_use_cases/get_region_use_case.dart';
 import 'package:seeds/navigation/navigation_service.dart';
 import 'package:seeds/screens/explore_screens/regions_screens/region_event_details/interactor/usecases/join_region_event_use_case.dart';
 import 'package:seeds/screens/explore_screens/regions_screens/region_event_details/interactor/usecases/leave_region_event_use_case.dart';
@@ -15,6 +17,7 @@ part 'region_event_details_state.dart';
 
 class RegionEventDetailsBloc extends Bloc<RegionEventDetailsEvent, RegionEventDetailsState> {
   RegionEventDetailsBloc(RegionEventModel event) : super(RegionEventDetailsState.initial(event)) {
+    on<OnRegionEventDetailsMounted>(_onRegionEventDetailsMounted);
     on<OnRegionMapsLinkTapped>((_, emit) => emit(state.copyWith(pageCommand: LaunchRegionMapsLocation())));
     on<OnJoinRegionEventButtonPressed>(_onJoinRegionEventButtonPressed);
     on<OnLeaveRegionEventButtonPressed>(_onLeaveRegionEventButtonPressed);
@@ -27,6 +30,22 @@ class RegionEventDetailsBloc extends Bloc<RegionEventDetailsEvent, RegionEventDe
     on<ClearRegionEventPageCommand>((_, emit) => emit(state.copyWith()));
   }
 
+  Future<void> _onRegionEventDetailsMounted(
+      OnRegionEventDetailsMounted event, Emitter<RegionEventDetailsState> emit) async {
+    emit(state.copyWith(pageState: PageState.loading));
+    // Check if user has joined this Region
+    final result = await GetRegionUseCase().run(settingsStorage.accountName);
+    if (result.isError) {
+      emit(state.copyWith(pageState: PageState.failure));
+    } else {
+      final RegionMemberModel? member = result.asValue?.value;
+      // If the member is null or if the id does not correspond to this region (very rare case)
+      // then it could not join the event (The join button is disabled.
+      final bool isBrowseView = member == null || member.region != state.event.regionAccount;
+      emit(state.copyWith(pageState: PageState.success, isBrowseView: isBrowseView));
+    }
+  }
+
   void _onEditEventNameAndDescriptionTapped(
       OnEditEventNameAndDescriptionTapped event, Emitter<RegionEventDetailsState> emit) {
     emit(state.copyWith(
@@ -36,29 +55,29 @@ class RegionEventDetailsBloc extends Bloc<RegionEventDetailsEvent, RegionEventDe
 
   Future<void> _onJoinRegionEventButtonPressed(
       OnJoinRegionEventButtonPressed event, Emitter<RegionEventDetailsState> emit) async {
-    emit(state.copyWith(pageState: PageState.loading));
+    emit(state.copyWith(isJoinLeaveButtonLoading: true));
     final result = await JoinRegionEventUseCase().run(JoinRegionEventUseCase.input(
       eventId: state.event.id,
       joiningUser: settingsStorage.accountName,
     ));
     if (result.isError) {
-      emit(state.copyWith(pageCommand: ShowErrorMessage('')));
+      emit(state.copyWith(isJoinLeaveButtonLoading: false, pageCommand: ShowErrorMessage('')));
     } else {
-      emit(state.copyWith(pageState: PageState.success, isUserJoined: true));
+      emit(state.copyWith(isJoinLeaveButtonLoading: false, isUserJoined: true));
     }
   }
 
   Future<void> _onLeaveRegionEventButtonPressed(
       OnLeaveRegionEventButtonPressed event, Emitter<RegionEventDetailsState> emit) async {
-    emit(state.copyWith(pageState: PageState.loading));
+    emit(state.copyWith(isJoinLeaveButtonLoading: true));
     final result = await LeaveRegionEventUseCase().run(LeaveRegionEventUseCase.input(
       eventId: state.event.id,
       joiningUser: settingsStorage.accountName,
     ));
     if (result.isError) {
-      emit(state.copyWith(pageCommand: ShowErrorMessage('')));
+      emit(state.copyWith(isJoinLeaveButtonLoading: false, pageCommand: ShowErrorMessage('')));
     } else {
-      emit(state.copyWith(pageState: PageState.success, isUserJoined: false));
+      emit(state.copyWith(isJoinLeaveButtonLoading: false, isUserJoined: false));
     }
   }
 }
--- lib/screens/explore_screens/regions_screens/region_event_details/interactor/viewmodels/region_event_details_state.dart
+++ lib/screens/explore_screens/regions_screens/region_event_details/interactor/viewmodels/region_event_details_state.dart
@@ -4,13 +4,17 @@ class RegionEventDetailsState extends Equatable {
   final PageCommand? pageCommand;
   final PageState pageState;
   final RegionEventModel event;
+  final bool isJoinLeaveButtonLoading;
   final bool isUserJoined;
+  final bool isBrowseView;
 
   const RegionEventDetailsState({
     this.pageCommand,
     required this.pageState,
     required this.event,
+    required this.isJoinLeaveButtonLoading,
     required this.isUserJoined,
+    required this.isBrowseView,
   });
 
   @override
@@ -18,20 +22,26 @@ class RegionEventDetailsState extends Equatable {
         pageCommand,
         pageState,
         event,
+        isJoinLeaveButtonLoading,
         isUserJoined,
+        isBrowseView,
       ];
 
   RegionEventDetailsState copyWith({
     PageCommand? pageCommand,
     PageState? pageState,
     RegionEventModel? event,
+    bool? isJoinLeaveButtonLoading,
     bool? isUserJoined,
+    bool? isBrowseView,
   }) {
     return RegionEventDetailsState(
       pageCommand: pageCommand,
       pageState: pageState ?? this.pageState,
       event: event ?? this.event,
+      isJoinLeaveButtonLoading: isJoinLeaveButtonLoading ?? this.isJoinLeaveButtonLoading,
       isUserJoined: isUserJoined ?? this.isUserJoined,
+      isBrowseView: isBrowseView ?? this.isBrowseView,
     );
   }
 
@@ -39,7 +49,9 @@ class RegionEventDetailsState extends Equatable {
     return RegionEventDetailsState(
       pageState: PageState.initial,
       event: event,
+      isJoinLeaveButtonLoading: false,
       isUserJoined: event.users.contains(settingsStorage.accountName),
+      isBrowseView: true,
     );
   }
 }
