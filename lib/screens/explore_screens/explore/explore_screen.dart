089375a5b530977db6f2c55afecd692e507b4892
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-04-26 21:03:09 -0400

    remove comment flag

--- lib/screens/explore_screens/explore/explore_screen.dart
+++ lib/screens/explore_screens/explore/explore_screen.dart
@@ -83,15 +83,13 @@ class ExploreScreen extends StatelessWidget {
         onTapEvent: const OnBuySeedsCardTapped(),
       ),
     ];
-/*     if (!remoteConfigurations.featureFlagRegionsEnabled) {
-      exploreItems.removeWhere((i) => i.title == context.loc.explorerRegionsItemTitle);
-    } */
-    if (!remoteConfigurations.featureFlagP2PEnabled) {
-      exploreItems.removeWhere((i) => i.title == context.loc.explorerSwapItemTitle);
-    }
     if (!remoteConfigurations.featureFlagRegionsEnabled) {
       exploreItems.removeWhere((i) => i.title == context.loc.explorerRegionsItemTitle);
     }
+    if (!remoteConfigurations.featureFlagP2PEnabled) {
+      exploreItems.removeWhere((i) => i.title == context.loc.explorerSwapItemTitle);
+    }
+
     return BlocProvider(
       create: (_) => ExploreBloc(),
       child: BlocConsumer<ExploreBloc, ExploreState>(
