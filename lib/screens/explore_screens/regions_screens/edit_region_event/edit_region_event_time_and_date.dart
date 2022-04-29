309173856b3c9f7219cf9b5c5797d0da960b9363
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-04-29 10:59:23 -0400

    remove scaffold from block

--- lib/screens/explore_screens/regions_screens/edit_region_event/edit_region_event_time_and_date.dart
+++ lib/screens/explore_screens/regions_screens/edit_region_event/edit_region_event_time_and_date.dart
@@ -16,7 +16,9 @@ class EditRegionEventTimeAndDate extends StatelessWidget {
   Widget build(BuildContext context) {
     final event = ModalRoute.of(context)!.settings.arguments as RegionEventModel?;
 
-    return BlocProvider(
+    return Scaffold(
+      appBar: AppBar(title: const Text("Edit Event")),
+      body: BlocProvider(
         create: (_) => EditRegionEventBloc(event!),
         child: BlocConsumer<EditRegionEventBloc, EditRegionEventState>(
           listenWhen: (_, current) => current.pageCommand != null,
@@ -28,32 +30,44 @@ class EditRegionEventTimeAndDate extends StatelessWidget {
             BlocProvider.of<EditRegionEventBloc>(context).add(const ClearEditRegionEventPageCommand());
           },
           builder: (context, state) {
-            return Scaffold(
-              appBar: AppBar(title: const Text("Edit Event")),
-              body: SafeArea(
-                minimum: const EdgeInsets.all(horizontalEdgePadding),
-                child: Stack(
-                  children: [
-                    Column(
-                      children: [
-                        const SizedBox(height: 20),
-                        DateTimeRow(
-                            label: "Select Event Date",
-                            icon: const Icon(Icons.calendar_today_outlined),
-                            onWidgetTapped: () async {
-                              final DateTime? selected = await showDatePicker(
-                                context: context,
-                                initialDate: DateTime.now(),
-                                firstDate: DateTime.now(),
-                                lastDate: DateTime(2099),
-                              );
-                              // ignore: use_build_context_synchronously
-                              BlocProvider.of<EditRegionEventBloc>(context).add(OnSelectDateChanged(selected));
-                            },
-                            timeInfo: state.eventDateAndTimeInfo),
-                        const SizedBox(height: 30),
-                        DateTimeRow(
-                          label: "Select Event Start Time",
+            return SafeArea(
+              minimum: const EdgeInsets.all(horizontalEdgePadding),
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      DateTimeRow(
+                          label: "Select Event Date",
+                          icon: const Icon(Icons.calendar_today_outlined),
+                          onWidgetTapped: () async {
+                            final DateTime? selected = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime.now(),
+                              lastDate: DateTime(2099),
+                            );
+                            // ignore: use_build_context_synchronously
+                            BlocProvider.of<EditRegionEventBloc>(context).add(OnSelectDateChanged(selected));
+                          },
+                          timeInfo: state.eventDateAndTimeInfo),
+                      const SizedBox(height: 30),
+                      DateTimeRow(
+                        label: "Select Event Start Time",
+                        icon: const Icon(Icons.access_time),
+                        onWidgetTapped: () async {
+                          final TimeOfDay? picked = await showTimePicker(
+                            context: context,
+                            initialTime: const TimeOfDay(hour: 00, minute: 00),
+                          );
+                          // ignore: use_build_context_synchronously
+                          BlocProvider.of<EditRegionEventBloc>(context).add(OnStartTimeChanged(picked));
+                        },
+                        timeInfo: state.startTimeInfo,
+                      ),
+                      const SizedBox(height: 30),
+                      DateTimeRow(
+                          label: "Select Event End Time",
                           icon: const Icon(Icons.access_time),
                           onWidgetTapped: () async {
                             final TimeOfDay? picked = await showTimePicker(
@@ -61,39 +75,26 @@ class EditRegionEventTimeAndDate extends StatelessWidget {
                               initialTime: const TimeOfDay(hour: 00, minute: 00),
                             );
                             // ignore: use_build_context_synchronously
-                            BlocProvider.of<EditRegionEventBloc>(context).add(OnStartTimeChanged(picked));
+                            BlocProvider.of<EditRegionEventBloc>(context).add(OnEndTimeChanged(picked));
                           },
-                          timeInfo: state.startTimeInfo,
-                        ),
-                        const SizedBox(height: 30),
-                        DateTimeRow(
-                            label: "Select Event End Time",
-                            icon: const Icon(Icons.access_time),
-                            onWidgetTapped: () async {
-                              final TimeOfDay? picked = await showTimePicker(
-                                context: context,
-                                initialTime: const TimeOfDay(hour: 00, minute: 00),
-                              );
-                              // ignore: use_build_context_synchronously
-                              BlocProvider.of<EditRegionEventBloc>(context).add(OnEndTimeChanged(picked));
-                            },
-                            timeInfo: state.endTimeInfo),
-                      ],
+                          timeInfo: state.endTimeInfo),
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: FlatButtonLong(
+                      isLoading: state.isSaveChangesButtonLoading,
+                      title: "Save Changes",
+                      // TODO(gguij004): next pr
+                      onPressed: () {},
                     ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: FlatButtonLong(
-                        isLoading: state.isSaveChangesButtonLoading,
-                        title: "Save Changes",
-                        // TODO(gguij004): next pr
-                        onPressed: () {},
-                      ),
-                    )
-                  ],
-                ),
+                  )
+                ],
               ),
             );
           },
-        ));
+        ),
+      ),
+    );
   }
 }
