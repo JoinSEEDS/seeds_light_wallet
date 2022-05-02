c9c06b492384d6bbe86860bfebbb11444eb36d6f
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-05-02 06:04:27 -0400

    fix lint

--- lib/screens/explore_screens/regions_screens/edit_region_event/interactor/viewmodel/edit_region_event_bloc.dart
+++ lib/screens/explore_screens/regions_screens/edit_region_event/interactor/viewmodel/edit_region_event_bloc.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:bloc/bloc.dart';
-import 'package:cloud_firestore_platform_interface/src/geo_point.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
