374ca145a723f42548bae5966cf4fd6ec328679e
gguij004 <51983923+gguij004@users.noreply.github.com>
2022-05-03 11:08:00 -0400

    Pass events  [1/2] (#1805)
    * Pass events UI
    
    * change black to red

--- lib/screens/explore_screens/regions_screens/regions_main/components/region_event_card/interactor/viewmodels/region_event_card_state.dart
+++ lib/screens/explore_screens/regions_screens/regions_main/components/region_event_card/interactor/viewmodels/region_event_card_state.dart
@@ -2,17 +2,19 @@ part of 'region_event_card_bloc.dart';
 
 class RegionEventCardState extends Equatable {
   final List<ProfileModel> profiles;
+  final bool isEventPassEndDate;
 
-  const RegionEventCardState({required this.profiles});
+  const RegionEventCardState({required this.profiles, required this.isEventPassEndDate});
 
   @override
-  List<Object> get props => [profiles];
+  List<Object> get props => [profiles, isEventPassEndDate];
 
-  RegionEventCardState copyWith({List<ProfileModel>? profiles}) {
-    return RegionEventCardState(profiles: profiles ?? this.profiles);
+  RegionEventCardState copyWith({List<ProfileModel>? profiles, bool? isEventPassEndDate}) {
+    return RegionEventCardState(
+        profiles: profiles ?? this.profiles, isEventPassEndDate: isEventPassEndDate ?? this.isEventPassEndDate);
   }
 
   factory RegionEventCardState.initial() {
-    return const RegionEventCardState(profiles: []);
+    return const RegionEventCardState(profiles: [], isEventPassEndDate: false);
   }
 }
