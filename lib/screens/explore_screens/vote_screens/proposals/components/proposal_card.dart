e4919cc595831cd8ef735a9830da652eca59e4c2
buildc0de <i@buildco.de>
2022-05-03 18:22:47 -0500

    Tidy up

--- lib/screens/explore_screens/vote_screens/proposals/components/proposal_card.dart
+++ lib/screens/explore_screens/vote_screens/proposals/components/proposal_card.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:seeds/design/app_colors.dart';
 import 'package:seeds/design/app_theme.dart';
 import 'package:seeds/images/vote/category_label.dart';
