90b1cf2a56ac97e375e4474ba52c969b157d6807
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2022-05-09 19:12:38 -0500

    Bug fix: hide create event button for no region members (#1816)
    Co-authored-by: gguij004 <51983923+gguij004@users.noreply.github.com>

--- lib/screens/explore_screens/regions_screens/regions_main/components/region_events.dart
+++ lib/screens/explore_screens/regions_screens/regions_main/components/region_events.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:seeds/components/full_page_loading_indicator.dart';
 import 'package:seeds/datasource/remote/model/firebase_models/region_event_model.dart';
+import 'package:seeds/design/app_colors.dart';
 import 'package:seeds/screens/explore_screens/regions_screens/regions_main/components/region_event_card/region_event_card.dart';
 import 'package:seeds/screens/explore_screens/regions_screens/regions_main/interactor/viewmodel/region_bloc.dart';
 
@@ -10,19 +11,33 @@ class RegionEvents extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<RegionEventModel>>(
-      stream: BlocProvider.of<RegionBloc>(context).regionEvents,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final List<RegionEventModel> events = snapshot.data!;
-          return ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            itemCount: events.length,
-            itemBuilder: (_, index) => RegionEventCard(events[index]),
-          );
-        } else {
-          return const FullPageLoadingIndicator();
-        }
+    return BlocBuilder<RegionBloc, RegionState>(
+      builder: (context, state) {
+        return Scaffold(
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: state.canCreateAnEvent
+              ? FloatingActionButton(
+                  backgroundColor: AppColors.green1,
+                  onPressed: () => BlocProvider.of<RegionBloc>(context)..add(const OnAddEventButtonPressed()),
+                  child: const Icon(Icons.add, color: AppColors.white),
+                )
+              : null,
+          body: StreamBuilder<List<RegionEventModel>>(
+            stream: BlocProvider.of<RegionBloc>(context).regionEvents,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final List<RegionEventModel> events = snapshot.data!;
+                return ListView.builder(
+                  padding: const EdgeInsets.all(16.0),
+                  itemCount: events.length,
+                  itemBuilder: (_, index) => RegionEventCard(events[index]),
+                );
+              } else {
+                return const FullPageLoadingIndicator();
+              }
+            },
+          ),
+        );
       },
     );
   }
