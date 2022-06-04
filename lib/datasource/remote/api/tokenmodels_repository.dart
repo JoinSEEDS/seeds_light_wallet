0475e199ce86348b95ce77527f12c2a3b444311b
chuck <cfharr@gmail.com>
2022-06-04 16:59:21 -0700

    token models: add explanatory comments to tokenmodels_repository.dart

--- lib/datasource/remote/api/tokenmodels_repository.dart
+++ lib/datasource/remote/api/tokenmodels_repository.dart
@@ -7,13 +7,18 @@ import 'package:seeds/datasource/remote/api/http_repo/http_repository.dart';
 import 'package:seeds/datasource/remote/api/http_repo/seeds_scopes.dart';
 import 'package:seeds/datasource/remote/model/token_model.dart';
 
+/// Retrieve token metadata (used for display of currency cards etc) from
+/// a table in the token master smart contract (tmastr.seeds)
 class TokenModelsRepository extends HttpRepository {
   Future<Result<List<TokenModel>>> getTokenModels(List<String> useCaseList) async {
+    /// imports data from all tokens which are accepted under any of the entries in useCaseList
     print("[http] importing token models");
     final v1ChainUrl = Uri.parse(
            'https://api.telosfoundation.io/v1/chain/get_table_rows');
     final idSet = <int>{};
     final useCaseMap = <int, List<String>>{} ;
+    /// accumulate accepted token id's in idSet
+    /// record valid usecases for each token in useCaseMap
     for(final useCase in useCaseList) {
       final String request = '''
       {
@@ -35,25 +40,27 @@ class TokenModelsRepository extends HttpRepository {
               idSet.addAll(tokenIds);
           });
     }
-      final String request = '''
-      {
-        "code":"${SeedsCode.accountTokenModels.value}",
-        "table":"tokens",
-        "scope":"${SeedsCode.accountTokenModels.value}",
-        "json":true
-      }
-      ''';
-      return http
-          .post(v1ChainUrl, headers: headers, body: request)
-          .then((http.Response response) => mapHttpResponse<List<TokenModel>>(response, (dynamic body) {
-                 final tokens = List<Map<String,dynamic>>.from(body['rows'].toList()
-                     .where((row) => idSet.contains(row['id'])));
-                 for (final token in tokens) {
-                   token['usecases'] = useCaseMap[token['id']];
-                 }
-                 return List<TokenModel?>.from(tokens.map((token) =>
-                    TokenModel.fromJson(token))).whereNotNull().toList();
-             }))
-          .catchError((dynamic error) => mapHttpError(error));
+    final String request = '''
+    {
+      "code":"${SeedsCode.accountTokenModels.value}",
+      "table":"tokens",
+      "scope":"${SeedsCode.accountTokenModels.value}",
+      "json":true
+    }
+    ''';
+    return http
+        .post(v1ChainUrl, headers: headers, body: request)
+        .then((http.Response response) => mapHttpResponse<List<TokenModel>>(response, (dynamic body) {
+            final tokens = List<Map<String,dynamic>>.from(body['rows'].toList()
+               .where((row) => idSet.contains(row['id'])));
+            /// retrieve entire list of tokens from master list, then filter by idSet
+            for (final token in tokens) {
+              token['usecases'] = useCaseMap[token['id']];
+            }
+            return List<TokenModel?>.from(tokens.map((token) =>
+                TokenModel.fromJson(token))).whereNotNull().toList();
+                /// build a TokenModel from each selected token's metadata
+        }))
+        .catchError((dynamic error) => mapHttpError(error));
   }
 }
