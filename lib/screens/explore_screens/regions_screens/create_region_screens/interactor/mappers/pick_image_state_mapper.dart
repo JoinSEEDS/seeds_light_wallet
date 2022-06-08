2a0228d38ffc17031410ce3bac61b315c8b6be9c
Raúl Urtecho <42857405+RaulUrtecho@users.noreply.github.com>
2022-06-07 21:22:23 -0500

    Remove unnesessary error return from use case (#1876)

--- lib/screens/explore_screens/regions_screens/create_region_screens/interactor/mappers/pick_image_state_mapper.dart
+++ lib/screens/explore_screens/regions_screens/create_region_screens/interactor/mappers/pick_image_state_mapper.dart
@@ -5,18 +5,23 @@ import 'package:seeds/domain-shared/result_to_state_mapper.dart';
 import 'package:seeds/screens/explore_screens/regions_screens/create_region_screens/interactor/viewmodels/create_region_bloc.dart';
 
 class PickImageStateMapper extends StateMapper {
-  CreateRegionState mapResultToState(CreateRegionState currentState, Result<File> result) {
+  CreateRegionState mapResultToState(CreateRegionState currentState, Result<File?> result) {
     if (result.isError) {
       return currentState.copyWith(
           createImageUrl: false,
           pictureBoxState: currentState.file != null ? PictureBoxState.imagePicked : PictureBoxState.pickImage,
           pageCommand: ShowErrorMessage("Error on selecting image"));
     } else {
-      return currentState.copyWith(
-        file: result.asValue!.value,
-        createImageUrl: true,
-        pictureBoxState: PictureBoxState.imagePicked,
-      );
+      final File? file = result.asValue!.value;
+      if (file != null) {
+        return currentState.copyWith(
+          file: result.asValue!.value,
+          createImageUrl: true,
+          pictureBoxState: PictureBoxState.imagePicked,
+        );
+      } else {
+        return currentState.copyWith(createImageUrl: false, pictureBoxState: PictureBoxState.pickImage);
+      }
     }
   }
 }
