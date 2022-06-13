3500859b1d0c1a782c4c6484e34db7cafbb38e48
chuck <cfharr@gmail.com>
2022-06-13 11:02:36 -0700

    token models: paginate token master blockchain retrieval

--- lib/datasource/remote/api/tokenmodels_repository.dart
+++ lib/datasource/remote/api/tokenmodels_repository.dart
@@ -7,32 +7,14 @@ import 'package:seeds/datasource/remote/api/http_repo/seeds_tables.dart';
 /// Retrieve token metadata (used for display of currency cards etc) from
 /// a table in the token master smart contract (tmastr.seeds)
 class TokenModelsRepository extends HttpRepository {
-  Future<Result<List<int>>> getAcceptedTokenIds(String useCase) async {
+
+  /// acceptances table in tmastr.seeds lists (by internal id) all tokens accepted for a usecase
+  Future<Result<Map<String, dynamic>>> getAcceptedTokenIds(String useCase, int lowerBound) async {
     final request = createRequest(
       code: SeedsCode.accountTokenModels,
       scope: useCase,
       table: SeedsTable.tableTokenMasterAcceptances,
-      limit: 100,
-    );
-    return http
-        .post(Uri.parse('$baseURL/v1/chain/get_table_rows'),
-            headers: headers, body: request)
-        .then((http.Response response) =>
-            mapHttpResponse<List<int>>(response, (dynamic body) {
-              final acceptances = body['rows'].toList();
-              final tokenIds = List<int>.from(
-                  acceptances.map((row) => row['token_id']).toList());
-              return tokenIds;
-            }))
-        .catchError((error) => mapHttpError(error));
-  }
-
-  Future<Result<Map<String, dynamic>>> getMasterTokenTable() async {
-    print('[http] get token master list');
-    final request = createRequest(
-      code: SeedsCode.accountTokenModels,
-      scope: SeedsCode.accountTokenModels.value,
-      table: SeedsTable.tableTokenMasterTokens,
+      lowerBound: lowerBound.toString(),
       limit: 100,
     );
     return http
@@ -45,6 +27,27 @@ class TokenModelsRepository extends HttpRepository {
         .catchError((error) => mapHttpError(error));
   }
 
+  /// master token table in tmastr.seeds contains all registered tokens for all usecases
+  Future<Result<Map<String, dynamic>>> getMasterTokenTable(int lowerBound) async {
+    print('[http] get token master list');
+    final request = createRequest(
+      code: SeedsCode.accountTokenModels,
+      scope: SeedsCode.accountTokenModels.value,
+      table: SeedsTable.tableTokenMasterTokens,
+      lowerBound: lowerBound.toString(),
+      limit: 100,
+    );
+    return http
+        .post(Uri.parse('$baseURL/v1/chain/get_table_rows'),
+            headers: headers, body: request)
+        .then((http.Response response) =>
+            mapHttpResponse<Map<String, dynamic>>(response, (dynamic body) {
+              return body;
+            }))
+        .catchError((error) => mapHttpError(error));
+  }
+
+  /// schema table in tmastr.seeds defines valid token metadata
   Future<Result<Map<String, dynamic>>> getSchema() async {
     print('[http] get token master list schema');
     final request = createRequest(
--- lib/domain-shared/shared_use_cases/get_token_models_use_case.dart
+++ lib/domain-shared/shared_use_cases/get_token_models_use_case.dart
@@ -13,6 +13,8 @@ class TokenModelSelector {
   });
 }
 
+/// Retrieve token metadata (used for display of currency cards etc) from
+/// token master smart contract (tmastr.seeds) and build TokenModel objects
 class GetTokenModelsUseCase extends InputUseCase<List<TokenModel>, TokenModelSelector> {
   @override
   Future<Result<List<TokenModel>>> run(TokenModelSelector input) async {
@@ -22,34 +24,68 @@ class GetTokenModelsUseCase extends InputUseCase<List<TokenModel>, TokenModelSel
     /// accumulate accepted token id's in idSet
     /// record valid usecases (from both acceptList and infoList) for each token in useCaseMap
     for(final useCase in input.acceptList + (input.infoList ?? [])) {
-      final tokenIdsResult = await TokenModelsRepository().getAcceptedTokenIds(useCase);
-      if(tokenIdsResult.isError) {
-        continue;
-      }
-      final tokenIds = tokenIdsResult.asValue!.value;
-      for (final id in tokenIds) {
-        useCaseMap[id] ??= [];
-        useCaseMap[id]!.add(useCase);
-      }
-      if(input.acceptList.contains(useCase)) {
-        idSet.addAll(tokenIds);
+      bool more = true;
+      int lastRetrieved = -1;
+      while(more) {
+        final acceptedTokenIdsResult = await TokenModelsRepository()
+            .getAcceptedTokenIds(useCase, lastRetrieved+1);
+        if (acceptedTokenIdsResult.isError) {
+          continue;
+        }
+        if(acceptedTokenIdsResult.isError) {
+          return acceptedTokenIdsResult.asError!;
+        }
+        final resultValue = acceptedTokenIdsResult.asValue!.value;
+        more = resultValue['more'];
+        final acceptances = resultValue['rows'].toList();
+        final tokenIds = List<int>.from(
+            acceptances.map((row) => row['token_id']).toList());
+        if(tokenIds.isEmpty) {
+          continue;
+        }
+        for (final id in tokenIds) {
+          useCaseMap[id] ??= [];
+          useCaseMap[id]!.add(useCase);
+        }
+        if (input.acceptList.contains(useCase)) {
+          idSet.addAll(tokenIds);
+        }
+        lastRetrieved = tokenIds.last;
       }
     }
 
-    final allTokensResult = await TokenModelsRepository().getMasterTokenTable();
-    if(allTokensResult.isError) {
-      return Result.error("failed to get master token list");
+    final List<int> remainingIds = idSet.toList();
+    remainingIds.sort();
+    bool more = true;
+    final rv = <TokenModel>[];
+    while(more && remainingIds.isNotEmpty) {
+      final allTokensResult = await TokenModelsRepository()
+          .getMasterTokenTable(remainingIds[0]);
+      if (allTokensResult.isError) {
+        return Result.error("failed to get master token list");
+      }
+      final resultValue = allTokensResult.asValue!.value;
+      more = resultValue["more"];
+      final allTokenRows = resultValue["rows"];
+      final tokens = allTokenRows.where((row) {
+        final id = row["id"];
+        if (remainingIds.contains(id)) {
+          remainingIds.remove(id);
+          return true;
+        } else {
+          return false;
+        }
+      }).toList();
+      /// retrieve entire list of tokens from master list, then filter by idSet; paginate by "more"
+      for (final token in tokens) {
+        token['usecases'] = useCaseMap[token['id']];
+      }
+      final theseTokens =List<TokenModel?>.from(tokens.map((token) =>
+          TokenModel.fromJson(token)));
+      rv.addAll(theseTokens.whereNotNull());
+          /// build a TokenModel from each selected token's metadata
     }
-    final allTokens = allTokensResult.asValue!.value["rows"];
-    final tokens = allTokens.where((row) => idSet.contains(row['id']));
-    /// retrieve entire list of tokens from master list, then filter by idSet
-    for (final token in tokens) {
-      token['usecases'] = useCaseMap[token['id']];
-    }
-    return Result.value(List<TokenModel?>.from(tokens.map((token) =>
-      TokenModel.fromJson(token))).whereNotNull().toList());
-      /// build a TokenModel from each selected token's metadata
-
+    return Result.value(rv);
   }
 
 }
