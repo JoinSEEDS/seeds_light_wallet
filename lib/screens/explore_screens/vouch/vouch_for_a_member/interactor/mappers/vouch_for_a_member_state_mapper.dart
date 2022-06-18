0c5f14dd59dd77e99cce96b53a9c8b35155f4977
NIK <nheger@gmail.com>
2022-06-18 11:49:38 +0800

    we don't actually need this, saving code

--- lib/screens/explore_screens/vouch/vouch_for_a_member/interactor/mappers/vouch_for_a_member_state_mapper.dart
+++ lib/screens/explore_screens/vouch/vouch_for_a_member/interactor/mappers/vouch_for_a_member_state_mapper.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:seeds/datasource/remote/model/transaction_response.dart';
 import 'package:seeds/domain-shared/page_command.dart';
 import 'package:seeds/domain-shared/page_state.dart';
@@ -8,6 +10,29 @@ import 'package:seeds/screens/explore_screens/vouch/vouch_for_a_member/interacto
 class VouchForAMemberStateMapper extends StateMapper {
   VouchForAMemberState mapResultToState(VouchForAMemberState currentState, Result<TransactionResponse> result) {
     if (result.isError) {
+//       extension EosErrorParser on String {
+//   String get userErrorMessage {
+//     try {
+//       return jsonDecode(this)["error"]["details"][0]["message"];
+//     } catch (error) {
+//       print("Error decoding error message $this $error");
+//       return this;
+//     }
+//   }
+// }
+
+      bool vouchedAlready = false;
+      try {
+        String errorString = result.asError!.error.toString();
+        print("error ");
+        print(result.asError!.error);
+        String message = jsonDecode(errorString)["error"]["details"][0]["message"];
+        print("message $message ");
+        print("errorString $errorString ");
+      } catch (err) {
+        print("error parsing message");
+        print(err);
+      }
       return currentState.copyWith(
         pageState: PageState.failure,
         pageCommand: ShowErrorMessage("Error when vouching for a member"),
