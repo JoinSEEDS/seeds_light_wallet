b779b1175d26f9f529bfdc46ebaa38c1798f1b0a
chuck <cfharr@gmail.com>
2022-09-04 14:25:26 -0700

    show blockchain errors to user in 'send' flow

--- lib/screens/transfer/send/send_enter_data/send_enter_data_screen.dart
+++ lib/screens/transfer/send/send_enter_data/send_enter_data_screen.dart
@@ -87,7 +87,7 @@ class SendEnterDataScreen extends StatelessWidget {
                       ? const SendLoadingIndicator()
                       : const SafeArea(child: FullPageLoadingIndicator());
                 case PageState.failure:
-                  return const SafeArea(child: FullPageErrorIndicator());
+                  return SafeArea(child: FullPageErrorIndicator(errorMessage: state.errorMessage));
                 case PageState.success:
                   return SafeArea(
                     minimum: const EdgeInsets.all(horizontalEdgePadding),
