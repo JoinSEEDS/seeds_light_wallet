df29de26906b620f1ac2ee8984c77cc98d4553de
NIK <nheger@gmail.com>
2024-05-01 11:00:53 +0800

    typo

--- lib/datasource/remote/api/eos_repo/eos_repository.dart
+++ lib/datasource/remote/api/eos_repo/eos_repository.dart
@@ -10,7 +10,7 @@ abstract class EosRepository {
   late final String cpuPrivateKey = dotenv.env['PAYCPU_SEEDS_KEY'] ?? '';
 
   late final String onboardingPrivateKey = dotenv.env['ONBOARDING_SEEDS_KEY'] ?? '';
-;
+
   String onboardingAccountName = 'join.seeds';
   int guardianRecoveryTimeDelaySec = const Duration(hours: 24).inSeconds;
 
