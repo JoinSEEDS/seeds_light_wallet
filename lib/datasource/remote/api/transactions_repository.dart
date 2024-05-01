462d43e8f275f991cb8f0f047e8d74ef7fa97e9d
C. Harrison <cfharr@gmail.com>
2024-04-30 18:49:27 -0700

    In transaction list, silently skip non-token transfers (e.g. NFT)

--- lib/datasource/remote/api/transactions_repository.dart
+++ lib/datasource/remote/api/transactions_repository.dart
@@ -1,4 +1,5 @@
 import 'package:async/async.dart';
+import 'package:collection/collection.dart';
 import 'package:http/http.dart' as http;
 import 'package:seeds/datasource/remote/api/http_repo/http_repository.dart';
 import 'package:seeds/datasource/remote/model/transaction_model.dart';
@@ -13,7 +14,7 @@ class TransactionsListRepository extends HttpRepository {
         .then((http.Response response) => mapHttpResponse<List<TransactionModel>>(response, (dynamic body) {
               final List<dynamic> transfers = body['actions'].toList();
 
-              return List<TransactionModel>.of(transfers.map((transfer) => TransactionModel.fromJson(transfer)));
+              return List<TransactionModel>.of(transfers.map((transfer) => TransactionModel.fromJson(transfer)).whereNotNull());
             }))
         .catchError((dynamic error) => mapHttpError(error));
   }
