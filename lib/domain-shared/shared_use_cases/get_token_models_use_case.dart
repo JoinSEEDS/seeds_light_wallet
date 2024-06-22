a21ae2e81a18d64959ea40b15ce355522ec15c09
NIK <nheger@gmail.com>
2024-06-22 08:30:14 +0800

    formatting, minor changes to fix errors

--- lib/domain-shared/shared_use_cases/get_token_models_use_case.dart
+++ lib/domain-shared/shared_use_cases/get_token_models_use_case.dart
@@ -1,10 +1,9 @@
 import 'package:collection/collection.dart';
 import 'package:dynamic_parallel_queue/dynamic_parallel_queue.dart';
-import 'package:seeds/datasource/remote/api/tokenmodels_repository.dart';
 import 'package:seeds/datasource/remote/api/stat_repository.dart';
+import 'package:seeds/datasource/remote/api/tokenmodels_repository.dart';
 import 'package:seeds/datasource/remote/model/token_model.dart';
 import 'package:seeds/domain-shared/base_use_case.dart';
-import 'package:seeds/screens/wallet/components/tokens_cards/interactor/usecases/load_token_balances_use_case.dart';
 
 class TokenModelSelector {
   final List<String> acceptList;
@@ -23,25 +22,24 @@ class GetTokenModelsUseCase extends InputUseCase<List<TokenModel>, TokenModelSel
   Future<Result<List<TokenModel>>> run(TokenModelSelector input) async {
     print("[http] importing token models");
     final idSet = <int>{};
-    final useCaseMap = <int, List<String>>{} ;
+    final useCaseMap = <int, List<String>>{};
+
     /// accumulate accepted token id's in idSet
     /// record valid usecases (from both acceptList and infoList) for each token in useCaseMap
-    for(final useCase in input.acceptList + (input.infoList ?? [])) {
+    for (final useCase in input.acceptList + (input.infoList ?? [])) {
       bool more = true;
       int lastRetrieved = -1;
       fetchOneUseCase:
-      while(more) {
-        final acceptedTokenIdsResult = await TokenModelsRepository()
-            .getAcceptedTokenIds(useCase, lastRetrieved+1);
-        if(acceptedTokenIdsResult.isError) {
+      while (more) {
+        final acceptedTokenIdsResult = await TokenModelsRepository().getAcceptedTokenIds(useCase, lastRetrieved + 1);
+        if (acceptedTokenIdsResult.isError) {
           break fetchOneUseCase;
         }
         final resultValue = acceptedTokenIdsResult.asValue!.value;
         more = resultValue['more'];
         final acceptances = resultValue['rows'].toList();
-        final tokenIds = List<int>.from(
-            acceptances.map((row) => row['token_id']).toList());
-        if(tokenIds.isEmpty) {
+        final tokenIds = List<int>.from(acceptances.map((row) => row['token_id']).toList());
+        if (tokenIds.isEmpty) {
           continue;
         }
         for (final id in tokenIds) {
@@ -59,9 +57,8 @@ class GetTokenModelsUseCase extends InputUseCase<List<TokenModel>, TokenModelSel
     remainingIds.sort();
     bool more = true;
     final rv = <TokenModel>[];
-    while(more && remainingIds.isNotEmpty) {
-      final allTokensResult = await TokenModelsRepository()
-          .getMasterTokenTable(remainingIds[0]);
+    while (more && remainingIds.isNotEmpty) {
+      final allTokensResult = await TokenModelsRepository().getMasterTokenTable(remainingIds[0]);
       if (allTokensResult.isError) {
         return Result.error("failed to get master token list");
       }
@@ -77,30 +74,31 @@ class GetTokenModelsUseCase extends InputUseCase<List<TokenModel>, TokenModelSel
           return false;
         }
       }).toList();
+
       /// retrieve entire list of tokens from master list, then filter by idSet; paginate by "more"
       for (final token in tokens) {
         token['usecases'] = useCaseMap[token['id']];
       }
       final StatRepository _statRepository = StatRepository();
-      List<TokenModel?> theseTokens = [];
+      final List<TokenModel?> theseTokens = [];
+
       /// verify token contract on chain and get contract precision
       loadData(token) async {
-        TokenModel? tm = TokenModel.fromJson(token as Map<String, dynamic>);
+        final TokenModel? tm = TokenModel.fromJson(token as Map<String, dynamic>);
         if (tm != null) {
-          await _statRepository
-            .getTokenStat(tokenContract: tm.contract, symbol: tm.symbol)
-            .then(
-              (stats) async {
-                if (stats.asValue != null) {
-                  final supply = stats.asValue!.value.supplyString;
-                  tm.setPrecisionFromString(supply);
-                  theseTokens.add(tm);
-                  print("supply: $supply");
-                }
-              },
-            ).catchError((dynamic error) => _statRepository.mapHttpError(error));
+          await _statRepository.getTokenStat(tokenContract: tm.contract, symbol: tm.symbol).then(
+            (stats) async {
+              if (stats.asValue != null) {
+                final supply = stats.asValue!.value.supplyString;
+                tm.setPrecisionFromString(supply);
+                theseTokens.add(tm);
+                print("supply: $supply");
+              }
+            },
+          ).catchError((dynamic error) => _statRepository.mapHttpError(error));
         }
       }
+
       final queue = Queue(parallel: 5);
       for (final dynamic token in tokens) {
         queue.add(() async {
@@ -109,9 +107,9 @@ class GetTokenModelsUseCase extends InputUseCase<List<TokenModel>, TokenModelSel
       }
       await queue.whenComplete();
       rv.addAll(theseTokens.whereNotNull());
-          /// build a TokenModel from each selected token's metadata
+
+      /// build a TokenModel from each selected token's metadata
     }
     return Result.value(rv);
   }
-
 }
