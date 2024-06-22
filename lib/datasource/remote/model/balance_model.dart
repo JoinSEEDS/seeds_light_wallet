db967a59e621a3bbb44717d11ce212722751d766
NIK <nheger@gmail.com>
2024-06-22 09:46:54 +0800

    loading token precisions with balance

--- lib/datasource/remote/model/balance_model.dart
+++ lib/datasource/remote/model/balance_model.dart
@@ -2,15 +2,19 @@
 class BalanceModel {
   final double quantity;
   final bool hasBalance;
+  final int precision;
 
-  const BalanceModel(this.quantity, {this.hasBalance = true});
+  const BalanceModel(this.quantity, this.precision, {this.hasBalance = true});
 
   factory BalanceModel.fromJson(List<dynamic> json) {
     if (json.isEmpty || json[0].isEmpty) {
-      return const BalanceModel(0, hasBalance: false);
+      return const BalanceModel(0, 0, hasBalance: false);
     } else {
-      final amount = double.parse((json[0] as String).split(' ').first);
-      return BalanceModel(amount);
+      final amountString = (json[0] as String).split(' ').first;
+      final amount = double.parse(amountString);
+      final decimalPointIndex = amountString.indexOf(".");
+      final precision = decimalPointIndex != -1 ? amountString.substring(decimalPointIndex).length - 1 : 0;
+      return BalanceModel(amount, precision);
     }
   }
 }
